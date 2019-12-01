59f6ec572cb40d832263f5b66f6429eb7a56f14a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-01 16:59:06 +0100

    Update video_capture.py
    ok chui perdu, voir tous les models dlib qu'on peut trouver mais la on peut faire les zones a l'intérieur de la figure, voir si les zones bougent par exemple pour un rictus

--- face/video_capture.py
+++ face/video_capture.py
@@ -1,32 +1,36 @@
-from cv2 import VideoCapture, resize, waitKey, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY
+from cv2 import VideoCapture, resize, waitKey, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY, imwrite
 from basics_operations import start_timmer, timmer
-from face_detection import findHead, headDetector
+from points_face.face_detection import findHead, headDetector
 
 
-def video_lecture(video_name):
+def video_lecture(video_name, face_points):
 
     video = VideoCapture(video_name)
 
     #Detect head
     detector = headDetector()
+    predictor = pointsPredictor(gray, head, face_points)
+
 
     while True:
 
         #Timmer start
         start = start_timmer()
 
-        frame = resize(video.read()[1], (400, 300))
+        frame = resize(video.read()[1], (500, 400))
         gray = cvtColor(frame, COLOR_BGR2GRAY)
 
         #Repear head points
-        findHead(detector, gray, frame, "displaying")
+        head = findHead(detector, gray, frame, "no_displaying")
 
         imshow('frame', frame)
 
         #Timmer end
         timmer(start)
 
-        if waitKey(1) & 0xFF == ord("q"):break
+        if waitKey(1) & 0xFF == ord("q"):
+            imwrite("ici.jpg", frame)
+            break
 
     video.release()
     destroyAllWindows()
