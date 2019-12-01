51c19e13a08f35b5e4e668f9cf309e8fa5bf2674
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-01 19:24:44 +0100

    Update face_detection.py

--- face/points_face/face_detection.py
+++ face/points_face/face_detection.py
@@ -1,56 +1,48 @@
-from dlib import get_frontal_face_detector
-from cv2 import rectangle
+from dlib import get_frontal_face_detector, shape_predictor
+from cv2 import rectangle, convexHull, Subdiv2D, boundingRect, line
+from numpy import array, int32
+
 
 def headDetector():
     detector = get_frontal_face_detector()
     return detector
 
-def findHead(detector, gray_frame, out_frame, displaying):
-    """We find the head"""
-
-    (x, y, w, h) = [[face.left(), face.top(), face.right(), face.bottom()] for
-                     face in detector(gray_frame)][0]
-
-    if displaying in ("displaying"):
-        rectangle(out_frame, (x, y), (w, h), (0, 0, 255), 3)
-
-    return x, y, w, h
-
-
-def pointsPredictor(gray, face, file):
-    predictor = dlib.shape_predictor(file)
+def pointsPredictor(file):
+    predictor = shape_predictor(file)
     return predictor
 
-
-def points(face, predictor):
-    #Nous sert pour les yeux aussi
+def points_landmarks(face, gray, predictor):
+    #SERT POUR LES YEUX
     for pts in face:
         landmarks = predictor(gray, pts)
         return landmarks
 
 #-------------------------------------- zone
 
-def intra_face(landmarks):
+def intra_face(landmarks, faces, img):
 
     #points of face
     points = [(landmarks.part(n).x, landmarks.part(n).y)
                          for pts in faces for n in range(0, 68)]
 
     #Convex points (contour of face)
-    convexhull = convexHull(np.array(points))
+    convexhull = convexHull(array(points))
+
+    head = boundingRect(convexhull)
 
     #Find triangles into rectangles points (face)
-    subdiv = cv2.Subdiv2D(cv2.boundingRect(convexhull))
+    subdiv = Subdiv2D(head)
 
     #Put points for extract the list
     subdiv.insert(points)
 
     #Get triangles list
-    triangles = np.array(subdiv.getTriangleList(), dtype=np.int32)
+    triangles = array(subdiv.getTriangleList(), dtype=int32)
 
     #Recup points
     t_points = [[(t[0], t[1]), (t[2], t[3]), (t[4], t[5])] for t in triangles ]
 
+    return t_points, head
 
 def exterior_face():
     pass
