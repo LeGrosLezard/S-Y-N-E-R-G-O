fae283d42dfcdfc19f3006d2c94341296e8468d0
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-02 15:45:12 +0100

    Update video_capture.py

--- face/video_capture.py
+++ face/video_capture.py
@@ -1,15 +1,27 @@
+from dlib import get_frontal_face_detector, shape_predictor
+from time import time
 from cv2 import VideoCapture, resize, waitKey, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY, imwrite
-from basics_operations import start_timmer, timmer
-from points_face.face_detection import headDetector, pointsPredictor, intra_face, points_landmarks
-from points_face.face_display import recuperate_face
+from face_detection import intra_face, points_landmarks, exterior_face
+#from face_display import recuperate_face
+
+
+
+def start_timmer():
+    start = time()
+    return start
+
+def timmer(start):
+    elapsed = time() - start
+    print(elapsed)
+
 
 def video_lecture(video_name, face_points):
 
     video = VideoCapture(video_name)
 
     #Detect head
-    detector = headDetector()
-    predictor = pointsPredictor(face_points)
+    detector = get_frontal_face_detector()
+    predictor = shape_predictor(face_points)
 
 
     while True:
@@ -25,12 +37,13 @@ def video_lecture(video_name, face_points):
 
         #Triangle of face points
         head_points, head, convexhull = intra_face(landmarks, face, frame)
+        exterior_face(head, gray)
 
         #Display
-        recuperate_face(convexhull, gray, frame, head_points)
+        #recuperate_face(convexhull, gray, frame, head_points)
         
 
-        #imshow('frame', frame)
+        imshow('frame', frame)
 
         #Timmer end
         timmer(start)
