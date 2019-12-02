2159e6e785ff988695aa3e59af24c212a42bb806
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-02 01:58:52 +0100

    Update video_capture.py

--- face/video_capture.py
+++ face/video_capture.py
@@ -1,7 +1,7 @@
 from cv2 import VideoCapture, resize, waitKey, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY, imwrite
 from basics_operations import start_timmer, timmer
 from points_face.face_detection import headDetector, pointsPredictor, intra_face, points_landmarks
-
+from points_face.face_display import recuperate_face
 
 def video_lecture(video_name, face_points):
 
@@ -24,10 +24,13 @@ def video_lecture(video_name, face_points):
         landmarks, face = points_landmarks(gray, predictor, detector)
 
         #Triangle of face points
-        head_points, head = intra_face(landmarks, face, frame)
+        head_points, head, convexhull = intra_face(landmarks, face, frame)
 
+        #Display
+        recuperate_face(convexhull, gray, frame, head_points)
+        
 
-        imshow('frame', frame)
+        #imshow('frame', frame)
 
         #Timmer end
         timmer(start)
