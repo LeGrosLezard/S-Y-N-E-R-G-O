ac115dd222e9f5fa8ebf3e672b7f366f5e6dc8f5
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-02 15:45:28 +0100

    Update main.py

--- face/main.py
+++ face/main.py
@@ -1,7 +1,16 @@
 from video_capture import video_lecture
 
+#Video
+videoA = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\video\a.mp4"
+videoB = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\video\b.mp4"
+
+
+#Models
+facePoints = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\face\models\shape_predictor_68_face_landmarks.dat"
+
+
+
+
 if __name__ == "__main__":
 
-    from path import videoA, videoB
-    
-    video_lecture(videoA)
+    video_lecture(videoA, facePoints)
