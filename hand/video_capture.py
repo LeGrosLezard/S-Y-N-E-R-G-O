1ca8787777437204d9e0f082a566cda03c19677d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-07 19:37:05 +0100

    Update video_capture.py
    grrrrrrrrr jvoulais le modele 3d faut télécharger chai pas quoi j'ai tout fais de a a z ca marche pas...

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -2,7 +2,7 @@ import cv2
 import tensorflow as tf
 import numpy as np
 from time import time
-
+import imutils
 
 print(tf.version.VERSION)
 
@@ -78,7 +78,7 @@ def no_hand(detections, hand):
 
 def hands(hand, img):
     """Make a crop"""
-    var = 25
+    var = 30
     hand = img[int(hand[0][2] - var):int(hand[0][3] + var), int(hand[0][0]) - var:int(hand[0][1]) + var]
     return hand
 
@@ -92,7 +92,7 @@ def skin_color(hand):
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
-
+    skinMask = cv2.erode(skinMask,kernel,iterations = 1)
     skinYCrCb = cv2.bitwise_and(hand, hand, mask = skinMask)
 
     return skinYCrCb
@@ -102,7 +102,7 @@ def video_capture(video_name, hand_model):
     detection_graph, sess = load_inference_graph(hand_model)
     video = cv2.VideoCapture(video_name)
     detections = [[], []]
-    fgbg = cv2.createBackgroundSubtractorMOG2()
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=3, varThreshold=25, detectShadows=False)
 
 
     while True:
@@ -113,111 +113,67 @@ def video_capture(video_name, hand_model):
         copy = frame.copy()
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        fgmask = fgbg.apply(frame)
-        bg = cv2.bitwise_and(frame, frame, mask=fgmask)
-
-        cv2.imshow("bg", bg)
-
-        frameRGB = cv2.cvtColor(bg, cv2.COLOR_BGR2RGB)
-        bg = skin_color(bg)
-
-        gray = cv2.cvtColor(bg, cv2.COLOR_BGR2GRAY)
-        contours = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
-
-        cv2.drawContours(copy, contours, -1, (255, 255, 255), 1)
-        cv2.fillPoly(copy, pts =contours, color=(255, 255, 255))
-
-        gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
-        ret, thresh1 = cv2.threshold(gray, 250, 255,cv2.THRESH_BINARY)
 
 
-        contours = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
-        for i in contours:
-            if cv2.contourArea(i) > 100:
-                x, y, w, h = cv2.boundingRect(i)
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        #Detect 2 boxes with > 0.27
+        boxes, scores = detect_objects(frameRGB, detection_graph, sess)
 
+        #Recuperate 2 boxes
+        areas = recuperate_detection(scores, boxes)
+        #Define left and right hands
+        left_hand, right_hand = determination_hand(areas)
 
+        #No hand
+        if left_hand == []:left_hand = no_hand(detections[0], "left")
+        if right_hand == []:right_hand = no_hand(detections[1], "right")
 
-        cv2.imshow("fgmask", frame)
+        #Litlle detection
+        left_hand = only_part(left_hand, detections[0])
+        right_hand = only_part(right_hand, detections[1])
+
+        detections[0] = left_hand
+        detections[1] = right_hand
+
+        #frameframe = frame.copy()
+        var = 30
+        #cv2.rectangle(frameframe, (int(right_hand[0][0]) - var, int(right_hand[0][2])- var), (int(right_hand[0][1]) + var, int(right_hand[0][3]) + var), (255, 0, 0), 3)
+        #cv2.rectangle(frameframe, (int(left_hand[0][0]) - var, int(left_hand[0][2])- var) ,(int(left_hand[0][1]) + var, int(left_hand[0][3]) + var), (0,0, 255) , 3)
 
 
 
+        left_hand = hands(left_hand, frame)
+        right_hand = hands(right_hand, frame)
+
+        left_hand_skin = skin_color(left_hand)
+        right_hand_skin = skin_color(right_hand)
+        frame_skin = skin_color(frame)
+
+        try:
+            R = cv2.RETR_TREE
+            P = cv2.CHAIN_APPROX_NONE
+            gray1 = cv2.cvtColor(left_hand_skin, cv2.COLOR_BGR2GRAY)
+            gray2 = cv2.cvtColor(right_hand_skin, cv2.COLOR_BGR2GRAY)
+
+            contours, _ = cv2.findContours(gray1, R, P)
+            c = max(contours, key = cv2.contourArea)
+            cv2.drawContours(left_hand, c, -1, (0,0,255), 1)
+            cv2.fillPoly(left_hand, pts =[c], color=(0, 255, 0))
 
 
+            contours, _ = cv2.findContours(gray2, R, P)
+            c = max(contours, key = cv2.contourArea)
+            cv2.drawContours(right_hand, c, -1, (0,0,255), 1)
+            cv2.fillPoly(right_hand, pts =[c], color=(0, 255, 0))
 
 
+            cv2.imshow("zfafaz", frame)
+        except:
+             cv2.imshow("frame_skin", frame_skin)
+             cv2.waitKey(0)
 
 
+        #cv2.imshow("fa", frameframe)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##        #Detect 2 boxes with > 0.27
-##        boxes, scores = detect_objects(frameRGB, detection_graph, sess)
-##
-##        #Recuperate 2 boxes
-##        areas = recuperate_detection(scores, boxes)
-##        #Define left and right hands
-##        left_hand, right_hand = determination_hand(areas)
-##
-##        #No hand
-##        if left_hand == []:left_hand = no_hand(detections[0], "left")
-##        if right_hand == []:right_hand = no_hand(detections[1], "right")
-##
-##        #Litlle detection
-##        left_hand = only_part(left_hand, detections[0])
-##        right_hand = only_part(right_hand, detections[1])
-##
-##        detections[0] = left_hand
-##        detections[1] = right_hand
-##
-##        cv2.rectangle(frame, (int(right_hand[0][0]), int(right_hand[0][2])), (int(right_hand[0][1]), int(right_hand[0][3])), (255, 0, 0), 3)
-##        cv2.rectangle(frame, (int(left_hand[0][0]), int(left_hand[0][2])),(int(left_hand[0][1]), int(left_hand[0][3])), (0,0, 255) , 3)
-##
-##        #Croping
-##        left_hand = hands(left_hand, frame)
-##        right_hand = hands(right_hand, frame)
-##
-##        left_hand_skin = skin_color(left_hand)
-##        right_hand_skin = skin_color(right_hand)
-##        frame_skin = skin_color(frame)
-##
-##
-##
-##
-##
-##
-##        cv2.imshow("left_hand_skin", left_hand_skin)
-##
-##        cv2.imshow("right_hand_skin", right_hand_skin)
-##        cv2.imshow("frame_skin", frame_skin)
-##
-##
-##        cv2.imshow("frame", frameRGB)
 
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
