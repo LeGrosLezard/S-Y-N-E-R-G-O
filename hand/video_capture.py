f974d519efa606c38564dc8580cc5badfe31fe9d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-09 03:42:31 +0100

    Update video_capture.py

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -17,6 +17,7 @@ def timmer(start):
     print(elapsed)
 
 def sustractor_background(frame, fgbg):
+    """Delete background, if one thing moves we detect it"""
 
     suba = fgbg.apply(frame)
     contours, _ = cv2.findContours(suba, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -33,6 +34,8 @@ def sustractor_background(frame, fgbg):
 
 
 def hands_detections(scores, boxes, droite, gauche, hist_droite, hist_gauche, frame):
+    """We detect hands, by position we define if it are to left or to right.
+    Recup it for history if we have impossible detections"""
 
     detected_points = [(int(boxes[i][1] * 500), int(boxes[i][3] * 500),
                         int(boxes[i][0] * 400), int(boxes[i][2] * 400)) for i in range(2) if scores[i] > 0.10]
@@ -41,6 +44,8 @@ def hands_detections(scores, boxes, droite, gauche, hist_droite, hist_gauche, fr
                            [(pt[0], pt[2], pt[1], pt[3]) for pt in detected_points if pt[0] > 250]   ]
 
     def append_to_hand(hand, movement, historic, part):
+        """Add movements to his list"""
+
         if len(movement[part]) >= 1:
             hand.append(movement[part][0])
             historic[0].append(movement[part][0][0])
@@ -51,6 +56,7 @@ def hands_detections(scores, boxes, droite, gauche, hist_droite, hist_gauche, fr
 
 
 def possibles_movement(historic, movement, possibility):
+    """By history if we have x and y axis > 60: impossible move"""
 
     nb = 60
     if abs(historic[0][-1] - historic[0][-2]) > nb or\
@@ -62,6 +68,9 @@ def possibles_movement(historic, movement, possibility):
 
 
 def detections_from_substractor(points_movements, movement):
+    """Recup all movement by substractor, recup min x and y and max width and height
+    It make all the hand"""
+
     points = [[], [], [], []]; x_w = 70; y_h = 100
 
     for i in points_movements:
@@ -81,6 +90,8 @@ def detections_from_substractor(points_movements, movement):
 
 
 def draw(frame, points_movements):
+    """Draw all movement close to the hand"""
+
     if len(points_movements[0]) != 0:
         cv2.rectangle(frame, (min(points_movements[0]), min(points_movements[1])),
                              (max(points_movements[2]), max(points_movements[3])),
@@ -88,6 +99,9 @@ def draw(frame, points_movements):
 
 
 def no_hand_detection(movement, points_movements, historic):
+    """ No hand detection (right or left) We make a detection
+    thank to movement by substractor in comparaison to the last hand detection.
+    Course we define this detection like a hand detection."""
 
     if len(movement) == 1 and len(points_movements[0]) != 0:
 
@@ -98,7 +112,8 @@ def no_hand_detection(movement, points_movements, historic):
         historic[1].append(min(points_movements[1]))
 
 def fusion_movement_detection(movement, points_movements):
-    
+    """Merge movement substractor with hand model detection"""
+
     if len(movement) == 2 and len(points_movements[0]) != 0:
         fusion = [[], [], [], []]
 
@@ -112,6 +127,8 @@ def fusion_movement_detection(movement, points_movements):
         movement[-1] = (min(fusion[0]), min(fusion[1]), max(fusion[2]), max(fusion[3]))
 
 def false_hand_detection(possibility, points_movements, movement):
+    """By history we observ a impossible detection, so we delete it from
+    the movement list and replace it by movement substracor detection"""
 
     if possibility == "impossible" and len(points_movements[0]) != 0:
         movement[-1] = ((min(points_movements[0]), min(points_movements[1]),
@@ -119,12 +136,43 @@ def false_hand_detection(possibility, points_movements, movement):
 
 
 def more_than_one_detection(movement):
+    """Sometime it can be 2 right or 2 left detections. We recup
+    the higther detection from the last hand detection"""
+
+    #ICIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 
     if len(movement) == 3:
         if (movement[0][0] - movement[1][0]) < (movement[0][0] -  movement[2][0]):
             movement[-1] = movement[-2]
 
+
 def detections(movement, points_movements, historic, possibility, frame):
+    """We look history.
+    History compare last hand detection with the new one.
+    We compare x and y. If last and actual are < 60 px we keep it. Else
+    We delete it and send possibility or no possible.
+
+    After we make a substractor of background who's detect all movements
+    all news element on the scene.
+
+    We drawing it to the screen (red rectangles).
+
+    If we havent got any detection, We ask last hand model detection and recup
+    all background movement detection in 100 closed px. We define it like a hand detection.
+
+    No we include hand model detection + movement background around 100 px.
+
+    If we had detected a impossible detection from the hand model we define
+    our hand detection by the backougrnd movement.
+
+    Finally we have more than one detection on a side (2 hand model detection on right for example)
+    so we recuperate the highter x from the last hand model detection.
+
+    We define our hand model detection like the last detection if we got it.
+    Else we define movement backougrnd detection in function of last hand model detection like
+    our last detection.
+
+    """
 
     if len(historic[0]) >= 2 and len(movement) >= 1:
         possibility = possibles_movement(historic, movement, possibility)
@@ -148,15 +196,11 @@ def video_capture(video_name, hand_model):
     video = cv2.VideoCapture(video_name)
     fgbg = cv2.createBackgroundSubtractorMOG2(history=120, detectShadows=False)
 
-    droite = []
-    gauche = []
+    droite = []; gauche = []; hist_droite = [[], []]; hist_gauche = [[], []]
 
-    hist_droite = [[], []]
-    hist_gauche = [[], []]
     while True:
 
-        possible_droite = "possible"
-        possible_gauche = "possible"
+        possible_droite = "possible"; possible_gauche = "possible"
 
         start = start_timmer()
 
