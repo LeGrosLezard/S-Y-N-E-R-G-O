127b8068047a62ec027131c4896bab8923f1ff6c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-09 23:44:11 +0100

    Update video_capture.py

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -53,19 +53,20 @@ def video_capture(video_name, hand_model):
     ok = False
     detection_graph, sess = load_inference_graph(hand_model)
     video = cv2.VideoCapture(video_name)
-    fgbg = cv2.createBackgroundSubtractorMOG2(history=80, varThreshold = 20, detectShadows=False)
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=120, varThreshold = 120, detectShadows=False)
 
 
     while True:
 
         frame = cv2.resize(video.read()[1], (500, 400))
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+
+
         br = fgbg.apply(frame)
-
         back = cv2.bitwise_and(frame, frame, mask = br)
-
-
-        cv2.imshow("back", back)
+        #cv2.imshow("back", back)
 
 
         boxes, scores = detect_objects(frameRGB, detection_graph, sess)
@@ -74,28 +75,20 @@ def video_capture(video_name, hand_model):
         c = 0
         for i in a:
             if len(i) > 0:
-                nb = 15
-
+                nb = 0
 
 
                 roi = frame[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
-                
-##                roiback = back[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
-##                cv2.imshow("roiback", roiback)
-
+                roiback = back[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
 
                 try:
                     min_YCrCb = np.array([0,140,85],np.uint8)
                     max_YCrCb = np.array([240,180,130],np.uint8)
-
-                    imageYCrCb = cv2.cvtColor(roi,cv2.COLOR_BGR2YCR_CB)
+                    imageYCrCb = cv2.cvtColor(roiback,cv2.COLOR_BGR2YCR_CB)
                     skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-
                     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-                    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
-
+                    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 4)
                     skinYCrCb = cv2.bitwise_and(roi, roi, mask = skinMask)
-
                     cv2.imshow("skinYCrCb", skinYCrCb)
 
 
@@ -105,37 +98,25 @@ def video_capture(video_name, hand_model):
 
 
 
-##                imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
-##                for thresh in range(5, 150, 5):
-##
-##
-##                    print(thresh)
-##                    ret, thresh = cv2.threshold(imgray, thresh, 255, 0)
-##                                    
-##                    cv2.imshow("roi", frame)
-##                    cv2.waitKey(0)
 
                 imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
-                ret, thresh = cv2.threshold(imgray, 100, 255, 0)
-                cv2.imshow("thresh", thresh)
+                ret, thresh = cv2.threshold(imgray, 125, 255, 0)
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+                thresh = cv2.erode(thresh, kernel, iterations = 1)
+    
+                #cv2.imshow("thresh", thresh)
 
                 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
                 for i in contours:
-                    print(cv2.contourArea(i))
-                    cv2.drawContours(roi, [i], -1, (0,255,0), 3)
+                    cv2.drawContours(roi, [i], -1, (0,255,0), 4)
                     cv2.fillPoly(roi, pts =[i], color=(77, 200, 9))
-##                    cv2.imshow("roi", frame)
-##                    cv2.waitKey(0)
-
-
-
 
 
 
 
         cv2.imshow("mask", frame)
-        if cv2.waitKey(0) & 0xFF == ord("q"):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
 
