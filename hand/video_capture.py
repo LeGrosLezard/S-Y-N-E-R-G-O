c59d40a5ca6fa8113d7cb36bd43b46e9e22b5821
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-09 20:50:07 +0100

    Update video_capture.py
    c pas super mais ca peut suffire c pas parfait mais bon

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -31,13 +31,29 @@ def hands_detections(scores, boxes, frame):
     return right_left_points
 
 
+def skin_color(hand):
+
+    min_YCrCb = np.array([0,140,85],np.uint8)
+    max_YCrCb = np.array([240,180,130],np.uint8)
+
+    imageYCrCb = cv2.cvtColor(hand,cv2.COLOR_BGR2YCR_CB)
+    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+
+    skinYCrCb = cv2.bitwise_and(hand, hand, mask = skinMask)
+
+    return skinYCrCb
+
+
 
 def video_capture(video_name, hand_model):
 
     ok = False
     detection_graph, sess = load_inference_graph(hand_model)
     video = cv2.VideoCapture(video_name)
-    fgbg = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold = 1, detectShadows=False)
+    fgbg = cv2.createBackgroundSubtractorMOG2(history=80, varThreshold = 20, detectShadows=False)
 
 
     while True:
@@ -62,20 +78,23 @@ def video_capture(video_name, hand_model):
 
 
 
-                roi = back[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
-                cv2.imshow("roi" + str(c), roi)
-
+                roi = frame[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
+                
+##                roiback = back[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
+##                cv2.imshow("roiback", roiback)
 
 
                 try:
-                    min_YCrCb = np.array([0,140,80],np.uint8)
-                    max_YCrCb = np.array([250,180,140],np.uint8)
-
+                    min_YCrCb = np.array([0,140,85],np.uint8)
+                    max_YCrCb = np.array([240,180,130],np.uint8)
 
                     imageYCrCb = cv2.cvtColor(roi,cv2.COLOR_BGR2YCR_CB)
                     skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
-                    skinYCrCb = cv2.bitwise_and(roi, roi, mask = skinRegionYCrCb)
+                    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+                    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+
+                    skinYCrCb = cv2.bitwise_and(roi, roi, mask = skinMask)
 
                     cv2.imshow("skinYCrCb", skinYCrCb)
 
@@ -86,59 +105,28 @@ def video_capture(video_name, hand_model):
 
 
 
+##                imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
+##                for thresh in range(5, 150, 5):
+##
+##
+##                    print(thresh)
+##                    ret, thresh = cv2.threshold(imgray, thresh, 255, 0)
+##                                    
+##                    cv2.imshow("roi", frame)
+##                    cv2.waitKey(0)
 
-##
-##
-##
-##
-##
-##
-##                try:
-##                    imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
-##                    ret, thresh = cv2.threshold(imgray, 100, 255, 0)
-##                    cv2.imshow("roi", thresh)
-##                    cv2.waitKey(1)
-##                except:
-##                    pass
-##
-##
-## 
-####                for thresh in range(5, 255, 5):
-####                    print(thresh)
-####                    ret, thresh = cv2.threshold(imgray, thresh, 255, 0)
-####                    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-####                    print("contour", len(contours))
-####
-####                    cv2.imshow("roi", thresh)
-####                    cv2.waitKey(0)
-##
-##
-####                try:
-####
-####                    cv2.imshow("roi" + str(c), roi)
-####                    cv2.waitKey(0)
-####                        
-####                except:
-####                    pass
-##
-##                c+=1
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
-##
+                imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
+                ret, thresh = cv2.threshold(imgray, 100, 255, 0)
+                cv2.imshow("thresh", thresh)
 
+                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
+                for i in contours:
+                    print(cv2.contourArea(i))
+                    cv2.drawContours(roi, [i], -1, (0,255,0), 3)
+                
+##                    cv2.imshow("roi", frame)
+##                    cv2.waitKey(0)
 
 
 
