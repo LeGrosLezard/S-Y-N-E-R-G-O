66d70c48d2052c72bb073aff3b778bb7d5aeb546
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-12 16:43:27 +0100

    Update video_capture.py
    ok tout réécrire en gros on enleve le modele pour gagner du temps

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -36,9 +36,55 @@ def detection_by_movement(frame, fgbg):
 
     return movement
 
-def detection_by_proximity(movement):
-    pass
 
+def detection_by_proximity(movement, historic):
+
+    out = [[], []]
+
+    if len(historic) > 0:
+        nb = 40
+        if len(historic[0]) > 0:
+            hand_movement_right = [move for move in movement if abs(move[0] - historic[0][0]) < nb and\
+                                                                abs(move[1] - historic[0][1]) < nb]
+            out[0] = hand_movement_right
+        if len(historic[1]) > 0:
+            hand_movement_left = [move for move in movement if abs(move[0] - historic[1][0]) < nb and\
+                                                               abs(move[1] - historic[1][1]) < nb]
+            out[1] = hand_movement_left
+
+    return out[0], out[1]
+
+
+
+def make_movement_detection(hand_movement_right, hand_movement_left, frame, historic):
+
+    points_right = [[], [], [], []]
+    points_left = [[], [], [], []]
+
+    out = [[], []]
+
+    if hand_movement_right != []:
+
+        for i in hand_movement_right:
+            points_right[0].append(i[0])
+            points_right[1].append(i[1])
+            points_right[2].append(i[0] + i[2])
+            points_right[3].append(i[1] + i[3])
+
+        out[0] = min(points_right[0]), min(points_right[1]), max(points_right[2]), max(points_right[3])
+
+    if hand_movement_left != []:
+
+        for i in hand_movement_left:
+            points_left[0].append(i[0])
+            points_left[1].append(i[1])
+            points_left[2].append(i[0] + i[2])
+            points_left[3].append(i[1] + i[3])
+
+
+        out[1] = min(points_left[0]), min(points_left[1]), max(points_left[2]), max(points_left[3])
+
+    return out
 
 
 def video_capture(video_name, hand_model):
@@ -52,7 +98,6 @@ def video_capture(video_name, hand_model):
 
     while True:
 
-        
 
         frame = cv2.resize(video.read()[1], (500, 400))
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -64,11 +109,23 @@ def video_capture(video_name, hand_model):
         for hand in detections:
             if len(hand) > 0:
                 cv2.rectangle(frame, (hand[0], hand[1]), (hand[2], hand[3]), (79, 220, 25), 4)
-            else:
-                print("processus de recherche")
-                detection_by_movement(frame, fgbg)
 
-        historic = [ [int(hand[0] + hand[2] / 2), int(hand[1] + hand[3] / 2)] for hand in detections]
+        movements = detection_by_movement(frame, fgbg)
+        hand_movement_right, hand_movement_left = detection_by_proximity(movements, historic)
+        right_detection, left_detection = make_movement_detection(hand_movement_right, hand_movement_left, frame, historic)
+
+        if right_detection != []:
+            cv2.rectangle(frame, (right_detection[0], right_detection[1]),
+                          (right_detection[2], right_detection[3]), (100, 50, 255), 3)
+
+        if left_detection != []:
+            cv2.rectangle(frame, (left_detection[0], left_detection[1]),
+                          (left_detection[2], left_detection[3]), (100, 50, 255), 3)
+
+
+
+
+        historic = [ [int((hand[0] + hand[2]) / 2), int((hand[1]+hand[3]) / 2)] for hand in detections]
 
     
 
