ceab0cc28498899e8f65f3d5616f1b7394447865
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-12 17:01:17 +0100

    Update video_capture.py

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -24,33 +24,31 @@ def detection_by_movement(frame, fgbg):
     background_substractor = fgbg.apply(frame)
     mask_substractor = cv2.bitwise_and(frame, frame, mask = background_substractor)
 
-    imgray = cv2.cvtColor(mask_substractor, cv2.COLOR_BGR2GRAY)
-    contours, hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    gray = cv2.cvtColor(mask_substractor, cv2.COLOR_BGR2GRAY)
+    contours = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
 
     movement = [cv2.boundingRect(cnts) for cnts in contours]
 
-##    for cnts in contours:
-##        if cv2.contourArea(cnts) > 10:
-##            x, y, w, h = cv2.boundingRect(cnts)
-##            cv2.rectangle(frame, (x,y),(x+w, y+h), (255, 50, 25), 2)
-
     return movement
+
 
 
 def detection_by_proximity(movement, historic):
 
     out = [[], []]
 
+    def define_movement(movement, historic, nb):
+
+        return [move for move in movement if abs(move[0] - historic[nb][0]) < 40 and\
+                                             abs(move[1] - historic[nb][1]) < 40]
+
     if len(historic) > 0:
-        nb = 40
+
         if len(historic[0]) > 0:
-            hand_movement_right = [move for move in movement if abs(move[0] - historic[0][0]) < nb and\
-                                                                abs(move[1] - historic[0][1]) < nb]
-            out[0] = hand_movement_right
+            out[0] = define_movement(movement, historic, 0)
+
         if len(historic[1]) > 0:
-            hand_movement_left = [move for move in movement if abs(move[0] - historic[1][0]) < nb and\
-                                                               abs(move[1] - historic[1][1]) < nb]
-            out[1] = hand_movement_left
+            out[1] = define_movement(movement, historic, 1)
 
     return out[0], out[1]
 
@@ -58,31 +56,26 @@ def detection_by_proximity(movement, historic):
 
 def make_movement_detection(hand_movement_right, hand_movement_left, frame, historic):
 
-    points_right = [[], [], [], []]
-    points_left = [[], [], [], []]
 
     out = [[], []]
 
+    def define_points(movement):
+
+        liste = [[], [], [], []]
+        for i in movement:
+            liste[0].append(i[0])
+            liste[1].append(i[1])
+            liste[2].append(i[0] + i[2])
+            liste[3].append(i[1] + i[3])
+
+        return  min(liste[0]), min(liste[1]), max(liste[2]), max(liste[3])
+
     if hand_movement_right != []:
-
-        for i in hand_movement_right:
-            points_right[0].append(i[0])
-            points_right[1].append(i[1])
-            points_right[2].append(i[0] + i[2])
-            points_right[3].append(i[1] + i[3])
-
-        out[0] = min(points_right[0]), min(points_right[1]), max(points_right[2]), max(points_right[3])
+        out[0] = define_points(hand_movement_right)
 
     if hand_movement_left != []:
+        out[1] = define_points(hand_movement_left)
 
-        for i in hand_movement_left:
-            points_left[0].append(i[0])
-            points_left[1].append(i[1])
-            points_left[2].append(i[0] + i[2])
-            points_left[3].append(i[1] + i[3])
-
-
-        out[1] = min(points_left[0]), min(points_left[1]), max(points_left[2]), max(points_left[3])
 
     return out
 
@@ -98,7 +91,6 @@ def video_capture(video_name, hand_model):
 
     while True:
 
-
         frame = cv2.resize(video.read()[1], (500, 400))
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -109,6 +101,7 @@ def video_capture(video_name, hand_model):
         for hand in detections:
             if len(hand) > 0:
                 cv2.rectangle(frame, (hand[0], hand[1]), (hand[2], hand[3]), (79, 220, 25), 4)
+
 
         movements = detection_by_movement(frame, fgbg)
         hand_movement_right, hand_movement_left = detection_by_proximity(movements, historic)
@@ -122,12 +115,14 @@ def video_capture(video_name, hand_model):
             cv2.rectangle(frame, (left_detection[0], left_detection[1]),
                           (left_detection[2], left_detection[3]), (100, 50, 255), 3)
 
-
-
-
         historic = [ [int((hand[0] + hand[2]) / 2), int((hand[1]+hand[3]) / 2)] for hand in detections]
 
     
+
+
+
+
+
 
 
 
