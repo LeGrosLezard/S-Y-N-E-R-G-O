c8149a4dec0a2d1a7346bc739f6fcecd1b9c9238
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-12 04:17:55 +0100

    Update video_capture.py

--- hand/video_capture.py
+++ hand/video_capture.py
@@ -1,10 +1,7 @@
-import cv2
 import tensorflow as tf
-import numpy as np
-from time import time
-import imutils
 from utils import load_inference_graph, detect_objects
-
+import numpy as np
+import cv2
 
 print(tf.version.VERSION)
 
@@ -14,106 +11,74 @@ def hands_detections(scores, boxes, frame):
     """We detect hands, by position we define if it are to left or to right.
     Recup it for history if we have impossible detections"""
 
-    for i in range(2):
-        if (scores[i] > 0.10):
-            (left, right, top, bottom) = (boxes[i][1] * 500, boxes[i][3] * 500,
-                                          boxes[i][0] * 400, boxes[i][2] * 400)
-            p1 = (int(left), int(top))
-            p2 = (int(right), int(bottom))
-            #cv2.rectangle(frame, p1, p2, (0, 0, 0), 4)
+    width = 500; height = 400
+    detections = [(int(boxes[i][1] * width), int(boxes[i][0] * height),
+                   int(boxes[i][3] * width), int(boxes[i][2] * height))
+                   for i in range(2) if (scores[i] > 0.10)]
 
-    detected_points = [(int(boxes[i][1] * 500), int(boxes[i][3] * 500),
-                        int(boxes[i][0] * 400), int(boxes[i][2] * 400)) for i in range(2) if scores[i] > 0.10]
-
-    right_left_points = [  [(pt[0], pt[2], pt[1], pt[3]) for pt in detected_points if pt[0] < 250],
-                           [(pt[0], pt[2], pt[1], pt[3]) for pt in detected_points if pt[0] > 250]   ]
-    print(right_left_points)
-    return right_left_points
+    return detections
 
 
-def skin_color(hand):
+def detection_by_movement(frame, fgbg):
 
-    min_YCrCb = np.array([0,140,85],np.uint8)
-    max_YCrCb = np.array([240,180,130],np.uint8)
+    background_substractor = fgbg.apply(frame)
+    mask_substractor = cv2.bitwise_and(frame, frame, mask = background_substractor)
 
-    imageYCrCb = cv2.cvtColor(hand,cv2.COLOR_BGR2YCR_CB)
-    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+    imgray = cv2.cvtColor(mask_substractor, cv2.COLOR_BGR2GRAY)
+    contours, hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+    movement = [cv2.boundingRect(cnts) for cnts in contours]
 
-    skinYCrCb = cv2.bitwise_and(hand, hand, mask = skinMask)
+##    for cnts in contours:
+##        if cv2.contourArea(cnts) > 10:
+##            x, y, w, h = cv2.boundingRect(cnts)
+##            cv2.rectangle(frame, (x,y),(x+w, y+h), (255, 50, 25), 2)
 
-    return skinYCrCb
+    return movement
+
+def detection_by_proximity(movement):
+    pass
 
 
 
 def video_capture(video_name, hand_model):
 
-    ok = False
     detection_graph, sess = load_inference_graph(hand_model)
+
     video = cv2.VideoCapture(video_name)
     fgbg = cv2.createBackgroundSubtractorMOG2(history=120, varThreshold = 120, detectShadows=False)
 
+    historic = []
 
     while True:
+
+        
 
         frame = cv2.resize(video.read()[1], (500, 400))
         frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-
-
-
-        br = fgbg.apply(frame)
-        back = cv2.bitwise_and(frame, frame, mask = br)
-        #cv2.imshow("back", back)
-
-
         boxes, scores = detect_objects(frameRGB, detection_graph, sess)
-        a = hands_detections(scores, boxes, frame)
-
-        c = 0
-        for i in a:
-            if len(i) > 0:
-                nb = 25
+        detections = hands_detections(scores, boxes, frame)
 
 
-                roi = frame[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
-                roiback = back[ i[0][1]-nb : i[0][3]+nb, i[0][0]-nb : i[0][2]+nb ]
+        for hand in detections:
+            if len(hand) > 0:
+                cv2.rectangle(frame, (hand[0], hand[1]), (hand[2], hand[3]), (79, 220, 25), 4)
+            else:
+                print("processus de recherche")
+                detection_by_movement(frame, fgbg)
 
-                try:
-                    min_YCrCb = np.array([0,140,85],np.uint8)
-                    max_YCrCb = np.array([240,180,130],np.uint8)
-                    imageYCrCb = cv2.cvtColor(roiback,cv2.COLOR_BGR2YCR_CB)
-                    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
-                    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
-                    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 3)
-                    skinYCrCb = cv2.bitwise_and(roi, roi, mask = skinMask)
-                    #cv2.imshow("skinYCrCb", skinYCrCb)
+        historic = [ [int(hand[0] + hand[2] / 2), int(hand[1] + hand[3] / 2)] for hand in detections]
+
+    
 
 
-                except:
-                    pass
 
-
-                imgray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
-                ret, thresh = cv2.threshold(imgray, 100, 255, 0)
-
-                cv2.imshow("thresh", thresh)
-
-
-                contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-                for i in contours:
-                    if cv2.contourArea(i) > 100:
-                        cv2.drawContours(roi, [i], -1, (0,255,0), 3)
-                        cv2.fillPoly(roi, pts =[i], color=(77, 200, 9))
-
-        cv2.imshow("mask", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(0) & 0xFF == ord("q"):
             break
 
-
+        frame += 1
 
     video.release()
     cv2.destroyAllWindows()
