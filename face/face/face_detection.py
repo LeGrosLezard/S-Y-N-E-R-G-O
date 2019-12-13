e6efb16e3306769c82da2259295ac795d26586da
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-13 12:33:02 +0100

    Update face_detection.py

--- face/face/face_detection.py
+++ face/face/face_detection.py
@@ -1,241 +1,197 @@
-from cv2 import rectangle, convexHull, Subdiv2D, boundingRect, resize, putText, countNonZero, FONT_HERSHEY_SIMPLEX
-from numpy import array, int32, expand_dims
-from numpy import min as np_min
-from numpy import max as np_max
-from math import sin, acos
-from scipy.spatial import distance as dist
-from keras.preprocessing.image import img_to_array
-from keras.models import load_model
-
-def points_landmarks(gray, predictor, detector):
-    """ Return the 68 points of the face and the face"""
-
-    face = detector(gray)
-    return predictor(gray, face[0]), face
+from cv2 import bitwise_and, polylines, fillConvexPoly, imshow, imread, resize, cvtColor, COLOR_BGR2GRAY, line
+from numpy import zeros_like, hstack, empty, uint8
 
 
+#si touché -> c pas dans display
 
-#---------------------------------------------------------------------------------------------------- Intra face
-#Interior of face
-def recuperate_intra_face_points(landmarks, faces, img):
-    """Recuperate all coordiantes of landmarks (faces points).
-    Recuperate convex points (exterior of face) and triangle
-    points (area of the interior of the face)."""
-
-    #points of face
-    points = [(landmarks.part(n).x, landmarks.part(n).y) for pts in faces for n in range(0, 68)]
-
-    #Convex points (contour of face)
-    convexhull = convexHull(array(points))
-
-    head = boundingRect(convexhull)
-
-    #Find triangles into rectangles points (face)
-    subdiv = Subdiv2D(head)
-
-    #Put points for extract the list
-    subdiv.insert(points)
-
-    #Get triangles list
-    triangles = array(subdiv.getTriangleList(), dtype=int32)
-
-    #Recup points
-    t_points = [[(t[0], t[1]), (t[2], t[3]), (t[4], t[5])] for t in triangles ]
-
-    return t_points, head, convexhull
+#mettre la zone en bleu
+#définir un nom de zone (ex: joue par droite + nombre de frame a la fin)
+#l'afficher
 
 
-def intra_face(img, gray, landmarks, face, fgbg):
+def displaying(gray, img, copy, convexhull):
 
-    #A CONTINUER
+    head_model = imread("display/head_model.jpg")
+    head_model = resize(head_model, (int(gray.shape[1]/2), gray.shape[0]))
 
-    subastrac = fgbg.apply(img)
+    mask = zeros_like(gray)
 
-    def crop_rectangle(area, color):
-        area = array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in range(area[0], area[1])])
-        x, y, w, h = boundingRect(area)
-        #cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
-        return x - 5, y - 5, w + 5, h + 10
+    polylines(copy, [convexhull], True, (0, 0, 255), 1)
+    fillConvexPoly(mask, convexhull, 255)
 
-    def crop_contour(area, color):
-        area = array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in area])
-        x, y, w, h = boundingRect(area)
-        #cv2.drawContours(img, [area], 0, color, 1)
-        return x , y , w, h
+    face = bitwise_and(copy, copy, mask=mask)
 
-    def counter(area):
-        a = countNonZero(area)
-        #print(a)
+    displaying = hstack((head_model, img))
+    displaying = hstack((displaying, face))
+
+    imshow('Display', displaying)
 
 
-    areas =  { "beet_eyes" :[21, 22, 27], "chin":[58, 56, 9, 7],
-               "chin1":[58, 7, 3, 48],  "chin2": [56, 54, 13, 9],
-               "cheek1": [48, 3, 0, 28], "cheek2":[54, 13, 16, 28],
-               "noze_area":[27, 48, 54], "mouse":(48, 61),
-               "onEye1":(17, 22), "onEye2":(22, 27),
-               "leftEye":(36, 42), "rightEye":(42, 48) }
+def recuperate_face(convexhull, gray, img, points):
+    """We displaying the frame, the face
+    from the frame"""
+
+    copy = img.copy()
+
+    for pts in points:
+        pts1 = pts[0]; pts2 = pts[1]; pts3 = pts[2]
+
+        line(copy, pts1, pts2, (0, 0, 255), 1)
+        line(copy, pts2, pts3, (0, 0, 255), 1)
+        line(copy, pts1, pts3, (0, 0, 255), 1)
+
+    displaying(gray, img, copy, convexhull)
 
 
-    area_by_contour = [crop_contour(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb <= 6]
-    area_by_rect = [crop_rectangle(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb > 6]
 
-##    for area, cnts in zip(areas, area_by_contour):
-##        print(area, cnts)
+
+
+
+##def make_rectangle(area, color):
+##    area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in range(area[0], area[1])])
+##    x, y, w, h = boundingRect(area)
+##    cv2.rectangle(img, (x, y), (x + w, y + h), color, 1) 
+##    return x, y, w, h
 ##
-##    for area, cnts in zip(areas, area_by_rect):
-##        print(area, cnts)
-
-
-
-
-def emotions_model(frame, gray, faces, emotion_model):
-
-    emotion_classifier = load_model(emotion_model, compile=False)
-    EMOTIONS = ["angry", "disgust", "scared", "happy", "sad", "surprised", "neutral"]
-    
-    if faces:
-
-        faces = sorted([list(faces)], reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-        (fX, fY, fW, fH) = faces
-        roi = gray[fY:fY + fH, fX:fX + fW]
-        roi = resize(roi, (64, 64))
-        roi = roi.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = expand_dims(roi, axis=0)
-        preds = emotion_classifier.predict(roi)[0]
-        emotion_probability = np_max(preds)
-        label = EMOTIONS[preds.argmax()]
-
-        putText(frame, label, (fX, fY - 10), FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-        rectangle(frame, (fX, fY), (fX + fW, fY + fH),(0, 0, 255), 2)
-
-
-
-def emotion_points(img, landmarks):
-
-
-    anatomy_y =  {"open_mouse_points_top" :[62, 63, 64], "open_mouse_points_bot":[58, 57, 66],
-                  "top_eyes_left": [38, 39], "bot_eyes_left":[42, 41],
-                  "top_eyes_right":[44, 45], "bot_eyes_right":[48, 47],
-                  "on_eye_left":[23, 24, 25, 26, 27], "on_eye_right":[18, 19, 20, 21, 22]}
-
-    anatomy_x = {"right_side_mouse": [49], "left_side_mouse": [55], "nose":[32, 36]}
-
-    def coordinates(coordinate, axis):
-        #(landmarks.part(i).x), (landmarks.part(i).y) ICI CIRCLE POUR VERIGIER
-        if axis == "x": out = [(landmarks.part(i).x) for i in coordinate]
-        else: out = [(landmarks.part(i).y) for i in coordinate]
-        return out
-
-    dico_points = {}
-
-
-    for (k1, v1), (k2, v2) in zip(anatomy_y.items(), anatomy_x.items()):
-        a = coordinates(v1, "y")
-        b = coordinates(v2, "x")
-
-        dico_points[k1] = a
-        dico_points[k2] = b
-
-
-    print(dico_points)
+##def make_contour(area, color):
+##    area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in area])
+##    cv2.drawContours(img, [area], 0, color, 1)
+##
+##
+##areas =  { "beet_eyes" :[21, 22, 27], "chin":[58, 56, 9, 7], "chin1":[58, 7, 3, 48],
+##           "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28],
+##           "cheek2":[54, 13, 16, 28], "noze_area":[27, 48, 54], "mouse":(48, 61),
+##           "onEye1":(17, 22), "onEye2":(22, 27), "leftEye":(36, 42), "rightEye":(42, 48)}
+##
+##[make_contour(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb <= 6]
+##[make_rectangle(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb > 6]
+##
 
 
 
 
 
+##def intra_face(img, gray, landmarks, face, fgbg):
+##
+##    #A CONTINUER
+##    import cv2
+##    import numpy as np
+##
+##    subastrac = fgbg.apply(img)
+##
+##    def crop_rectangle(area, color):
+##        area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in range(area[0], area[1])])
+##        x, y, w, h = boundingRect(area)
+##        #cv2.rectangle(img, (x, y), (x + w, y + h), color, 1)
+##        return x - 5, y - 5, w + 5, h + 10
+##
+##    def crop_contour(area, color):
+##        area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for pts in face for n in area])
+##        x, y, w, h = boundingRect(area)
+##        #cv2.drawContours(img, [area], 0, color, 1)
+##        return x , y , w, h
+##
+##
+##    def counter(area):
+##        a = cv2.countNonZero(area)
+##        #print(a)
+##
+##
+##    areas =  { "beet_eyes" :[21, 22, 27], "chin":[58, 56, 9, 7],
+##               "chin1":[58, 7, 3, 48],  "chin2": [56, 54, 13, 9],
+##               "cheek1": [48, 3, 0, 28], "cheek2":[54, 13, 16, 28],
+##               "noze_area":[27, 48, 54], "mouse":(48, 61),
+##               "onEye1":(17, 22), "onEye2":(22, 27),
+##               "leftEye":(36, 42), "rightEye":(42, 48) }
+##
+##
+##    b = [crop_contour(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb <= 6]
+##    a = [crop_rectangle(areas[k], (0,255,0)) for nb, k in enumerate(areas) if nb > 6]
+##    d = [cv2.resize(img[i[1]:i[1] + i[3], i[0]:i[0] + i[2]], (100, 50)) for i in a + b]
+##
+##    e = [cv2.resize(subastrac[i[1]:i[1] + i[3], i[0]:i[0] + i[2]], (100, 50)) for i in a + b]
+##
+##
+##
+##    numpy_horizontal_concat = np.concatenate( (d[0], d[1]), axis=1 )
+##    numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, d[2]), axis=1 )
+##    numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, d[3]), axis=1 )
+##    numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, d[4]), axis=1 )
+##    numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, d[5]), axis=1 )
+##
+##    numpy_horizontal_concat2 = np.concatenate( (d[7], d[9]), axis=1 )
+##    numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2,  d[8]), axis=1 )
+##    numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2, d[10]), axis=1 )
+##    numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2, d[6]), axis=1 )
+##    numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2, d[11]), axis=1 )
+##
+##    numpy_vertical = np.vstack( (numpy_horizontal_concat, numpy_horizontal_concat2) )
+##
+##
+##    counter(e[0])
+##
+##    numpy_horizontal_concat3 = np.concatenate( (e[0], e[1]), axis=1 )
+##    numpy_horizontal_concat3 = np.concatenate( (numpy_horizontal_concat3, e[2]), axis=1 )
+##    numpy_horizontal_concat3 = np.concatenate( (numpy_horizontal_concat3, e[3]), axis=1 )
+##    numpy_horizontal_concat3 = np.concatenate( (numpy_horizontal_concat3, e[4]), axis=1 )
+##    numpy_horizontal_concat3 = np.concatenate( (numpy_horizontal_concat3, e[5]), axis=1 )
+##
+##    numpy_horizontal_concat4 = np.concatenate( (e[7], e[9]), axis=1 )
+##    numpy_horizontal_concat4 = np.concatenate( (numpy_horizontal_concat4,  e[8]), axis=1 )
+##    numpy_horizontal_concat4 = np.concatenate( (numpy_horizontal_concat4, e[10]), axis=1 )
+##    numpy_horizontal_concat4 = np.concatenate( (numpy_horizontal_concat4, e[6]), axis=1 )
+##    numpy_horizontal_concat4 = np.concatenate( (numpy_horizontal_concat4, e[11]), axis=1 )
+##
+##    numpy_vertical11 = np.vstack( (numpy_horizontal_concat3, numpy_horizontal_concat4) )
+##
 
 
-#---------------------------------------------------------------------------------------------------- Intra face
+##    cv2.imshow("numpy_vertical", numpy_vertical)
+##    cv2.imshow("numpy_vertical11", numpy_vertical11)
+##    cv2.waitKey(0)
 
 
-
-
-
-
-
-#Exterior of face
-def exterior_face(face, img):
-
-    """Recuperate area of the forehead and of the exterior of the head"""
-
-    #front, FAIRE RATIO
-    rectangle(img, (face[0], face[1] - 25), (face[0] + face[2], face[1]), 3)
-
-    #Haut tete
-    rectangle(img, (face[0], face[1] - 70), (face[0] + face[2], face[1] - 20), 3)
-
-    #cou
-    rectangle(img, (face[0], face[3] + face[1]), (face[0] + face[2], face[3] + face[1] + 40), 3)
-
-    #chevelure (effacer la tronche recupérer le pourtour ?)
-    #rectangle(img, (face[0] - 50, face[1] - 100), (face[0] + face[2] + 50, face[1] + face[3]), 3)
-
-##    cv2.imshow("img", img)
+##    images = [Image.fromarray(name) for name in d]
+##    somme=[]
+##    for img in images:
+##        somme+=list((np.asarray(img)))
+##
+##    cv2.imshow("img", np.array(somme))
 ##    cv2.waitKey(0)
 
 
 
 
+##def emotion_points(img, landmarks):
+##
+##    import cv2
+##
+##    anatomy_y =  {"open_mouse_points_top" :[61, 62, 63], "open_mouse_points_bot":[67, 66, 65],
+##                  "top_eyes_left": [37, 38], "bot_eyes_left":[41, 40],
+##                  "top_eyes_right":[43, 44], "bot_eyes_right":[47, 46],
+##                  "on_eye_right":[17, 18, 19, 20, 21], "on_eye_left":[22, 23, 24, 25, 26]}
+##
+##    anatomy_x = {"right_side_mouse": [48], "left_side_mouse": [54], "nose":[31, 35]}
+##
+##
+##    def coordinates(coordinate, axis):
+##
+##        if axis == "x": out = [(landmarks.part(i).x, landmarks.part(i).y) for i in coordinate]
+##        else: out = [(landmarks.part(i).x, landmarks.part(i).y) for i in coordinate]
+##        return out
+##
+##    dico_points = {}
+##
+##    for k1, v1 in anatomy_y.items():
+##        a = coordinates(v1, "y")
+##        dico_points[k1] = a
+##
+##    for k1, v1 in anatomy_x.items():
+##        b = coordinates(v1, "x")
+##        dico_points[k1] = b
+##
+##
+##    for k, v in dico_points.items():
+##        for i in v:
+##            cv2.circle(img, (i[0], i[1]), 1, (0, 255, 0), 1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#---------------------------------------------------------------------------------------------------- inclinaison
-
-def inclinaison(landmarks, img):
-    """Analyse angles of the triangle beetween eyes and nose points"""
-
-    #Recuperate landmarks.
-    a = landmarks.part(36).x, landmarks.part(36).y
-    b = landmarks.part(45).x, landmarks.part(45).y
-    c = landmarks.part(30).x, landmarks.part(30).y
-
-    #Recuperate distances.
-    d_eyes = dist.euclidean(a, b) 
-    d1 = dist.euclidean(a, c) 
-    d2 = dist.euclidean(b, c) 
-
-    coeff = d1 + d2
-
-    #Calculus angle.
-    cosb = np_min( (pow(d2, 2) - pow(d1, 2) + pow(d_eyes, 2) ) / (2*d2*d_eyes) )
-    a1 = int(250*(d1-d2)/coeff)
-    a2 = int(250*(d2*sin(acos(cosb))-coeff/4)/coeff)
-    a3 = int(250*(a[1]-b[1])/coeff)
-    head = ""
-
-    #Display it
-    if a1 < - 20: head += "a droite "
-    elif a1 > 20: head += "a gauche "
-
-    if a2 < 15: head += "en haut "
-    elif a2 < 0: head += "tres haut "
-    elif a2 > 30: head += "en bas "
-    elif a2 > 40: head += "tres bas "
-
-    if a3 < - 20: head += "et incline la tete a gauche "
-    elif a3 > 20: head += "et incline la tete a droite "
-
-    #print(head)
-    return head, a1, a2
-#---------------------------------------------------------------------------------------------------- inclinaison
