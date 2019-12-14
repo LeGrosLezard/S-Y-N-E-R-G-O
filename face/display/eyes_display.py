16909078e3b3c08a2184a615e7790e05c30b7c7a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-14 20:16:57 +0100

    Update eyes_display.py

--- face/display/eyes_display.py
+++ face/display/eyes_display.py
@@ -1,83 +1,79 @@
 import cv2
 from numpy import array, hstack, zeros_like
 
-def eyes_animations(frame, eye):
 
-    #Crop
-    x, y, w, h = cv2.boundingRect(eye)
-    eye = frame[y-10:y+h, x:x+w]
+def work_on_eye_picture(points, frame):
 
-    #Resize
-    height, width = eye.shape[:2]
-    eye = cv2.resize(eye, (width*10, height*8))
+    def eyes_crop(eye, frame):
+        x, y, w, h = cv2.boundingRect(eye)
+        return frame[y-10:y+h, x:x+w], x, y, w, h
 
-    #Make border
-    height_frame, width_frame = frame.shape[:2]
+    def resizing(crop):
+        height, width = crop.shape[:2]
+        return cv2.resize(crop, (width*10, height*8))
 
-    #Take 1/2 of frame, crop dimensions
-    h_diff = int((height_frame - height) / 2)
-    w_diff = int((width_frame - width) / 2)
 
-    #Make border
-    eye = cv2.copyMakeBorder(eye, top=h_diff,bottom=h_diff,left=w_diff,right=w_diff,
-                             borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    def bordering(crop, border_height, border_width):
+      
+        return cv2.copyMakeBorder(crop, top=border_height,bottom=border_height,
+                                 left=border_width,right=border_width,
+                                 borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0])
 
-    return eye, h_diff, w_diff, x, y, w, h
+    def difference_dimension(frame, crop):
+        height, width = crop.shape[:2]
+        height_frame, width_frame = frame.shape[:2]
+
+        return int((height_frame - height) / 2), int((width_frame - width) / 2)
+
+
+    crop, x, y, w, h = eyes_crop(points, frame)
+    crop = resizing(crop)
+    border_height, border_width = difference_dimension(frame, crop)
+    crop = bordering(crop, border_height, border_width)
+
+    return crop, border_height, border_width, x, y, w, h
+
+
 
 
 def animations(h, w, x1, y1, w1, h1, eye, eye_display):
+    """Eye display is the last movements from the eyes of personn"""
 
-    dr = 0
-    ga = 0
-    ha = 0
-    ba = 0
     movement = []
+    dico_movement = {"right":0, "left":0, "top":0, "bot":0}
+
     if eye_display != []:
         for i in eye_display:
-            if i == "droite":
-                dr += 1
-            if i == "gauche":
-                ga += 1
+            for k, v in dico_movement.items():
+                if i == k:
+                    dico_movement[k] += 1
 
-            if i == "haut":
-                ha += 1
-
-            if i == "bas":
-                ba += 1
-
-    if dr > 0 and ha > 0:
-        movement.append("droite haut")
-
-    elif dr > 0 and ba > 0:
-        movement.append("droite bas")
-
-    elif ga > 0 and ha > 0:
-        movement.append("gauche haut")
-
-    elif ga > 0 and ha > 0:
-        movement.append("gauche bas")
-
-    elif dr > 0:
-        movement.append("droite")
-
-    elif ga > 0:
-        movement.append("gauche")
+    for k, v in dico_movement.items():
+        if dico_movement["right"] > 0 and dico_movement["top"] > 0: movement.append("droite haut")
+        elif dico_movement["right"] > 0 and dico_movement["bot"] > 0: movement.append("droite bas")
+        elif dico_movement["left"] > 0 and dico_movement["top"] > 0: movement.append("gauche haut")
+        elif dico_movement["left"] > 0 and dico_movement["bot"] > 0: movement.append("gauche bas")
+        elif dico_movement["right"] > 0: movement.append("droite")
+        elif dico_movement["left"] > 0: movement.append("gauche")
 
 
+    corner_top = int(((y1+h1)) + h - 90)
+    center = int(((y1+h1)) + h - 30)
+    corner_bot = int(((y1+h1)) + h + 30)
 
 
+    #(x, y) frame, end line, arrow line1, arrow line2
+    moves = {"droite":[(w, center), (w - 200, center), (w - 200 + 30, center + 30), (w - 200 + 30, center - 30)],
+             "gauche":[(w + 140, center), (w + 340, center), (w + 340 - 30, center - 30), (w + 340 - 30, center + 30)],
+             "droite haut":[(w, corner_top), (w - 100, corner_top - 100), (w - 100, corner_top - 100 + 30),
+                            (w - 100 + 30, corner_top - 100)],
+             "droite bas":[(w, corner_bot), (w - 100, corner_bot + 100),(w - 100 + 30, corner_bot + 100),
+                           (w - 100, corner_bot + 100 - 30)],
+             "gauche haut":[(w + 140, corner_top),(w + 240, corner_top - 100),(w + 240, corner_top - 100 + 30),
+                            (w + 240 - 30, corner_top - 100)],
+             "gauche bas":[(w + 140, corner_bot), (w + 240, corner_bot + 100), (w + 240 - 30, corner_bot + 100),
+                           (w + 240, corner_bot + 100 - 30)]}
 
-    aa = int(((y1+h1)) + h - 90)
-    a = int(((y1+h1)) + h - 30)
-    aaa = int(((y1+h1)) + h + 30)
-
-
-    moves = {"droite":[(w, a), (w - 200, a), (w - 200 + 30, a + 30), (w - 200 + 30, a - 30)],
-             "droite haut":[(w, aa), (w - 100, aa - 100), (w - 100, aa - 100 + 30), (w - 100 + 30, aa - 100)],
-             "droite bas":[(w, aaa), (w - 100, aaa + 100), (w - 100 + 30, aaa + 100), (w - 100, aaa + 100 - 30)],
-             "gauche":[(w + 140, a), (w + 340, a), (w + 340 - 30, a - 30), (w + 340 - 30, a + 30)],
-             "gauche haut":[(w + 140, aa), (w + 240, aa - 100), (w + 240, aa - 100 + 30), (w + 240 - 30, aa - 100)],
-             "gauche bas":[(w + 140, aaa), (w + 240, aaa + 100), (w + 240 - 30, aaa + 100), (w + 240, aaa + 100 - 30)]}
 
     watch = ""
     for i in movement:
@@ -86,11 +82,8 @@ def animations(h, w, x1, y1, w1, h1, eye, eye_display):
                 cv2.line(eye, (v[0][0], v[0][1]), (v[1][0], v[1][1]), (0, 0, 255), 3)
                 cv2.line(eye, (v[1][0], v[1][1]), (v[2][0], v[2][1]), (0, 0, 255), 3)
                 cv2.line(eye, (v[1][0], v[1][1]), (v[3][0], v[3][1]), (0, 0, 255), 3)
-
                 watch += k
-    
 
-    
     if watch == "":
         watch = "center"
 
@@ -110,17 +103,34 @@ def eyes_display(frame, gray, landmarks, eyes_movements, eye_display, counter_fr
     if eye_display != [] and eyes_movements == None:
         raising = True
 
-    
 
     right_eye_points = cv2.convexHull(array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(36, 42)]))
     left_eye_points = cv2.convexHull(array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(42, 48)]))
 
+    right_crop, _, _, _, _, _,_  = work_on_eye_picture(right_eye_points, frame)
+    left_crop, border_height, border_width, x, y, w, h = work_on_eye_picture(left_eye_points, frame)
 
-    right_eye, h, w, x1, y1, w1, h1 = eyes_animations(frame, right_eye_points)
-    right_eye, watch = animations(h, w, x1, y1, w1, h1, right_eye, eye_display)
+    right_eye, watch = animations(border_height, border_width, x, y, w, h, right_crop, eye_display)
+    left_eye, watch = animations(border_height, border_width, x, y, w, h, left_crop, eye_display)
 
-    left_eye, h, w, x1, y1, w1, h1 = eyes_animations(frame, left_eye_points)
-    left_eye, watch = animations(h, w, x1, y1, w1, h1, left_eye, eye_display)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     import numpy as np
@@ -151,15 +161,8 @@ def eyes_display(frame, gray, landmarks, eyes_movements, eye_display, counter_fr
         y += 50
 
     displaying1 = hstack((image, mask))
-
-
-    
-
     displaying = hstack((right_eye, frame))
     displaying = hstack((displaying, left_eye))
-
-
-
 
     numpy_horizontal_concat = np.vstack( (displaying1, displaying) )
 
