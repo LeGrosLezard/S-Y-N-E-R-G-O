005f35e2ffffbcb5c0e29bec07620d42a22ab4ce
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-15 01:12:35 +0100

    Update face_display.py
    faire le truk des region mtn

--- face/display/face_display.py
+++ face/display/face_display.py
@@ -4,16 +4,17 @@ from PIL import Image
 import cv2
 import numpy as np
 
-
+#=============================================================================================================  Face focus
 def mask_head(gray, img, copy, convexhull):
     mask = zeros_like(gray)
     polylines(copy, [convexhull], True, (0, 0, 255), 1)
     fillConvexPoly(mask, convexhull, 255)
-    face = bitwise_and(copy, copy, mask=mask)
 
-    return face
+    return bitwise_and(copy, copy, mask=mask)
 
 
+
+#=============================================================================================================  Face draw
 def make_rectangle(area, color, img, landmarks):
     area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(area[0], area[1])])
     x, y, w, h = boundingRect(area)
@@ -23,6 +24,9 @@ def make_contour(area, color, img, landmarks):
     area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in area])
     cv2.drawContours(img, [area], 0, color, 1)
 
+
+
+#=============================================================================================================  Recup area face
 def crop_rectangle(area, color, landmarks):
     area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(area[0], area[1])])
     x, y, w, h = boundingRect(area)
@@ -35,10 +39,13 @@ def crop_contour(area, color, landmarks):
 def concatenate_areas(area):
 
     numpy_horizontal_concat = np.concatenate( (area[0], area[1]), axis=1 )
-    numpy_horizontal_concat2 = np.concatenate( (area[7], area[9]), axis=1 )
+    numpy_horizontal_concat2 = np.concatenate( (area[6], area[7]), axis=1 )
 
-    for i in range(2, 6): numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, area[i]), axis=1 )
-    for i in range(8, 12): numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2,  area[i]), axis=1 )
+    for i in range(2, 6):
+        numpy_horizontal_concat = np.concatenate( (numpy_horizontal_concat, area[i]), axis=1 )
+
+    for i in range(8, 12):
+        numpy_horizontal_concat2 = np.concatenate( (numpy_horizontal_concat2,  area[i]), axis=1 )
 
     return np.vstack( (numpy_horizontal_concat, numpy_horizontal_concat2) )
 
@@ -46,35 +53,42 @@ def concatenate_areas(area):
 def make_border(img, areas):
     return copyMakeBorder(areas, top=0,bottom=0, left=0,right=540, borderType=BORDER_CONSTANT, value=[0, 0, 0])
 
+
 def intra_face_displaying(img, gray, landmarks, face, fgbg, displaying):
 
+    areas =  { "cheek2":[54, 13, 16, 28], "chin":[58, 56, 9, 7], "beet_eyes" :[21, 22, 27], "chin1":[58, 7, 3, 48],
+               "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28], "noze_area":[27, 48, 54],
+               "mouse":(48, 61), "onEye1":(17, 22), "onEye2":(22, 27), "leftEye":(36, 42), "rightEye":(42, 48)}
 
-    areas =  { "beet_eyes" :[21, 22, 27], "chin":[58, 56, 9, 7], "chin1":[58, 7, 3, 48],  "chin2": [56, 54, 13, 9],
-               "cheek1": [48, 3, 0, 28], "cheek2":[54, 13, 16, 28], "noze_area":[27, 48, 54], "mouse":(48, 61),
-               "onEye1":(17, 22), "onEye2":(22, 27), "leftEye":(36, 42), "rightEye":(42, 48) }
 
     area_by_contour = [crop_contour(areas[k], (0,255,0), landmarks) for nb, k in enumerate(areas) if nb <= 6]
     area_by_rectangle = [crop_rectangle(areas[k], (0,255,0), landmarks) for nb, k in enumerate(areas) if nb > 6]
-    area = [cv2.resize(img[i[1]:i[1] + i[3], i[0]:i[0] + i[2]], (100, 50)) for i in area_by_contour + area_by_rectangle]
+    all_area = [cv2.resize(img[i[1]:i[1] + i[3], i[0]:i[0] + i[2]], (100, 50)) for i in area_by_contour + area_by_rectangle]
 
-    areas = concatenate_areas(area)
+    areas = concatenate_areas(all_area)
     areas = make_border(img, areas)
 
     return areas
 
 
+
+#============================================================================================================= Main
 def face_displaying(gray, img, convexhull, head_points, landmarks, fgbg):
 
     copy = img.copy()
-    areas =  { "beet_eyes" :[21, 22, 27], "chin":[58, 56, 9, 7], "chin1":[58, 7, 3, 48],
-               "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28],
-               "cheek2":[54, 13, 16, 28], "noze_area":[27, 48, 54], "mouse":(48, 61),
-               "onEye1":(17, 22), "onEye2":(22, 27), "leftEye":(36, 42), "rightEye":(42, 48)}
+    areas =  { "chin":[58, 56, 9, 7], "beet_eyes" :[21, 22, 27], "chin1":[58, 7, 3, 48],
+               "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28], "noze_area":[27, 48, 54],
+               "cheek2":[54, 13, 16, 28], "mouse":(48, 61), "onEye1":(17, 22), "onEye2":(22, 27),
+               "leftEye":(36, 42), "rightEye":(42, 48)}
 
     [make_contour(areas[k], (0,0,255), copy, landmarks) for nb, k in enumerate(areas) if nb <= 6]
     [make_rectangle(areas[k], (0,255,0), copy, landmarks) for nb, k in enumerate(areas) if nb > 6]
 
     face = mask_head(gray, img, copy, convexhull)
+
+
+    #DICO IMAGE DU MEC EN BLEU
+    
     image = imread(r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\face\display\face_pictures\head_model.jpg")
     head_model = resize(image, (int(gray.shape[1]/2), gray.shape[0]))
 
@@ -86,7 +100,6 @@ def face_displaying(gray, img, convexhull, head_points, landmarks, fgbg):
     areas = cv2.resize(areas, (int(displaying.shape[1]), int(areas.shape[0])) )
 
     horizontal_concat = vstack( (areas, displaying) )
-
 
 
     cv2.imshow("horizontal_concat", horizontal_concat)
