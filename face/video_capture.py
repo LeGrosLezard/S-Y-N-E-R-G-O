ca8feb43f4adbe1178da0211ae03061c8771f045
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-15 01:13:15 +0100

    Update video_capture.py

--- face/video_capture.py
+++ face/video_capture.py
@@ -1,10 +1,11 @@
 from dlib import get_frontal_face_detector, shape_predictor
 from time import time
-from cv2 import VideoCapture, resize, waitKey, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY, imwrite
-from face.face_detection import recuperate_intra_face_points, intra_face, points_landmarks, exterior_face, inclinaison
+from cv2 import VideoCapture, resize, waitKey, createBackgroundSubtractorMOG2, destroyAllWindows, imshow, cvtColor, COLOR_BGR2GRAY, imwrite
+from face.face_detection import recuperate_intra_face_points, intra_face, points_landmarks, exterior_face, inclinaison, emotion_points, emotions_model, expressions
 from eyes.eyes_detection import tracking_eyes
-from display.face_display import recuperate_face
 
+from display.face_display import face_displaying
+from display.eyes_display import eyes_display
 
 #from face_display import recuperate_face
 
@@ -19,17 +20,24 @@ def timmer(start):
     print(elapsed)
 
 
-def video_lecture(video_name, face_points):
+def video_capture(video_name, face_points, emotion_model):
 
     video = VideoCapture(video_name)
+    fgbg = createBackgroundSubtractorMOG2(history=200, detectShadows=False)
 
     #Detect head
     detector = get_frontal_face_detector()
     predictor = shape_predictor(face_points)
 
-    right_eye = {"droite":0, "gauche":0, "haut":0, "bas":0}
-    left_eye = {"droite":0, "gauche":0, "haut":0, "bas":0}
+    right_eye = {"right":0, "left":0, "top":0, "bot":0}
+    left_eye = {"right":0, "left":0, "top":0, "bot":0}
 
+    em_nose = [[], []]
+    open_right_eye = [[], []]
+    open_left_eye = [[], []]
+    eye_display = []
+
+    counter_frame = 0
     while True:
 
         #Timmer start
@@ -52,28 +60,34 @@ def video_lecture(video_name, face_points):
 
         #DOIT ETRE UN THREAD
         inclinaison(landmarks, frame)
-        leftEye, rightEye = tracking_eyes(landmarks, head, frame, gray, left_eye, right_eye)
+        eyes_movements = tracking_eyes(landmarks, head, frame, gray, left_eye, right_eye)
 
         #Doit etre un multiprocess ou thread chpas
-        intra_face(frame, gray, landmarks, head, leftEye, rightEye)
-
-
+        intra_face(frame, gray, landmarks, head, fgbg)
+        #emotion_points(frame, landmarks, em_nose, open_right_eye, open_left_eye)
+        #expressions(counter_frame, em_nose, open_right_eye, open_left_eye)
 
 
         #except (IndexError): pass
 
  
         #Display
-        #recuperate_face(convexhull, gray, frame, head_points)
+        face_displaying(gray, frame, convexhull, head_points, landmarks, fgbg)
 
-
+        #raising = eyes_display(frame, gray, landmarks, eyes_movements, eye_display, counter_frame)
+        raising = False
         #imshow('frame', frame)
 
         #Timmer end
-        timmer(start)
+        #timmer(start)
+
+
+        if raising == True:
+            eye_display = []
+
+        counter_frame += 1
 
         if waitKey(0) & 0xFF == ord("q"):
-            imwrite("ici.jpg", frame)
             break
 
     video.release()
