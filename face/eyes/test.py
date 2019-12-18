45e9e572b7b6cb4ec2bad98de57db79642a03436
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-18 21:35:41 +0100

    Update test.py
    ok

--- face/eyes/test.py
+++ face/eyes/test.py
@@ -19,45 +19,25 @@ def recuperate_intra_face_points(landmarks, faces, img):
     return head, convexhull
 
 
-def position(landmarks, points_position):
-
-    pointsA = [0, 1, 2]
-    pointsB = [16, 15, 14]
-
-    out = False
-    for i in range(len(pointsA)):
-        a = landmarks.part(pointsA[i]).x
-        b = landmarks.part(pointsB[i]).x
-
-
-
-        if (b-a) < np.mean(points_position[i]) - 10 and np.mean(points_position[i]) < 110 :
-            print("changement de plan recule")
-            out = True
-        elif (b-a) > np.mean(points_position[i]) + 10 and np.mean(points_position[i]) < 110:
-            print("changement de plan, gros plan")
-            out = True
-
-        points_position[i].append(b-a)
-
-    return out, b-a
 
 facePoints = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\face\models\shape_predictor_68_face_landmarks.dat"
 
 
 video_name = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\video\a.mp4"
+xml = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\face\frontalEyes35x16.xml"
+
+
+
 
 video = cv2.VideoCapture(video_name)
 
 detector = get_frontal_face_detector()
 predictor = shape_predictor(facePoints)
 
-xx = []
-yy = []
 
-xx1 = []
-yy1 = []
-points_position = [[], [], []]
+
+last = []
+
 while True:
 
 
@@ -65,21 +45,9 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
-
-
-
-    
     try:
 
         landmarks, face = points_landmarks(gray, predictor, detector)
-        out, a = position(landmarks, points_position)
-        print("iciiiiiiiiiiii", a)
-        if out is True:
-            xx = []
-            yy = []
-            points_position = [[], [], []]
-            xx1 = []
-            yy1 = []
 
 
         faces, convexhull = recuperate_intra_face_points(landmarks, face, frame)
@@ -87,31 +55,35 @@ while True:
         cv2.rectangle(frame, (faces[0], faces[1]), (faces[0] + faces[2], faces[1] + faces[3]), (0, 0, 255), 3)
 
 
-        eyes = (cv2.convexHull(np.array([(landmarks.part(n).x, landmarks.part(n).y)
-                        for pts in faces for n in range(36, 42)])),
-                cv2.convexHull(np.array([(landmarks.part(n).x, landmarks.part(n).y)
-                        for pts in faces for n in range(42, 48)])))
+        a = (landmarks.part(36).x, landmarks.part(36).y)
+        b = (landmarks.part(37).x - 2, landmarks.part(37).y - 2)
+        c = (landmarks.part(38).x + 2, landmarks.part(38).y - 2)
+        d = (landmarks.part(39).x, landmarks.part(39).y)
+        e = (landmarks.part(40).x, landmarks.part(40).y)
+        f = (landmarks.part(41).x, landmarks.part(41).y)
 
         cv2.circle(frame, (landmarks.part(36).x, landmarks.part(36).y), 1, (0, 0, 255), 1)
         cv2.circle(frame, (landmarks.part(39).x, landmarks.part(39).y), 1, (0, 0, 255), 1)
+        
 
-        cv2.circle(frame, (landmarks.part(42).x, landmarks.part(42).y), 1, (0, 255, 255), 1)
-        cv2.circle(frame, (landmarks.part(45).x, landmarks.part(45).y), 1, (0, 255, 255), 1)
+
+        eyes = np.array([a, b, c, d, e, f])
 
 
         height, width = gray.shape[:2]
         black_frame = np.zeros((height, width), np.uint8)
         mask = np.full((height, width), 255, np.uint8)
-        cv2.fillPoly(mask, [eyes[0]], (0, 0, 0))
+        cv2.fillPoly(mask, [eyes], (0, 0, 0))
         mask = cv2.bitwise_not(black_frame, gray.copy(), mask=mask)
 
-        x, y, w, h = cv2.boundingRect(eyes[0])
-        cropMask = mask[y:y+h, x:x+w]
-        cropImg = frame[y:y+h, x:x+w]
+        x, y, w, h = cv2.boundingRect(eyes)
 
+        cropMask = mask[y:y+h, x:x+w]
         height, width = cropMask.shape[:2]
         cropMask = cv2.resize(cropMask, (width*10, height*8))
 
+        aa = frame[y:y+h, x:x+w]
+        aa = cv2.resize(aa, (width*10, height*8))
 
 
         minimum_thresh = [10000, 0]
@@ -130,8 +102,6 @@ while True:
 
         tresh = cv2.threshold(mask, minimum_thresh[1], 255, cv2.THRESH_BINARY)[1]
 
-
-
         contours = cv2.findContours(tresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0][-2:]
         contours = sorted(contours, key=cv2.contourArea)
         nan = False
@@ -139,124 +109,39 @@ while True:
             moment = cv2.moments(contours[-1])
             x = int(moment['m10'] / moment['m00'])
             y = int(moment['m01'] / moment['m00'])
-
-            if a > 90 and a < 110:
-                nb = 8
-            elif a > 110:
-                nb = 8
-            elif a > 65 and a < 90:
-                nb = 5
-            if len(xx) > 5:
-
-                if x <= np.mean(xx) - nb:
-                    print("droite")
-                    nan = True
-                elif x >= np.mean(xx) + nb:
-                    print("gauche")
-                    nan = True
-            
-                print(np.mean(xx), x)
-                print(np.mean(yy), y)
-                
-
-            if nan is False:
-                xx.append(x)
-                yy.append(y)
-
-            cv2.circle(tresh, (x, y), 2, (255, 255, 255), 2)
-
-        except (IndexError):
+        except:
             pass
 
+        cv2.circle(frame, (x, y), 1, (0, 0, 255), 1)
 
 
+        print((0,0), x, y, (tresh.shape[1], tresh.shape[0]))
+
+        if x > (0.65) * tresh.shape[1]:
+            print("droite")
+
+        elif x < 0.45 * tresh.shape[1]:
+            print("gauche")
+
+        if y < 0.40 * tresh.shape[0]:
+            print("haut")
+
+        elif y > 0.70 * tresh.shape[0]:
+            print("bas")
 
 
+   
+        droite = ""
+        gauche = ""
+        droit_bas = ""
+        droit_haut = ""
+        gauche_bas = ""
+        gauche_haut = ""
 
 
-
-
-
-
-
-
-            
-
-
-
-##
-##        height, width = gray.shape[:2]
-##        black_frame = np.zeros((height, width), np.uint8)
-##        mask = np.full((height, width), 255, np.uint8)
-##        cv2.fillPoly(mask, [eyes[0]], (0, 0, 0))
-##        mask = cv2.bitwise_not(black_frame, gray.copy(), mask=mask)
-##
-##        x, y, w, h = cv2.boundingRect(eyes[0])
-##        cropMask = mask[y:y+h, x:x+w]
-##        cropImg = frame[y:y+h, x:x+w]
-##
-##        height, width = cropMask.shape[:2]
-##        cropMask = cv2.resize(cropMask, (width*10, height*8))
-##
-##
-##
-##        minimum_thresh = [10000, 0]
-##        for thresh in range(5, 100, 5):
-##
-##            kernel = np.ones((3, 3), np.uint8)
-##            mask = cv2.bilateralFilter(cropMask, 10, 15, 15)
-##            mask = cv2.erode(mask, kernel, iterations=3)
-##            mask = cv2.threshold(mask, thresh, 255, cv2.THRESH_BINARY)[1]
-##
-##            height, width = cropMask.shape[:2]
-##            nb_pixels = height * width
-##            blacks_pixels = nb_pixels - cv2.countNonZero(mask) / nb_pixels
-##            if blacks_pixels < minimum_thresh[0]:
-##                minimum_thresh[0], minimum_thresh[1] = blacks_pixels, thresh
-##
-##        tresh = cv2.threshold(mask, minimum_thresh[1], 255, cv2.THRESH_BINARY)[1]
-##
-##
-##
-##        contours = cv2.findContours(tresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0][-2:]
-##        contours = sorted(contours, key=cv2.contourArea)
-##
-##        try:
-##            moment = cv2.moments(contours[-1])
-##            x = int(moment['m10'] / moment['m00'])
-##            y = int(moment['m01'] / moment['m00'])
-##
-##
-##            if len(xx1) > 10:
-##
-##                if x < np.mean(xx1) - 5:
-##                    print("droite")
-##
-##                elif x > np.mean(xx1) + 5:
-##                    print("gauche")
-##
-##            
-##                print(np.mean(xx1), x)
-##                print(np.mean(yy1), y)
-##
-##
-##
-##            xx1.append(x)
-##            yy1.append(y)
-##
-##            cv2.circle(tresh, (x, y), 2, (255, 255, 255), 2)
-##
-##
-##
-##
-##
-##        except (IndexError):
-##            pass
-##
-##        cv2.imshow('cropMask', cropMask)
-##        cv2.imshow('tresh', tresh)
-
-
+        cv2.imshow('cropMask', cropMask)
+        cv2.imshow('tresh', tresh)
+        cv2.imshow('aa', aa)
 
 
 
@@ -267,6 +152,13 @@ while True:
 
     except (IndexError):
         pass
+
+
+
+
+
+
+
 
 
 
