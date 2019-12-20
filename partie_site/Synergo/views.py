7ddad61b01e76a50ae1c4640625e3c6d76c1a14c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-20 01:58:22 +0100

    Add files via upload

--- partie_site/Synergo/views.py
+++ partie_site/Synergo/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
 
-def Home(request):
+
+
+def Garde(request):
+    return render(request, "Garde.html")
+
+def transition(request):
+    return render(request, "Transition.html")
+
+def home(request):
     return render(request, "Home.html")
+
+def upload(request):
+
+    if request.method == 'POST':
+            form = UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                handle_uploaded_file(request.FILES['file'])
+                return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    
+      
+    return render(request, 'Upload.html', {'form': form})
+
+
