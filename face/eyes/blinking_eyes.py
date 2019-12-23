97c472aa582669934f19684266e93f83a16044c8
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-23 01:15:37 +0100

    Update blinking_eyes.py

--- face/eyes/blinking_eyes.py
+++ face/eyes/blinking_eyes.py
@@ -4,66 +4,48 @@ import dlib
 from math import hypot
 import time
 
-cap = cv2.VideoCapture(0)
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-
 
 
 def midpoint(p1 ,p2):
+    """Mid points beetween the 2 tops points."""
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
-def get_blinking_ratio(eye_points, facial_landmarks, frame):
 
-    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
-    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+def get_length_eye(eye_points, facial_landmarks):
+    """Euclidiens distance from the mid point"""
+
+    center_top = midpoint(facial_landmarks.part(eye_points[0]), facial_landmarks.part(eye_points[1]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[2]), facial_landmarks.part(eye_points[3]))
     ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
-
-
-    points = cv2.convexHull(np.array([(landmarks.part(n).x,
-                                       landmarks.part(n).y)
-                                      for n in range(eye_points[0], eye_points[-1])]))
-
-
-    x, y, w, h = cv2.boundingRect(points)
-    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
 
     return ver_line_lenght
 
 
 def points_landmarks(gray, predictor, detector):
     """ Return the 68 points of the face and the face"""
-
+    out = None, None
     face = detector(gray)
-    return predictor(gray, face[0]), face
+    if len(face) > 0:
+        landmarks = predictor(gray, face[0])
+        out = landmarks, face
+    return out
 
+def get_length_face(landmarks, faces, frame):
+    """Recuperate euclidian distance height of the head"""
 
-def get_faces(landmarks, faces, frame):
-    """Recuperate all coordiantes of landmarks (faces points).
-    Recuperate convex points (exterior of face) and triangle
-    points (area of the interior of the face)."""
-
-    #points of face
     points = [(landmarks.part(n).x, landmarks.part(n).y) for pts in faces for n in range(0, 68)]
-
-    #Convex points (contour of face)
     convexhull = cv2.convexHull(np.array(points))
-
-    #Head points
     x, y, w, h = cv2.boundingRect(convexhull)
-
-    headhead = hypot( (0), ((y+h) - y))
+    length_head = hypot( (0), ((y+h) - y))
     
-    return headhead
+    return length_head
 
 
 
+cap = cv2.VideoCapture("d.mp4")
 
-
-
-
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
    
@@ -71,42 +53,33 @@ while True:
 
     frame = cv2.resize(frame, (500, 400))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
-    try:
-        landmarks, faces = points_landmarks(gray, predictor, detector)
 
-        headhead = get_faces(landmarks, faces, frame)
+    #We analysed 4 videos with faces
+    #we deduced a ratio around 0.035 beetween head and eyes length.
+    #Care need to analyse head position if the person
+    #tilt your head forward it could be false.
+    head_eye_ratio = 0.035
 
+    landmarks, faces = points_landmarks(gray, predictor, detector)
+    if landmarks is not None:
+        length_head = get_length_face(landmarks, faces, frame)
 
+        length_right = get_length_eye([37, 38, 40, 41], landmarks)
+        length_left = get_length_eye([43, 44, 46, 47], landmarks)
 
-
-        ver_line_lenght1 = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks, frame)
-
-
-
-        ver_line_lenght2 = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks, frame)
-
-
-        o = (ver_line_lenght1 + ver_line_lenght2) / 2
-        print(o, headhead)
-
-        if o < 0.035 * headhead:
-            print("oui")
-
-
-
-
-
-       
-
-
-    except IndexError:pass
-
+        length_mean = (length_right + length_left) / 2
+        if length_mean < head_eye_ratio * length_head:
+            print("BLINK")
+    else:
+        print("no detection")
 
     cv2.imshow("Frame", frame)
 
 
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
  
 cap.release()
