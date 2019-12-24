99078374fbfa4a2941f00fdebfbda33ddfc83b9b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-24 18:01:34 +0100

    Update video_treatment.py

--- video_treatment/video_treatment.py
+++ video_treatment/video_treatment.py
@@ -24,23 +24,41 @@ def points_landmarks(gray, predictor, detector):
 
 
 
+def plan_switch1(landmarks, points_position):
+    #TODOOOOOOOOOOOO
+    pointsA = [0, 1, 2]
+    pointsB = [16, 15, 14]
+
+    out = False
+    for i in range(len(pointsA)):
+        a = landmarks.part(pointsA[i]).x
+        b = landmarks.part(pointsB[i]).x
+
+        if (b-a) < mean(points_position[i]) - 10:
+            print("changement de plan recule")
+            out = True
+        elif (b-a) > mean(points_position[i]) + 10:
+            print("changement de plan, gros plan")
+            out = True
+
+        points_position[i].append(b-a)
+
+    return out
+
+
+def plan_switch12(frame, subtractor, frame_size):
+    #TODOOOOOOOOOOOO
+    sub = subtractor.apply(frame)
+    a = cv2.countNonZero(sub)
+
+    if a > 0 and a >= ((frame_size) * 0.625):
+        print("CHANGEMENT DE FRAME")
 
 
 
+def resize_frame(frame, predictor, detector):
 
-
-cap = cv2.VideoCapture("c.mp4")
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-
-
-while True:
-   
-    _, frame = cap.read()
     height, width = frame.shape[:2]
-
     nb = 0.1
     ocontinue = True
     while ocontinue:
@@ -50,8 +68,9 @@ while True:
         frame = cv2.resize(frame, (int(width / nb), int(height / nb)))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         landmarks, faces = points_landmarks(gray, predictor, detector)
-        x, y, w, h = get_faces(landmarks, faces)
 
+        
+        x, y, w, h = get_faces(landmarks, faces)
 
         print(w, nb)
         
@@ -59,6 +78,32 @@ while True:
             ocontinue = False
 
 
+    return nb
+
+
+
+
+
+
+cap = cv2.VideoCapture("c.mp4")
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=5, detectShadows=True)
+
+
+
+while True:
+   
+    _, frame = cap.read()
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    landmarks, faces = points_landmarks(gray, predictor, detector)
+
+    to_resize = resize_frame(frame, predictor, detector)
+
+    plan_switch1(landmarks, points_position)
+    plan_switch12(frame, subtractor)
 
 
  
