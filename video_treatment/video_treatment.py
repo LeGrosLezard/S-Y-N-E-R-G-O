208277cfb97796959a2c639d7537c4cf3dbf68f2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-24 22:30:11 +0100

    Update video_treatment.py

--- video_treatment/video_treatment.py
+++ video_treatment/video_treatment.py
@@ -24,7 +24,7 @@ def points_landmarks(gray, predictor, detector):
 
 
 
-def plan_switch1(landmarks, points_position):
+def movements_dude(landmarks, points_position):
     #TODOOOOOOOOOOOO
     pointsA = [0, 1, 2]
     pointsB = [16, 15, 14]
@@ -35,10 +35,10 @@ def plan_switch1(landmarks, points_position):
         b = landmarks.part(pointsB[i]).x
 
         if (b-a) < mean(points_position[i]) - 10:
-            print("changement de plan recule")
+            print("recule")
             out = True
         elif (b-a) > mean(points_position[i]) + 10:
-            print("changement de plan, gros plan")
+            print("s'avance")
             out = True
 
         points_position[i].append(b-a)
@@ -46,7 +46,7 @@ def plan_switch1(landmarks, points_position):
     return out
 
 
-def plan_switch12(frame, subtractor, frame_size):
+def plan_switch(frame, subtractor, frame_size):
     #TODOOOOOOOOOOOO
     sub = subtractor.apply(frame)
     a = cv2.countNonZero(sub)
@@ -63,7 +63,7 @@ def resize_frame(frame, predictor, detector):
     ocontinue = True
     while ocontinue:
 
-        nb += 0.1
+        nb += 0.05
 
         frame = cv2.resize(frame, (int(width / nb), int(height / nb)))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -85,14 +85,14 @@ def resize_frame(frame, predictor, detector):
 
 
 
-cap = cv2.VideoCapture("c.mp4")
+cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=5, detectShadows=True)
 
 
-
+points_position = [[], [], []]
 while True:
    
     _, frame = cap.read()
@@ -102,12 +102,14 @@ while True:
 
     to_resize = resize_frame(frame, predictor, detector)
 
-    plan_switch1(landmarks, points_position)
-    plan_switch12(frame, subtractor)
+##    he_moved = movements_dude(landmarks, points_position)
+##    if he_moved is True:
+##        print("RE SIZERRRRRRRRRR")
+##    plan_switch(frame, subtractor)
 
 
  
-    print(nb)
+
     cv2.imshow("Frame", frame)
     if cv2.waitKey(0) & 0xFF == ord('q'):
         break
