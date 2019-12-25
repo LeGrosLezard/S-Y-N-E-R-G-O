be82fd4c635bcbf64b944242e08c794b046ee554
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-25 18:59:00 +0100

    Update video_treatment.py

--- video_treatment/video_treatment.py
+++ video_treatment/video_treatment.py
@@ -1,7 +1,7 @@
 import dlib
 import cv2
 import numpy as np
-
+from dlib import get_frontal_face_detector, shape_predictor
 
 def movements_dude(landmarks, points_position):
     #TODOOOOOOOOOOOO
@@ -35,26 +35,75 @@ def plan_switch(frame, subtractor, frame_size):
 
 
 
+
+
+
+
+
+
+
+
+
+
+def recuperate_landmarks(gray_frame, predictor, detector):
+    """Recuperate landmarks from dlib"""
+
+    faces = detector(gray_frame)
+    out = None, None
+
+    if len(faces) > 0:
+        landmarks = predictor(gray_frame, faces[0])
+        out = faces, landmarks
+
+    return out
+
+
+
+def get_face_in_box(landmarks):
+    """Recuperate all coordiantes of landmarks (faces points).
+    Recuperate convex points (exterior of face) and triangle
+    points (area of the interior of the face)."""
+
+    points = [(landmarks.part(n).x, landmarks.part(n).y)
+              for n in range(0, 68)]
+
+    convexhull = cv2.convexHull(np.array(points))
+    head_box = cv2.boundingRect(convexhull)
+
+    return head_box
+
+
+
+
 def resizer(frame, predictor, detector):
 
+    
+    nb = 0.35
     height, width = frame.shape[:2]
-    nb = 0.1
     ocontinue = True
     while ocontinue:
 
         nb += 0.05
 
+        
         frame = cv2.resize(frame, (int(width / nb), int(height / nb)))
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        landmarks, faces = points_landmarks(gray, predictor, detector)
+        try:
+            faces, landmarks = recuperate_landmarks(gray, predictor, detector)
+            head = get_face_in_box(landmarks)
+            x, y, w, h = head
+            print(w, nb)
 
-        
-        x, y, w, h = get_faces(landmarks, faces)
 
-        print(w, nb)
-        
-        if w <= 93: 
-            ocontinue = False
+            if w <= 93: 
+                ocontinue = False
+
+        except:
+            print("no found")
+
+        cv2.imshow("frameframe", frame)
+        cv2.waitKey(0)
+
 
 
     return nb
@@ -64,32 +113,26 @@ def resize_frame(frame):
     """Resize frame for a ' good accuracy ' and speed """
  
     height, width = frame.shape[:2]
-    nb = 1.5
-    nb = 2
-    #nb = 1.5500000000000007
+    nb = 1.5#c
+    nb = 2 #a
+    nb = 1.5500000000000007#cam normal
+    nb = 1.4500000000000006 #f
     frame = cv2.resize(frame, (int(width / nb), int(height / nb)))
     gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
 
     return frame, gray
 
 
-def recuperate_intra_face_points(landmarks, faces, img):
-    """Recuperate all coordiantes of landmarks (faces points).
-    Recuperate convex points (exterior of face) and triangle
-    points (area of the interior of the face)."""
-
-
-    points = [(landmarks.part(n).x, landmarks.part(n).y) for n in range(0, 68)]
-    convexhull = cv2.convexHull(np.array(points))
-    head = cv2.boundingRect(convexhull)
-
-    return head
 
 
 
 
 
-##cap = cv2.VideoCapture(0)
+
+
+
+
+##cap = cv2.VideoCapture("f.mp4")
 ##
 ##detector = dlib.get_frontal_face_detector()
 ##predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -102,9 +145,10 @@ def recuperate_intra_face_points(landmarks, faces, img):
 ##    _, frame = cap.read()
 ##    
 ##    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-##    landmarks, faces = points_landmarks(gray, predictor, detector)
+##    faces, landmarks = recuperate_landmarks(gray, predictor, detector)
 ##
-##    to_resize = resizer(frame, predictor, detector)
+##    nb = resizer(frame,  predictor, detector)
+##
 ##
 ####    he_moved = movements_dude(landmarks, points_position)
 ####    if he_moved is True:
