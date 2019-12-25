c32dda9bc6593e112a4261b2f9f790c80061a2b7
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-25 17:46:10 +0100

    Update video_treatment.py

--- video_treatment/video_treatment.py
+++ video_treatment/video_treatment.py
@@ -3,27 +3,6 @@ import cv2
 import numpy as np
 
 
-def get_faces(landmarks, faces):
-    """Recuperate all coordiantes of landmarks (faces points).
-    Recuperate convex points (exterior of face) and triangle
-    points (area of the interior of the face)."""
-
-    points = [(landmarks.part(n).x, landmarks.part(n).y) for pts in faces for n in range(0, 68)]
-    convexhull = cv2.convexHull(np.array(points))
-    x, y, w, h = cv2.boundingRect(convexhull)
-
-    return x, y, w, h
-
-
-def points_landmarks(gray, predictor, detector):
-    """ Return the 68 points of the face and the face"""
-
-    face = detector(gray)
-    return predictor(gray, face[0]), face
-
-
-
-
 def movements_dude(landmarks, points_position):
     #TODOOOOOOOOOOOO
     pointsA = [0, 1, 2]
@@ -56,7 +35,7 @@ def plan_switch(frame, subtractor, frame_size):
 
 
 
-def resize_frame(frame, predictor, detector):
+def resizer(frame, predictor, detector):
 
     height, width = frame.shape[:2]
     nb = 0.1
@@ -81,38 +60,63 @@ def resize_frame(frame, predictor, detector):
     return nb
 
 
-
-
-
-
-cap = cv2.VideoCapture(0)
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=5, detectShadows=True)
-
-
-points_position = [[], [], []]
-while True:
-   
-    _, frame = cap.read()
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    landmarks, faces = points_landmarks(gray, predictor, detector)
-
-    to_resize = resize_frame(frame, predictor, detector)
-
-##    he_moved = movements_dude(landmarks, points_position)
-##    if he_moved is True:
-##        print("RE SIZERRRRRRRRRR")
-##    plan_switch(frame, subtractor)
-
-
+def resize_frame(frame):
+    """Resize frame for a ' good accuracy ' and speed """
  
+    height, width = frame.shape[:2]
+    nb = 1.5
+    nb = 2
+    #nb = 1.5500000000000007
+    frame = cv2.resize(frame, (int(width / nb), int(height / nb)))
+    gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
 
-    cv2.imshow("Frame", frame)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-        break
- 
-cap.release()
-cv2.destroyAllWindows()
+    return frame, gray
+
+
+def recuperate_intra_face_points(landmarks, faces, img):
+    """Recuperate all coordiantes of landmarks (faces points).
+    Recuperate convex points (exterior of face) and triangle
+    points (area of the interior of the face)."""
+
+
+    points = [(landmarks.part(n).x, landmarks.part(n).y) for n in range(0, 68)]
+    convexhull = cv2.convexHull(np.array(points))
+    head = cv2.boundingRect(convexhull)
+
+    return head
+
+
+
+
+
+##cap = cv2.VideoCapture(0)
+##
+##detector = dlib.get_frontal_face_detector()
+##predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+##subtractor = cv2.createBackgroundSubtractorMOG2(history=1, varThreshold=5, detectShadows=True)
+##
+##
+##points_position = [[], [], []]
+##while True:
+##   
+##    _, frame = cap.read()
+##    
+##    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+##    landmarks, faces = points_landmarks(gray, predictor, detector)
+##
+##    to_resize = resizer(frame, predictor, detector)
+##
+####    he_moved = movements_dude(landmarks, points_position)
+####    if he_moved is True:
+####        print("RE SIZERRRRRRRRRR")
+####    plan_switch(frame, subtractor)
+##
+##
+## 
+##
+##    cv2.imshow("Frame", frame)
+##    if cv2.waitKey(0) & 0xFF == ord('q'):
+##        break
+## 
+##cap.release()
+##cv2.destroyAllWindows()
