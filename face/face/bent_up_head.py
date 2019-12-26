2a44277a7319e9dccaa69bc80488357ba3dfcca4
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-26 22:16:54 +0100

    Update bent_up_head.py

--- face/face/bent_up_head.py
+++ face/face/bent_up_head.py
@@ -1,5 +1,47 @@
-def look_top_bot(ok, ok_haut, right_eye, left_eye, nose):
+from numpy import min as np_min
+from numpy import sin
+from math import acos, pow
+from scipy.spatial import distance as dist
+
+DOWN_COUNTER = 0
+UP_COUNTER = 0
+
+def analyse_position(bent_up):
+
+    global DOWN_COUNTER
+    global UP_COUNTER
+    out = ""
+
+    if DOWN_COUNTER > 10 and bent_up >= 17:
+        out = "position baissé"
+
+    elif bent_up >= 17:
+        out = "tete baissé"
+        DOWN_COUNTER += 1
+
+    elif UP_COUNTER > 10 and bent_up <= -4:
+        out = "position levé"
+
+    elif bent_up <= -4:
+        out = "tete levé"
+        UP_COUNTER += 1
+
+    else:
+        if UP_COUNTER > 10:
+            out = "reprise position non levé"
+        if DOWN_COUNTER > 10:
+            out = "reprise position non enfouis"
+
+        DOWN_COUNTER = 0
+        UP_COUNTER = 0
+
+    return out
+
+def bent_up_head(right_eye, left_eye, nose):
     """Calculus distance beetween nose and eyes line"""
+
+    global DOWN_COUNTER
+    global UP_COUNTER
 
     d_eyes = dist.euclidean(right_eye, left_eye) 
     d1 = dist.euclidean(right_eye, nose) 
@@ -10,23 +52,6 @@ def look_top_bot(ok, ok_haut, right_eye, left_eye, nose):
     cosb = np_min( (pow(d2, 2) - pow(d1, 2) + pow(d_eyes, 2) ) / (2*d2*d_eyes) )
     bent_up = int(250*(d2*sin(acos(cosb))-coeff/3.5)/coeff)
 
+    out = analyse_position(bent_up)
 
-    if ok > 10 and bent_up >= 17: print("position baissé")
-
-    elif bent_up >= 17:
-        print("tete baissé")
-        ok += 1
-
-    elif ok_haut > 10 and bent_up <= -4: print("position levé")
-
-    elif bent_up <= -4:
-        print("tete levé")
-        ok_haut += 1
-
-    else:
-        if ok_haut > 10: print("reprise position non levé")
-        if ok > 10:print("reprise position non enfouis")
-        ok = 0
-        ok_haut = 0
-
-    return ok, ok_haut
+    return out
