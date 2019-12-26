191371439a9f3b0ce8ad9ebc34bc38017c5915dc
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-26 01:47:52 +0100

    Update eyes_movement.py
    bon ca vaut ceque ca vaut, blink et mouvement du bas... par émotion detection on dira

--- eyes/eyes_movement.py
+++ eyes/eyes_movement.py
@@ -1,34 +1,113 @@
-"""here eyes_movements"""
-
-
 import cv2
 import numpy as np
-import dlib
+
+
+LAST_H_RIGHT = []
+LAST_H_LEFT = []
+def eyes_position(landmarks, frame, right_eye, left_eye):
+
+    global LAST_H_RIGHT
+    global LAST_H_LEFT
+
+    eyes = (cv2.convexHull(np.array([(landmarks.part(n).x, landmarks.part(n).y)
+            for n in range(36, 42)])),
+    cv2.convexHull(np.array([(landmarks.part(n).x, landmarks.part(n).y)
+            for n in range(42, 48)])))
+
+    right = ""
+    left = ""
+    right_top = ""
+    left_top = ""
+    right_bot = ""
+    left_bot = ""
+    if right_eye[0] is not None:
+
+        x, y, w, h = cv2.boundingRect(eyes[0])
+        cv2.rectangle(frame, (x, (y-5)), (x+w, y+h+5), (0,255,0), 1)
+
+        x_center = right_eye[0]
+        y_center = right_eye[1]
+
+        h = h + 10
+
+
+
+        if len(LAST_H_RIGHT) >= 2 and h >= LAST_H_RIGHT[-2] + 2 and y_center >= int(0.40*h):
+            right_top = "haut"
+        else:
+            LAST_H_RIGHT.append(h)
+
+        #if height of detection shrink of -1.5 of the mean height frame
+        #and pupil Y < 40% of the height of frame
+        if h <=  np.mean(LAST_H_RIGHT) - 1.5 and y_center <= int(0.45*h):
+            right_bot = "bas"
+
+
+        if x_center >= int(0.85 * w):
+            right = "gauche"
+
+        elif x_center <= int(0.72 * w):
+            right = "droite"
 
 
 
 
-cap = cv2.VideoCapture("a.mp4")
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
-while True:
-   
-    _, frame = cap.read()
-
-    height, width = frame.shape[:2]
-    frame = cv2.resize(frame, (int(width/2), int(height/2)))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    
 
 
-    cv2.imshow("Frame", frame)
 
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
- 
-cap.release()
-cv2.destroyAllWindows()
+
+    if left_eye[0] is not None:
+
+        x, y, w, h = cv2.boundingRect(eyes[1])
+        cv2.rectangle(frame, (x, (y-5)), (x+w, y+h+5), (0,255,0), 1)
+
+        x_center = left_eye[0]
+        y_center = left_eye[1]
+
+        h = h + 10
+
+
+        if x_center >= int(0.85 * w):
+            left = "gauche"
+
+        elif x_center <= int(0.72 * w):
+            left = "droite"
+
+
+        if len(LAST_H_LEFT) >= 2 and h >= LAST_H_LEFT[-2] + 2 and y_center >= int(0.40*h):
+            left_top = "haut"
+        else:
+            LAST_H_LEFT.append(h)
+
+        #if height of detection shrink of -1.5 of the mean height frame
+        #and pupil Y < 40% of the height of frame
+        if h <=  np.mean(LAST_H_LEFT) - 1.5 and y_center <= int(0.45*h):
+            left_bot = "bas"
+
+
+    movement = ""
+
+    if right == "gauche" and left == "gauche":
+        movement += "gauche "
+    elif right == "droite" and left == "droite":
+        movement += "droite "
+
+
+    if right_top == "haut" and left_top == "haut":
+        movement += "haut")
+    elif right_bot == "bas" and left_bot == "bas":
+        movement += "bas"
+
+
+
+
+
+
+
+
+        
+
+
+
