633248b1d4c835f402ae21bd52cba38051b8b1fe
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-27 00:10:48 +0100

    Add files via upload

---
+++ head/advanced_recedes_head.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+points_position = [[], [], []]
+def advanced_recedes_head(landmarks):
+
+    global points_position
+
+    pointsA = [0, 1, 2]
+    pointsB = [16, 15, 14]
+
+    out = False
+    for i in range(len(pointsA)):
+        a = landmarks.part(pointsA[i]).x
+        b = landmarks.part(pointsB[i]).x
+        c = (b-a)
+
+        if c < np.mean(points_position[i]) - 6:
+            print("recule")
+            out = True
+ 
+        elif c > np.mean(points_position[i]) + 7:
+            print("s'avance")
+            out = True
+
+        points_position[i].append(c)
+
+    return out
---
+++ head/head_movement.py
@@ -0,0 +1,46 @@
+"""Let you watch that https://www.youtube.com/watch?v=ibuEFfpVWlU&t=518s"""
+
+
+
+from head_points import eyes_points_for_head_analysis
+
+#Head movement
+from turn_head import turn_head
+from advanced_recedes_head import advanced_recedes_head
+from leanning_head import leanning_head
+from bent_up_head import bent_up_head
+from head_emotion import head_emotion
+
+#Face movement
+from head_composition import head_composition
+
+
+def head_movement(landmarks, head_box, emotion_classifier, gray):
+
+    #Recuperate right_eye, left_eye, nose
+    right_eye, left_eye, nose = eyes_points_for_head_analysis(landmarks)
+
+    #Person moves his head to right or left
+    turn = turn_head(right_eye, left_eye, nose, head_box)
+    #if turn != "": print(turn)
+
+    #Person leaning his head to right or left
+    leanning = leanning_head(right_eye, left_eye, nose, head_box)
+    #if leanning != "":print(leanning)
+
+    #Person moves his head to bot or top
+    bot_top = bent_up_head(right_eye, left_eye, nose)
+    #if bot_top != "": print(bot_top)
+
+    #Emotion on face
+    emotion = head_emotion(head_box, emotion_classifier, gray)
+    #print(emotion)
+
+
+
+
+
+
+
+
+
