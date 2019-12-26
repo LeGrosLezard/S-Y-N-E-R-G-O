dfdd7a9b838cdff16469f78e1d2c6a126d5772b2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-26 21:57:12 +0100

    Update turn_head.py

--- face/face/turn_head.py
+++ face/face/turn_head.py
@@ -1,5 +1,5 @@
 from scipy.spatial import distance as dist
-def look_right_left(right_eye, left_eye, nose, head_box):
+def turn_head(right_eye, left_eye, nose, head_box):
     """Calculus difference beetween left right distance"""
 
     d1 = dist.euclidean(right_eye, nose) 
@@ -9,6 +9,8 @@ def look_right_left(right_eye, left_eye, nose, head_box):
     look_to = int(250*(d1-d2)/coeff)
 
     w = head_box[2]
+
+    out = ""
 
     if look_to > int(0.25 * w):
         out = "gauche"
