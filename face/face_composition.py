89ed166a2378f39d8cd38246ad180de763e5d5da
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-27 00:28:53 +0100

    Update face_composition.py

--- face/face_composition.py
+++ face/face_composition.py
@@ -1,11 +1,35 @@
-"""Here analysis"""
+import cv2
+import numpy as np
 
 
-def on_eyes(landmarks):
-    on_eyes_points = {"onEye1":(17, 21), "onEye2":(22, 26)}
-    
-    #monté
-    #baisse vers l'intérieur
+def make_landmarks_points(landmarks, area):
+    return np.array([(landmarks.part(n).x, landmarks.part(n).y)
+                     for n in range(area[0], area[1])])
+
+def on_eyes(landmarks, frame):
+    on_eyes_points = {"onEye1":[18, 22], "onEye2":[22, 26]}
+
+
+    onEye1 = make_landmarks_points(landmarks, on_eyes_points["onEye1"])
+    onEye2 = make_landmarks_points(landmarks, on_eyes_points["onEye2"])  
+
+
+    for i in onEye1:
+        cv2.circle(frame, (i[0], i[1]), 1, (0, 255, 0), 1)
+
+    for i in onEye2:
+        cv2.circle(frame, (i[0], i[1]), 1, (0, 255, 0), 1)
+
+
+
+
+
+
+
+    #monté 2
+    #monté 1
+    #baisse vers l'intérieur 1
+    #baisse vers l'intérieur 2
 
 
 
