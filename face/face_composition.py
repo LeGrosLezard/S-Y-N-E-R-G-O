b532beff6fade308216ae4d2e7ec491c23eb6b34
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-27 00:11:06 +0100

    Add files via upload

---
+++ face/face_composition.py
@@ -0,0 +1,17 @@
+"""Here analysis"""
+
+
+def on_eyes(landmarks):
+    on_eyes_points = {"onEye1":(17, 21), "onEye2":(22, 26)}
+    
+    #monté
+    #baisse vers l'intérieur
+
+
+
+
+
+
+
+
+
---
+++ face/faces_movement.py
@@ -0,0 +1,5 @@
+from face_composition import on_eyes
+
+
+def face_movement(landmarks):
+    on_eyes(landmarks)
