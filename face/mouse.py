17879416f15b10c70776a842a8b3dd1da6c9cfe3
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-29 04:07:53 +0100

    Update mouse.py

--- face/mouse.py
+++ face/mouse.py
@@ -22,10 +22,10 @@ def mouse(landmarks, frame, head_box):
     mouse_points = make_landmarks_points(landmarks, mouse)
 
 
-    x, y, w, h = head_box
+    x1, y1, w1, h1 = head_box
 
-    cv2.circle(frame, (mouse_points[0][0], mouse_points[0][1]), 0, (255, 255, 255), 0)
-    cv2.circle(frame, (mouse_points[6][0], mouse_points[6][1]), 0, (255, 255, 255), 0)
+    cv2.circle(frame, (mouse_points[0][0], mouse_points[0][1]), 0, (0, 255, 0), 0)
+    cv2.circle(frame, (mouse_points[6][0], mouse_points[6][1]), 0, (255, 0, 0), 0)
 
 
 
@@ -36,22 +36,52 @@ def mouse(landmarks, frame, head_box):
 
     mouse = right_mid_mouse + left_mid_mouse
 
-    if mouse > w * 0.43:
-        print("sourire")
 
 
     x, y, w, h = cv2.boundingRect(mouse_points)
-    cv2.rectangle(frame, (x-5, y-5), (x+w + 5, y+h+5), (0, 255, 0), 1)
+    #cv2.rectangle(frame, (x-5, y-5), (x+w + 5, y+h+5), (0, 255, 0), 1)
 
-    print(x, x+w, w)
+
 
     crop = frame[y-5:y+h+5, x-5:x+w+5]
-    crop = cv2.cvtColor(crop,cv2.COLOR_RGB2GRAY)
+    
 
-    white = [(i, j) for i in range(crop.shape[0])
-             for j in range(crop.shape[1]) if crop[i, j] == 255]
+    green = [(i, j) for i in range(crop.shape[0])
+             for j in range(crop.shape[1]) if crop[i, j][0] == 0 and
+             crop[i, j][1] == 255 and crop[i, j][2] == 0][0]
 
-    print(white)
+    blue = [(i, j) for i in range(crop.shape[0])
+             for j in range(crop.shape[1]) if crop[i, j][0] == 255 and
+             crop[i, j][1] == 0 and crop[i, j][2] == 0][0]
+
+
+
+
+    left_width, left_height = blue[1], blue[0]
+    right_width, right_height = green[1], green[0]
+
+    height_crop, width_crop = crop.shape[:2]
+
+
+
+    print(right_height, left_height, height_crop)
+
+    a = ""
+    b = ""
+
+    if right_height <= int(height_crop * 0.27):
+        a = "droite"
+        print("droite")
+    if left_height <= int(height_crop * 0.27):
+        b = "gauche"
+        print("gauche")
+
+    if mouse > w1 * 0.41:
+        print("sourire 22 % car 17 lettre, sourire, tic, rapprochement, baillement")
+ 
+    if a == "droite" and b == "gauche":
+        print("sourire")
+    
 
     cv2.imshow("aa", crop)
 
@@ -75,14 +105,10 @@ def mouse(landmarks, frame, head_box):
     print("")
     
 
-    #sourrire
-        #dans le cas ou le mec cris ? truk tonalité de voix
-
 
     #sourire coté
 
-        #distance entre les deux extreémité etirement
-        #distance avec bas tete ratio
+        #quand la tete est incliné ca marche plus va falloir mettre l'inclinaison
 
 
     #boude
