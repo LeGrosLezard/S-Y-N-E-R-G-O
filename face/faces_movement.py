22daff3e7ea8ed1e18b3c8b1a55bb21932affdc3
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-30 00:03:46 +0100

    Update faces_movement.py

--- face/faces_movement.py
+++ face/faces_movement.py
@@ -1,5 +1,13 @@
 from on_eyes import on_eyes
-
+from mouse import mouse
 
 def face_movements(landmarks, frame, head_box):
-    on_eyes(landmarks, frame, head_box)
+
+    #Recuperate On eye movements
+    on_eye_movemement = on_eyes(landmarks, frame, head_box)
+    #if on_eye_movemement[0] != "": print(on_eye_movemement[0])
+    #if on_eye_movemement[1] != "": print(on_eye_movemement[1])
+
+    smyle, side_smyle = mouse(landmarks, frame, head_box)
+    if smyle != "": print(smyle)
+    if side_smyle != "": print(side_smyle)
