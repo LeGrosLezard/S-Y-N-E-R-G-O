e27cf766fe2b23e3d64f266b1629f1d5473c3abd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-30 16:35:32 +0100

    Update faces_movement.py
    sauf que le mouvement de la tete defonce le truk

--- face/faces_movement.py
+++ face/faces_movement.py
@@ -28,41 +28,107 @@ def make_contour_by_range_NONE(points, color, frame):
     cv2.drawContours(frame, [area], 0, color, 1)
 
 
+def make_mask_area(area, gray, frame, sub):
+
+    height, width = gray.shape[:2]
+    nb = 0
+    copy = frame.copy()
+    black_frame = np.zeros((height, width), np.uint8)
+    mask = np.full((height, width), 255, np.uint8)
+    cv2.fillPoly(mask, [area], (0, 0, 255))
+    mask = cv2.bitwise_not(black_frame, gray.copy(), mask=mask)
+
+
+    x, y, w, h = cv2.boundingRect(area)
+
+    cropMask = mask[y-nb : (y+h)+nb, x-nb : (x+w)+nb]
+    cropImg = copy[y-nb : (y+h)+nb, x-nb : (x+w)+nb]
+    cropSub = sub[y-nb : (y+h)+nb, x-nb : (x+w)+nb]
+
+    for i in range(cropMask.shape[0]):
+        for j in range(cropMask.shape[1]):
+            if cropMask[i, j] > 240:
+                cropImg[i, j] = 255
+                cropSub[i, j] = 255
+
+    return cropMask, cropImg, cropSub
+
+
+
+def movement_detection(cropSub):
+    none_black = cv2.countNonZero(cropSub)
+    print(none_black)
+
+def head_tracker():
+    pass
+    #Ici on va suivre la tete
+    #du genre droite 2px droite 2px plus rien donc droit 2px ? 1px ?
+    #moindre jcrois
+
+
+def head_size():
+    pass
+    #ICI on récuepre les dimensions des zones des fois trop petit
+    #ou le truk deviens trop grand
+    #si oui on récupere une autre d
 
 
 AREA1 = []
+AREA1_SIZE = []
+COLOR_AREA1 = []
+
 AREA2 = []
 
-def face_area(frame, landmarks):
+def face_area(frame, landmarks, subtractor):
 
 
     global AREA1
     global AREA2
 
-    areas =  { "cheek2":[54, 13, 16, 28], "chin":[58, 56, 9, 7], "beet_eyes" :[21, 22, 28],
-               "chin1":[58, 7, 3, 48], "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28],
+    areas =  { "cheek2":[54, 13, 16, 28], "chin":[58, 56, 9, 7],
+               "beet_eyes" :[21, 22, 28], "chin1":[58, 7, 3, 48],
+               "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28],
                "noze_area":[28, 48, 54], "mouse":(48, 50, 52, 54, 56, 58),
                "leftEye":(0, 17, 21, 28), "rightEye":(22, 26, 16, 28)}
 
     areas2 = {"onEye1":[17, 22], "onEye2":[22, 27]}
 
 
-
- 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    sub = subtractor.apply(frame)
 
     if landmarks is not None:
 
         AREA1 = [make_contour(areas[k], (255,0,0), frame, landmarks)
                            for nb, k in enumerate(areas)]
 
+        a, b, c = make_mask_area(np.array(AREA1[5]), gray, frame, sub)
+        movement_detection(c)
+        cv2.imshow("a", a)
+        cv2.imshow("b", b)
+        cv2.imshow("c", c)
+
+
+
+
+
         AREA2 = [make_contour_by_range(areas2[k], (255,0,0), frame, landmarks)
                  for nb, k in enumerate(areas2)]
+
 
 
     else:
 
         for i in AREA1:
             make_contour_NONE(i, (0, 255, 0), frame)
+
+
+        a, b, c = make_mask_area(np.array(AREA1[5]), gray, frame, sub)
+        movement_detection(c)
+        cv2.imshow("a", a)
+        cv2.imshow("b", b)
+        cv2.imshow("c", c)
+
 
         for i in AREA2:
             make_contour_by_range_NONE(i, (0, 255, 0), frame)
