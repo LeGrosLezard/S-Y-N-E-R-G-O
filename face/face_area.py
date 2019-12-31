b1f76fa379b1b658ee7803a351a0343a37c4454c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-12-31 17:26:05 +0100

    Update face_area.py
    les petites zones dans un rectangle et normalement c bon

--- face/face_area.py
+++ face/face_area.py
@@ -1,77 +1,295 @@
 import cv2
 import numpy as np
 
+#================================================> AREA
 
-
-
-def make_contour(area, color, frame, landmarks):
+def make_contour(area, landmarks, frame):
     area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in area])
-    cv2.drawContours(frame, [area], 0, color, 1)
+    cv2.drawContours(frame, [area], 0, (0, 0, 255), 1)
 
     return area.tolist()
 
-def make_contour_by_range(area, color, frame, landmarks):
+
+def make_contour_by_range(area, landmarks, frame):
     area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(area[0], area[1])])
-    cv2.drawContours(frame, [area], 0, color, 1)
+    #cv2.drawContours(frame, [area], 0, (0, 0, 255), 1)
+
+    return area.tolist()
 
 
-def make_contour_NONE(points, color, frame):
+def make_contour_NONE(points, frame):
     area = np.array([points])
-    cv2.drawContours(frame, [area], 0, color, 1)
+    cv2.drawContours(frame, [area], -1, (0, 255, 0), 1)
+
+
+def make_contour_by_range_NONE(points, color, frame):
+    area = np.array([points])
+    #cv2.drawContours(frame, [area], 0, color, 1)
+
+
+def make_mask_area(area, gray, frame):
+
+
+    height, width = gray.shape[:2]
+
+    copy = frame.copy()
+    black_frame = np.zeros((height, width), np.uint8)
+    mask = np.full((height, width), 255, np.uint8)
+    cv2.fillPoly(mask, [area], (0, 0, 255))
+    mask = cv2.bitwise_not(black_frame, gray.copy(), mask=mask)
+
+    x, y, w, h = cv2.boundingRect(area)
+    cropMask = mask[y : (y+h), x : (x+w)]
+    cropMask = gray[y : (y+h), x : (x+w)]
+    return cropMask
+
+
+#================================================> AREA
+
+
+#================================================> NO AREA
+def head_tracker(head_box):
+    pass
+    #Ici on va suivre la tete
+    #du genre droite 2px droite 2px plus rien donc droit 2px ? 1px ?
+    #moindre jcrois
+
+
+def head_size():
+    pass
+    #ICI on récuepre les dimensions des zones des fois trop petit
+    #ou le truk deviens trop grand
+    #si oui on récupere une autre d
+
+#================================================> NO AREA
+
+#================================================> TREAT AREA
+
+
+def make_area(cropMask, AREAS_FRAME_0):
+    for i in cropMask:
+        AREAS_FRAME_0.append(i)
 
 
 
-CHEEK2 = []
-CHEEK1 = []
+NB_FRAME = 0
 
-def face_area(frame, landmarks):
+AREA_LANDMARKS_1 = []
+AREA_LANDMARKS_2 = []
 
-    global CHEEK2
-    global CHEEK1
-    areas =  { "cheek2":[54, 13, 16, 28], "chin":[58, 56, 9, 7], "beet_eyes" :[21, 22, 28],
-               "chin1":[58, 7, 3, 48], "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 0, 28],
-               "noze_area":[28, 48, 54], "mouse":(48, 50, 52, 54, 56, 58),
-               "onEye1":(17, 22), "onEye2":(22, 27),
-               "leftEye":(0, 17, 21, 28), "rightEye":(22, 26, 16, 28)}
+AREAS_FRAME_0 = []
+AREAS_FRAME_1 = []
 
 
+oo = []
+pp = []
 
-##    
-##    area_by_contour = [make_contour(areas[k], (0,255,0), frame, landmarks)
-##                       for nb, k in enumerate(areas)]
 
+def face_area(frame, landmarks, subtractor, head_box):
+
+    global NB_FRAME
+
+    global AREA_LANDMARKS_1
+    global AREA_LANDMARKS_2
+
+    global AREAS_FRAME_0
+    global AREAS_FRAME_1
+
+    global oo
+    global pp
+
+
+    areas =  { "cheek2":[54, 13, 15, 28], "chin":[58, 56, 9, 7], "beet_eyes" :[21, 22, 28],
+               "chin1":[58, 7, 3, 48], "chin2": [56, 54, 13, 9], "cheek1": [48, 3, 1, 28],
+               "noze_area":[28, 48, 54], "mouse":(48, 50, 52, 54, 56, 58), "leftEye":(1, 17, 21, 28),
+               "rightEye":(22, 26, 15, 28)}
+
+    areas2 = {"onEye1":[17, 22], "onEye2":[22, 27]}
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
 
 
     if landmarks is not None:
 
-        CHEEK2 = make_contour(areas["cheek2"], (255, 0, 0), frame, landmarks)
-
-        CHEEK1 = make_contour(areas["cheek1"], (255, 0, 0), frame, landmarks)
-
-        make_contour(areas["chin"], (255, 0, 0), frame, landmarks)
-        make_contour(areas["chin1"], (255, 0, 0), frame, landmarks)
-        make_contour(areas["chin2"], (255, 0, 0), frame, landmarks)
-
-        make_contour(areas["noze_area"], (255, 0, 0), frame, landmarks)
-        
-        make_contour(areas["beet_eyes"], (255, 0, 0), frame, landmarks)
 
 
-        make_contour(areas["leftEye"], (255, 0, 0), frame, landmarks)
-        make_contour(areas["rightEye"], (255, 0, 0), frame, landmarks)
+        #Recuperate area from dlib
+        AREA_LANDMARKS_1 = [make_contour(areas[k], landmarks, frame)
+                          for nb, k in enumerate(areas)]
 
-        make_contour_by_range(areas["onEye1"], (255, 0, 0), frame, landmarks)
-        make_contour_by_range(areas["onEye2"], (255, 0, 0), frame, landmarks)
+        #Make area from the face
+        cropMask = [make_mask_area(np.array(AREA_LANDMARKS_1[n]), gray, frame)
+                    for n in range(10)]
+
+
+
+        if NB_FRAME == 0:
+
+            for i in cropMask:
+                AREAS_FRAME_0.append(i)
+
+            NB_FRAME += 1
+
+            oo = cropMask[5]
+            cv2.imshow("oo", oo)
+
+
+
+
+        elif NB_FRAME == 1:
+
+            NB_FRAME = 0
+
+
+            #for i in cropMask:
+                #AREAS_FRAME_1.append(i)
+
+
+            pp = cropMask[5]
+
+            width, height = oo.shape[:2]
+            pp = cv2.resize(pp, (height, width))
+
+            diff = cv2.subtract(oo, pp)
+            ret,diff = cv2.threshold(diff,10,255,cv2.THRESH_BINARY)
+
+            cv2.imshow("pp", pp)
+            cv2.imshow("diff", np.array(diff))
 
             
-        make_contour(areas["mouse"], (255, 0, 0), frame, landmarks)
+            h,w = diff.shape[:2]
+
+            no_zero_px = cv2.countNonZero(diff)
+            print(h*w)
+            print(no_zero_px)
+
+
+
+
+            print("")
+
+
+
+
+
+
+
+
+
+
+##            counter = 0
+##            for frame1, frame2 in zip(AREAS_FRAME_0, AREAS_FRAME_1):
+##
+##                width, height = frame1.shape[:2]
+##                frame2 = cv2.resize(frame2, (height, width))
+##
+##                diff = cv2.subtract(frame1, frame2)
+##
+##
+##                dico_copy = list(areas.copy())
+##                
+##                print(dico_copy[counter])
+##
+##                cv2.imshow("frame1", frame1)
+##                cv2.imshow("frame2", frame2)
+##
+##
+##                cv2.imshow("diff", np.array(diff))
+##
+##
+##                h,w = diff.shape[:2]
+##                no_zero_px = cv2.countNonZero(diff)
+##
+##                print(no_zero_px)
+##
+##                
+##                cv2.waitKey(0)
+##
+##
+##
+##
+##
+##
+##                counter += 1
+##
+##
+##
+##
+##            AREAS_FRAME_0 = []
+##            AREAS_FRAME_1 = []
+
+
+
+
+
+
+
+##        AREA2 = [make_contour_by_range(areas2[k], (255,0,0), frame, landmarks)
+##                 for nb, k in enumerate(areas2)]
+
+
 
 
     else:
-        print(CHEEK2)
-        make_contour_NONE(CHEEK2, (0, 255, 0), frame)
-        print(CHEEK1)
-        make_contour_NONE(CHEEK1, (0, 255, 0), frame)
+
+
+
+
+        #Recuperate area from dlib
+        AREA_LANDMARKS_2 = [make_contour_NONE(i, frame)
+                            for i in AREA_LANDMARKS_1]
+
+
+
+
+        #Make area from the face
+        cropMask = [make_mask_area(np.array(AREA_LANDMARKS_1[n]), gray, frame)
+                    for n in range(10)]
+
+        if NB_FRAME == 0:
+
+            for i in cropMask:
+                AREAS_FRAME_0.append(i)
+
+            NB_FRAME += 1
+
+            oo = cropMask[5]
+            cv2.imshow("oo", oo)
+
+
+
+
+        elif NB_FRAME == 1:
+
+            NB_FRAME = 0
+
+
+            #for i in cropMask:
+                #AREAS_FRAME_1.append(i)
+
+
+            pp = cropMask[5]
+
+            width, height = oo.shape[:2]
+            pp = cv2.resize(pp, (height, width))
+
+            diff = cv2.subtract(oo, pp)
+            ret,diff = cv2.threshold(diff,10,255,cv2.THRESH_BINARY)
+
+            cv2.imshow("pp", pp)
+            cv2.imshow("diff", np.array(diff))
+
+            
+            h,w = diff.shape[:2]
+
+            no_zero_px = cv2.countNonZero(diff)
+            print(h*w)
+            print(no_zero_px)
+
+
+
+
+            print("")
 
 
 
@@ -83,21 +301,9 @@ def face_area(frame, landmarks):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+##        #Make area from the face
+##        cropMask = [make_mask_area(np.array(AREA_LANDMARKS_1[n]), gray, frame)
+##                    for n in range(10)]
 
 
 
