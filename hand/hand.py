4a358ed8804c0b3cba94b91192f0bb5092eabedd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-05 22:37:37 +0100

    Update hand.py

--- hand/hand.py
+++ hand/hand.py
@@ -16,7 +16,51 @@ from skimage.filters import rank
 from skimage.color import rgb2gray
 from skimage.util import img_as_ubyte
 
+def head_hand_distance_possibility(head_box, frame):
 
+    if head_box != None:
+
+        x, y, w, h = head_box
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
+
+        center_x = int(x+w / 2)
+        center_y = int(y+h * 2)
+
+        cv2.circle(frame, (center_x, center_y), 1, (0, 0, 255), 1)
+
+        #hight head to cm
+        head = (y+h) / 37.79527559055
+        #ratio 30 mean head / current head
+        head = 30 / head
+
+        #64 + 10 approx arm / ratio head
+        arm = (64 + 10) / head
+        #arm to pixel
+        arm = int(arm * 37.79527559055)
+        #region arm possible
+        cv2.circle(frame, (center_x, center_y), arm, (0, 0, 255), 1)
+
+
+
+def hand_possibility(hand, head, frame):
+
+    x, y, w, h = hand
+
+    
+    center_x = int( (x+w) / 2)
+    center_y = int( (y+h) / 2)
+    #cv2.circle(frame, (center_x, center_y), 1, (255, 0, 0), 5)
+
+    #head ratio
+    x1, y1, w1, h1 = head
+    head = (y1+h1) / 37.79527559055
+    #ratio 30 mean head / current head
+    head = 30 / head
+
+    #arm distance 20 cm possibility to move
+    arm = int( (20 * 37.79527559055) / head)
+    cv2.circle(frame, (center_x, center_y), arm, (255, 0, 0), 5)
+    
 
 # Load a frozen infrerence graph into memory
 def load_inference_graph(path_to_ckpt):
@@ -64,109 +108,37 @@ def hands_detections(scores, boxes, frame):
 
 
 
-def skin_detector(crop):
+def skin_detector(region, frame):
+
+    nb = 35
+
+    crop = frame[region[1] - nb:region[3] + nb, region[0] - nb:region[2] + nb]
+
     min_YCrCb = np.array([0,140,85],np.uint8)
     max_YCrCb = np.array([240,180,130],np.uint8)
     imageYCrCb = cv2.cvtColor(crop, cv2.COLOR_BGR2YCR_CB)
     skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 1)
 
     skinYCrCb = cv2.bitwise_and(crop, crop, mask = skinMask)
 
-    return skinYCrCb
+    gray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
 
 
+    contours = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = sorted(contours, key=cv2.contourArea)
 
+    cv2.fillPoly(crop, [contours[-2]], (79, 220, 25))
+    cv2.drawContours(crop, [contours[-2]], -1 , (0, 0, 0), 3)
 
-def head_hand_distance_possibility(head_box, frame):
+    return opening
 
-    if head_box != None:
-
-        x, y, w, h = head_box
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
-
-        center_x = int(x+w / 2)
-        center_y = int(y+h * 2)
-
-        cv2.circle(frame, (center_x, center_y), 1, (0, 0, 255), 1)
-
-        #hight head to cm
-        head = (y+h) / 37.79527559055
-        #ratio 30 mean head / current head
-        head = 30 / head
-
-        #64 + 10 approx arm / ratio head
-        arm = (64 + 10) / head
-        #arm to pixel
-        arm = int(arm * 37.79527559055)
-        #region arm possible
-        cv2.circle(frame, (center_x, center_y), arm, (0, 0, 255), 1)
-
-
-
-def hand_possibility(hand, head, frame):
-
-    x, y, w, h = hand
-
-    
-    center_x = int( (x+w) / 2)
-    center_y = int( (y+h) / 2)
-    cv2.circle(frame, (center_x, center_y), 1, (255, 0, 0), 5)
-
-    #head ratio
-    x1, y1, w1, h1 = head
-    head = (y1+h1) / 37.79527559055
-    #ratio 30 mean head / current head
-    head = 30 / head
-
-    #arm distance 20 cm possibility to move
-    arm = int( (20 * 37.79527559055) / head)
-    cv2.circle(frame, (center_x, center_y), arm, (255, 0, 0), 5)
-
-
-
-def segmentation(region, frame):
-
-    roi = frame[region[1]:region[3], region[0]:region[2]]
-    height, width = roi.shape[:2]
-    gray = rgb2gray(roi)
-    image = img_as_ubyte(gray)
-    markers = rank.gradient(image, disk(5)) < 20
-    markers = ndi.label(markers)[0]
-    gradient = rank.gradient(image, disk(2))
-
-    labels = watershed(gradient, markers)
-
-
-     
-    fig = plt.figure()
-    fig.set_size_inches(1, 1, forward=False)
-    axes = plt.Axes(fig, [0., 0., 1., 1.])
-    axes.set_axis_off()
-    fig.add_axes(axes)
-
-
-    axes.imshow(image, cmap=plt.cm.gray, interpolation="nearest")
-
-    axes.imshow(labels, cmap=plt.cm.get_cmap("Spectral"), interpolation ="nearest",
-                 alpha=0.1)
-
-
-    plt.axis("off")
-
-
-
- 
-    fig.canvas.draw()
-
-    img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8,
-            sep='')
-    img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
-    img = cv2.resize(img, (width, height))
-    cv2.imshow("plot",img)
 
 
 
@@ -177,8 +149,8 @@ def segmentation(region, frame):
 def hand(frame, detection_graph, sess, head_box):
 
 
+    #head_hand_distance_possibility(head_box, frame)
 
-    head_hand_distance_possibility(head_box, frame)
 
 
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -187,45 +159,13 @@ def hand(frame, detection_graph, sess, head_box):
 
     detections = hands_detections(scores, boxes, frame)
 
-    for hand in detections:
-        cv2.rectangle(frame, (hand[0], hand[1]),
-                      (hand[2], hand[3]), (79, 220, 25), 4)
-
-        hand_possibility(hand, head_box, frame)
-
-        segmentation(hand, frame)
+    for nb, hand in enumerate(detections):
 
 
+        crop = skin_detector(hand, frame)
+        cv2.imshow(str(nb), crop)
 
+        #cv2.rectangle(frame, (hand[0], hand[1]), (hand[2], hand[3]), (79, 220, 25), 4)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #hand_possibility(hand, head_box, frame)
 
