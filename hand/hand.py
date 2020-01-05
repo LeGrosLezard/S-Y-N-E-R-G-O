f352eaecb3fbfc021343e4291d66c1f88c12d250
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-05 19:49:52 +0100

    Update hand.py
    bah jy arrive pas

--- hand/hand.py
+++ hand/hand.py
@@ -3,6 +3,21 @@ from numpy import expand_dims, squeeze
 import numpy as np
 import cv2
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sys import exit
+from scipy import ndimage as ndi
+from skimage.morphology import watershed, disk
+from skimage import data
+from skimage.io import imread
+from skimage.filters import rank
+from skimage.color import rgb2gray
+from skimage.util import img_as_ubyte
+
+
+
 # Load a frozen infrerence graph into memory
 def load_inference_graph(path_to_ckpt):
     # load frozen tensorflow model into memory
@@ -91,6 +106,70 @@ def head_hand_distance_possibility(head_box, frame):
 
 
 
+def hand_possibility(hand, head, frame):
+
+    x, y, w, h = hand
+
+    
+    center_x = int( (x+w) / 2)
+    center_y = int( (y+h) / 2)
+    cv2.circle(frame, (center_x, center_y), 1, (255, 0, 0), 5)
+
+    #head ratio
+    x1, y1, w1, h1 = head
+    head = (y1+h1) / 37.79527559055
+    #ratio 30 mean head / current head
+    head = 30 / head
+
+    #arm distance 20 cm possibility to move
+    arm = int( (20 * 37.79527559055) / head)
+    cv2.circle(frame, (center_x, center_y), arm, (255, 0, 0), 5)
+
+
+
+def segmentation(region, frame):
+
+    roi = frame[region[1]:region[3], region[0]:region[2]]
+    height, width = roi.shape[:2]
+    gray = rgb2gray(roi)
+    image = img_as_ubyte(gray)
+    markers = rank.gradient(image, disk(5)) < 20
+    markers = ndi.label(markers)[0]
+    gradient = rank.gradient(image, disk(2))
+
+    labels = watershed(gradient, markers)
+
+
+     
+    fig = plt.figure()
+    fig.set_size_inches(1, 1, forward=False)
+    axes = plt.Axes(fig, [0., 0., 1., 1.])
+    axes.set_axis_off()
+    fig.add_axes(axes)
+
+
+    axes.imshow(image, cmap=plt.cm.gray, interpolation="nearest")
+
+    axes.imshow(labels, cmap=plt.cm.get_cmap("Spectral"), interpolation ="nearest",
+                 alpha=0.1)
+
+
+    plt.axis("off")
+
+
+
+ 
+    fig.canvas.draw()
+
+    img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8,
+            sep='')
+    img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    img = cv2.resize(img, (width, height))
+    cv2.imshow("plot",img)
+
+
+
 
 
 
@@ -112,11 +191,9 @@ def hand(frame, detection_graph, sess, head_box):
         cv2.rectangle(frame, (hand[0], hand[1]),
                       (hand[2], hand[3]), (79, 220, 25), 4)
 
+        hand_possibility(hand, head_box, frame)
 
-
-
-
-
+        segmentation(hand, frame)
 
 
 
