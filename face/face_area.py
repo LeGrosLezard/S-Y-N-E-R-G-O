64dab30387984ae7093ca957b832f43a1483429e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-05 04:35:11 +0100

    Update face_area.py
    faut dire si la tete se tourne ect ne pas detecter ect ect

--- face/face_area.py
+++ face/face_area.py
@@ -4,42 +4,54 @@ import numpy as np
 #================================================> AREA
 
 def make_contour(area, landmarks, frame):
-    area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in area])
-    cv2.drawContours(frame, [area], 0, (0, 0, 255), 1)
+    """Here we recuperate areas formed by dlib points
+    and make a beautiful display !"""
 
-    return area.tolist()
+    area_landmarks = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in area])
+    cv2.drawContours(frame, [area_landmarks], 0, (0, 0, 255), 1)
+
+    if area == (39, 27, 33, 31):
+        area_landmarks = area_landmarks.tolist()
+        area_landmarks[-1][0] = area_landmarks[-1][0] - 20
+        return area_landmarks
+
+    elif area == (42, 27, 33, 35):
+        area_landmarks = area_landmarks.tolist()
+        area_landmarks[-1][0] = area_landmarks[-1][0] + 20
+        return area_landmarks
 
 
-def make_contour_by_range(area, landmarks, frame):
-    area = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in range(area[0], area[1])])
-    cv2.drawContours(frame, [area], 0, (0, 255, 0), 1)
+    elif area == (17, 18, 19, 20, 21, 38, 36) or area == (22, 23, 24, 25, 26, 44, 42):
+        area_landmarks = area_landmarks.tolist()
+        for i in range(len(area_landmarks)):
+            area_landmarks[i][1] = area_landmarks[i][1] - 10
+        return area_landmarks
 
-    return area.tolist()
 
+
+    return area_landmarks.tolist()
 
 
 def make_contour_NONE(points, frame):
+    """Here we draws contours from dlib points savegarde"""
+
     area = np.array([points])
     cv2.drawContours(frame, [area], -1, (0, 255, 0), 1)
 
 
-def make_contour_NONE2(points, copy):
 
-    area = np.array([points])
-    cv2.drawContours(copy, [area], -1, (0, 255, 0), 1)
-
-
-def make_contour_by_range_NONE(points, color, frame):
-    area = np.array([points])
-    cv2.drawContours(frame, [area], 0, color, 1)
 
 
 def display(area, landmarks, frame):
+    """Here we filled the contour in case of a touch"""
+
     area = np.array([landmarks])
     cv2.fillPoly(frame, [area], (255, 0, 0))
 
 
 def make_mask_area(area, gray, frame):
+    """Here we make a mask of the area from the frame"""
+
     nb = 5
 
     height, width = gray.shape[:2]
@@ -54,17 +66,6 @@ def make_mask_area(area, gray, frame):
 
     x, y, w, h = cv2.boundingRect(area)
     cropMask = mask[y-nb : (y+h)+nb, x-nb : (x+w)+nb]
-    cropMask1 = frame[y : (y+h), x : (x+w)]
-    cropMask2 = gray[y : (y+h), x : (x+w)]
-
-
-##    a = 0; n=0
-##    for i in range(cropMask2.shape[0]):
-##        for j in range(cropMask2.shape[1]):
-##            a += cropMask2[i, j]
-##            n += 1
-
-
 
     return cropMask
 
@@ -90,6 +91,7 @@ def head_size():
 
 #================================================> TREAT AREA
 def recuperate_area_zone(zone, frame):
+    """We recuperate the highter contour"""
 
     contours = cv2.findContours(zone, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
     contours = sorted(contours, key=cv2.contourArea)
@@ -102,10 +104,14 @@ def recuperate_area_zone(zone, frame):
 
 
 
-def zone_detected(mask, frame, diviser, zone, landmarks, area, msg, nb, no_area):
+def zone_detected(mask, frame, diviser, zone, landmarks, area, msg, nb, no_area, out):
+    """We make an average of all contours. If we have a contour who's smaller than
+    the mean we detected it
+    nb we make area/contour dont know why but we'll improve it"""
+
+
 
     increment = False
-
     contour, size_crop = recuperate_area_zone(mask[no_area], frame)
 
     try:
@@ -117,7 +123,7 @@ def zone_detected(mask, frame, diviser, zone, landmarks, area, msg, nb, no_area)
     #if  diviser[no_area] > 0 and (size_crop/contour) > zone[no_area] / diviser[no_area] + nb:
     if  diviser[no_area] > 0 and contour > 0 and\
        (size_crop/contour) > (zone[no_area] / diviser[no_area]) + nb:
-        print(msg)
+        out += msg
         display(area, landmarks[no_area], frame)
         increment = True
 
@@ -125,24 +131,25 @@ def zone_detected(mask, frame, diviser, zone, landmarks, area, msg, nb, no_area)
         zone[no_area] += size_crop/contour
         diviser[no_area] += 1
 
-
+    return out
 
 
 
 
         
-
+#Save position of dlib points
 AREA_LANDMARKS_1 = []
 
+#Contours from dlib points
+ZONE = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#Number of increment for make average
+ZONE_INCREMENT = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-ZONE = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-ZONE_INCREMENT = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-NUMBER_MSG = [("joue gauche", 0.65), ("menton", 1), ("entre oeil",1.2),
-              ("chin 1", 2), ("chin2", 2), ("joue droite", 0.65), ("angel finguer", 1),
-              ("mouse", 30), ("oeil gauche", 2), ("oeil droit", 2)]
-
-
+#Message and number: ex:   current joue gauche == 45 and mean == 50, joue gauche > mean + 4
+NUMBER_MSG = [("joue gauche", 4), ("menton",4), ("entre oeil",4.5),
+              ("chin 1", 4), ("chin2", 4), ("joue droite", 4), ("angel finguer", 5),
+              ("noze_area1", 1), ("noze_area2", 1), ("oeil gauche", 2), ("oeil droit", 2),
+              ("onEye1", 2), ("onEye2", 2)]
 
 
 def face_area(frame, landmarks, head_box):
@@ -152,82 +159,52 @@ def face_area(frame, landmarks, head_box):
     global NUMBER_MSG
     global ZONE
 
+    out = ""
 
     areas =  { "cheek2":[54, 13, 15, 28], "chin":[58, 57, 56, 10,  9, 8, 7, 6], "beet_eyes" :[20, 23, 42, 39],
                "chin1":[58, 7, 6, 5, 4, 3, 48, 59], "chin2": [56, 55, 54, 13, 12, 11 ,10, 9], "cheek1": [48, 3, 1, 28],
-              "angel_finger":[31, 49, 53, 35], "mouse":(48, 50, 52, 54, 56, 58), "leftEye":(1, 17, 21, 28),
-               "rightEye":(22, 26, 15, 28)}
-
-    #noze_area:[41, 46, 54, 48]
-    #mouse:(48, 50, 52, 54, 56, 58)
-    areas2 = {"onEye1":[17, 22], "onEye2":[22, 27]}
+              "angel_finger":[31, 49, 53, 35], "noze_area1":(39, 27, 33, 31), "noze_area2":(42, 27, 33, 35),
+               "leftEye":(1, 17, 21, 28), "rightEye":(22, 26, 15, 28), "onEye1":(17, 18, 19, 20, 21, 38, 36),
+               "onEye2":(22, 23, 24, 25, 26, 44, 42)}
 
 
     blur = cv2.bilateralFilter (frame, 25, 75, 75)
     gray = cv2.cvtColor(blur,cv2.COLOR_RGB2GRAY)
-
-    
     th2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
                                 cv2.THRESH_BINARY,11,2)
 
 
-    
-
-
-
-    cv2.imshow("th2", th2)
-
-
-    
     if landmarks is not None:
 
+        #Make a save
         AREA_LANDMARKS_1.append([make_contour(areas[k], landmarks, frame)
                                  for nb, k in enumerate(areas)])
-
+        #Recuperate mask of the region
         cropMask = [make_mask_area(np.array(AREA_LANDMARKS_1[-1][n]), th2, frame)
-                                for n in range(10)]
+                                for n in range(len(areas))]
+        #Detect if current contour < average contour
+        out = [(zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE, AREA_LANDMARKS_1[-1],
+                        areas[k], NUMBER_MSG[nb][0], NUMBER_MSG[nb][1], nb, out)) for nb, k in enumerate(areas)]
 
 
-        
-
-
-        [(zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE, AREA_LANDMARKS_1[-1],
-                        areas[k], NUMBER_MSG[nb][0], NUMBER_MSG[nb][1], nb)) for nb, k in enumerate(areas)]
-
-
-
-        a = 6
-        cv2.imshow("daz", cropMask[a])
-        zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE,
-                      AREA_LANDMARKS_1[-1], areas["angel_finger"], NUMBER_MSG[a][0], NUMBER_MSG[a][1], a)
 
 
     else:
-
+        #No landmarks
         if len(AREA_LANDMARKS_1) > 0:
 
-            
-            
+            #Make contours from saved
             cropMask = [make_contour_NONE(i, frame) for i in AREA_LANDMARKS_1[-1]]
             cropMask = [make_mask_area(np.array(AREA_LANDMARKS_1[-1][n]), th2, frame)
-                        for n in range(10)]
+                        for n in range(len(areas))]
+
+            out = [(zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE, AREA_LANDMARKS_1[-1],
+                            areas[k], NUMBER_MSG[nb][0], NUMBER_MSG[nb][1], nb, out)) for nb, k in enumerate(areas)]
 
 
 
-            [(zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE, AREA_LANDMARKS_1[-1],
-                            areas[k], NUMBER_MSG[nb][0], NUMBER_MSG[nb][1], nb)) for nb, k in enumerate(areas)]
-
-            if cropMask != []:
-
-                a = 6
-                cv2.imshow("daz", cropMask[a])
-                zone_detected(cropMask, frame, ZONE_INCREMENT, ZONE,
-                              AREA_LANDMARKS_1[-1], areas["angel_finger"],
-                              NUMBER_MSG[a][0], NUMBER_MSG[a][1], a)
-
-
-    print("")
-
+    if out != "" : print(out)
+    return out
 
 
 
