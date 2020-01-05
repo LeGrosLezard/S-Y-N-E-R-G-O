6d9dbcfbce9f6bf431c2533707705311a15a03f6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-05 19:24:03 +0100

    Update and rename main.py to hand.py

---
+++ hand/hand.py
@@ -0,0 +1,150 @@
+import tensorflow as tf
+from numpy import expand_dims, squeeze
+import numpy as np
+import cv2
+
+# Load a frozen infrerence graph into memory
+def load_inference_graph(path_to_ckpt):
+    # load frozen tensorflow model into memory
+    detection_graph = tf.Graph()
+
+    with detection_graph.as_default():
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.io.gfile.GFile(path_to_ckpt, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.compat.v1.Session(graph=detection_graph)
+    return detection_graph, sess
+
+
+
+
+def detect_objects(image_np, detection_graph, sess):
+
+
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    image_np_expanded = expand_dims(image_np, axis=0)
+
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+
+    (boxes, scores) = sess.run([detection_boxes, detection_scores],
+                               feed_dict={image_tensor: image_np_expanded})
+
+    return squeeze(boxes), squeeze(scores)
+
+
+def hands_detections(scores, boxes, frame):
+    """We detect hands, by position we define if it are to left or to right.
+    Recup it for history if we have impossible detections"""
+
+    height, width = frame.shape[:2]
+    detections = [(int(boxes[i][1] * width), int(boxes[i][0] * height),
+                   int(boxes[i][3] * width), int(boxes[i][2] * height))
+                   for i in range(2) if (scores[i] > 0.10)]
+
+    return detections
+
+
+
+
+def skin_detector(crop):
+    min_YCrCb = np.array([0,140,85],np.uint8)
+    max_YCrCb = np.array([240,180,130],np.uint8)
+    imageYCrCb = cv2.cvtColor(crop, cv2.COLOR_BGR2YCR_CB)
+    skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+
+    skinYCrCb = cv2.bitwise_and(crop, crop, mask = skinMask)
+
+    return skinYCrCb
+
+
+
+
+def head_hand_distance_possibility(head_box, frame):
+
+    if head_box != None:
+
+        x, y, w, h = head_box
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
+
+        center_x = int(x+w / 2)
+        center_y = int(y+h * 2)
+
+        cv2.circle(frame, (center_x, center_y), 1, (0, 0, 255), 1)
+
+        head = (y+h) / 37.79527559055
+        head = 30 / head
+
+        arm = 64 / head
+        arm = int(arm * 37.79527559055)
+
+        cv2.circle(frame, (center_x, center_y), arm, (0, 0, 255), 1)
+
+
+
+
+
+
+
+def hand(frame, detection_graph, sess, head_box):
+
+
+
+    head_hand_distance_possibility(head_box, frame)
+
+
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    boxes, scores = detect_objects(frameRGB, detection_graph, sess)
+
+
+    detections = hands_detections(scores, boxes, frame)
+
+    for hand in detections:
+        cv2.rectangle(frame, (hand[0], hand[1]),
+                      (hand[2], hand[3]), (79, 220, 25), 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
