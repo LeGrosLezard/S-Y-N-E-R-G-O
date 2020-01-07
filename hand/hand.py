490034660c7f1df862ce093cbe9adbbad354e94c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-07 03:01:07 +0100

    Update hand.py
    je veux un contour tout droit mais j'y arrive pas... et si y'a un trou que j'ai pas comblé c'est foutu

--- hand/hand.py
+++ hand/hand.py
@@ -100,11 +100,11 @@ def hands_detections(scores, boxes, frame):
     return detections
 
 
-def make_line(thresh):
+def make_line(thresh, margin):
     """We make line for detect more than one area
     with border, on eyelashes is paste to the border"""
 
-    margin = 2
+
 
     cv2.line(thresh, (0, 0), (0, thresh.shape[0]), (255, 255, 255), margin)
     cv2.line(thresh, (0, 0), (thresh.shape[1], 0), (255, 255, 255), margin)
@@ -113,13 +113,15 @@ def make_line(thresh):
 
 
 
+
 def skin_detector(region, frame):
 
     nb = 35
 
-
     crop = frame[region[1] - nb:region[3] + nb, region[0] - nb:region[2] + nb]
     crop_convex = crop.copy()
+
+    #crop = cv2.blur(crop, (5, 5))
 
     min_YCrCb = np.array([0,140,85],np.uint8)
     max_YCrCb = np.array([240,180,130],np.uint8)
@@ -127,53 +129,116 @@ def skin_detector(region, frame):
     skinRegionYCrCb = cv2.inRange(imageYCrCb,min_YCrCb,max_YCrCb)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 1)
+
+    skinMask = cv2.dilate(skinRegionYCrCb, kernel, iterations = 2)
+    skinMask = cv2.morphologyEx(skinMask, cv2.MORPH_CLOSE, kernel)
 
     skinYCrCb = cv2.bitwise_and(crop, crop, mask = skinMask)
 
+
     gray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-
-    kernel = np.ones((3,3),np.uint8)
-    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
-
-    make_line(opening)
-
-    contours = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
     contours = sorted(contours, key=cv2.contourArea)
 
-    cv2.fillPoly(crop, [contours[-2]], (79, 220, 25))
-    cv2.drawContours(crop, [contours[-2]], -1 , (0, 0, 0), 1)
+    for nb, cnts in enumerate(contours):
+        if nb < len(contours) - 1:
+            hull = cv2.convexHull(cnts)
+            cv2.drawContours(skinYCrCb, [hull], -1, (0, 0, 255), 1)
+            cv2.fillPoly(skinYCrCb, [hull], (0, 0, 0))
+
+
+
+    gray = cv2.cvtColor(skinYCrCb, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    make_line(thresh, 5)
+
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = sorted(contours, key=cv2.contourArea)
+
+    for nb, cnts in enumerate(contours):
+        if nb <  len(contours) - 2:
+            cv2.fillPoly(thresh, [cnts], (255, 255, 255))
+
+
+    cv2.imshow("thresh", thresh)
+
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = sorted(contours, key=cv2.contourArea)
+    for nb, cnts in enumerate(contours):
+        if nb <  len(contours) - 2:
+            cv2.fillPoly(thresh, [cnts], (0, 0, 0))
+
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    morph_img = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    contours = cv2.findContours(morph_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = sorted(contours, key=cv2.contourArea)
+    for nb, cnts in enumerate(contours):
+        if nb <  len(contours) - 2:
+            cv2.fillPoly(morph_img, [cnts], (255, 255, 255))
+
+
+    contours = cv2.findContours(morph_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    contours = sorted(contours, key=cv2.contourArea)
+
+    [cv2.drawContours(morph_img, [i], -1 , (255, 255, 255), 2) for i in contours]
+
+
+    cv2.imshow("morph_img", morph_img)
 
 
 
 
-    acc = 0.02 * cv2.arcLength(contours[-2], True)
-    approx = cv2.approxPolyDP(contours[-2], acc, True)
-
-    hull = cv2.convexHull(approx, returnPoints=False)
-    hull_draw = cv2.convexHull(approx)
-
-    cv2.drawContours(crop_convex, [hull_draw], -1 , (255, 0, 0), 1)
-    cv2.drawContours(crop_convex, [approx], -1 , (0, 0, 255), 1)
 
 
 
+##
+##
+##
+##
+##
+##
+##
+##
+##    kernel = np.ones((3,3),np.uint8)
+##    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 1)
+##
+##    make_line(opening)
+##
+##    contours = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+##    contours = sorted(contours, key=cv2.contourArea)
+##
+##    cv2.fillPoly(crop, [contours[-2]], (79, 220, 25))
+##    cv2.drawContours(crop, [contours[-2]], -1 , (0, 0, 0), 1)
+##
+##
+##
+##    acc = 0.02 * cv2.arcLength(contours[-2], True)
+##    approx = cv2.approxPolyDP(contours[-2], acc, True)
+##
+##    hull = cv2.convexHull(approx, returnPoints=False)
+##    hull_draw = cv2.convexHull(approx)
+##
+##    cv2.drawContours(crop_convex, [hull_draw], -1 , (255, 0, 0), 1)
+##    cv2.drawContours(crop_convex, [approx], -1 , (0, 0, 255), 1)
+##
+##
+##    cv2.imshow("crop_convex", crop_convex)
+##
+##
+##
+##    M = cv2.moments(contours[-2])
+##    cX = int(M["m10"] / M["m00"])
+##    cY = int(M["m01"] / M["m00"])
+##
+##
+##    cv2.circle(crop_convex, (cX, cY), 1, (0, 0, 255), 2)
+##
 
 
-    #skeletton
-    M = cv2.moments(contours[-2])
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
 
-
-    cv2.circle(crop_convex, (cX, cY), 1, (0, 0, 255), 2)
-
-    cv2.imshow("crop_convex", crop_convex)
-
-
-    return opening, crop
 
 
 
@@ -193,7 +258,7 @@ def hand(frame, detection_graph, sess, head_box):
 
     for nb, hand in enumerate(detections):
 
-        crop_thresh, crop_image = skin_detector(hand, frame)
+        skin_detector(hand, frame)
 
         #cv2.rectangle(frame, (hand[0], hand[1]), (hand[2], hand[3]), (79, 220, 25), 4)
         #hand_possibility(hand, head_box, frame)
