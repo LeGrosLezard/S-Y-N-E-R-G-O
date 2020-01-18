895a10a49799a26db5f2a470161cd1c9af6bb0d2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-18 16:46:26 +0100

    Add files via upload
    en vrai c'est le dawa la dedans

---
+++ fingers/thumb_finger.py
@@ -0,0 +1,15 @@
+import cv2
+from scipy.spatial import distance as dist
+
+
+def printing(sorted_fingers):
+    print("\nTHUMB FINGER", sorted_fingers[0], "\n")
+
+
+def thumb_finger(sorted_fingers):
+
+    printing(sorted_fingers)
+
+
+    for i in sorted_fingers:
+        print(i)
