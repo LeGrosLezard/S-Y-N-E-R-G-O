fad319a3683cfc13d26f703d06a53b79a74fb160
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-21 23:14:20 +0100

    Add files via upload

---
+++ to_csv/c_csv_files/theorique/data_predict/thumb_finger.py
@@ -0,0 +1,20 @@
+import cv2
+from scipy.spatial import distance as dist
+
+
+def printing(sorted_fingers):
+    print("\nTHUMB FINGER", sorted_fingers, "\n")
+
+
+
+
+
+def thumb_finger(sorted_fingers):
+
+    printing(sorted_fingers)
+
+
+
+
+    for i in sorted_fingers:
+        print(i)
