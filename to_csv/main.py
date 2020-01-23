5e1e7053b43f32387f9636f0e8d6f3d946a9047e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-24 00:43:50 +0100

    Add files via upload

--- to_csv/main.py
+++ to_csv/main.py
@@ -34,6 +34,21 @@ def move_picture(sorted_fingers, path, image):
     shutil.move(path, path_ok_picture)
 
 
+def rename_picture_to_label(label, picture):
+    path_ok_picture = r"C:\Users\jeanbaptiste\Desktop\dougy_petits_pecs\b_stock_image_5_pts\{}"
+
+    print(path_ok_picture.format(picture))
+    print(path_ok_picture.format(str(label) + ".jpg"))
+    os.rename(path_ok_picture.format(picture), path_ok_picture.format(str(label) + ".jpg"))
+    
+def stock_skeletton(label, skeletton):
+    path = r"C:\Users\jeanbaptiste\Desktop\dougy_petits_pecs\j_skeletton"
+
+    with open(path + "/skeletton.csv", 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=["label", "points"])
+        writer.writerow({"label":str(label), "points":str(skeletton)})
+
+
 protoFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_deploy.prototxt"
 weightsFile = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handa\models\pose_iter_102000.caffemodel"
 
@@ -48,7 +63,7 @@ if __name__ == "__main__":
 
     for i in liste:
         image_reading = i[:-4]
-        print(image_reading)
+        print("reading picture : ", image_reading)
 
         copy_img, rectangle, img, path = reading_picture(image_reading)
 
@@ -60,16 +75,19 @@ if __name__ == "__main__":
 
             sorted_fingers = treat_skeletton_points(points, position, finger, rectangle, img)
             move_picture(sorted_fingers, path, image_reading)
-            #rename picture to label
-            #recuperate skeleton
-            #make csv svm
 
             informations = fingers_analyse(sorted_fingers, img)
-            main_csv(informations)
+
+            picture = i
+            label = main_csv(informations)
+
+            rename_picture = label
+            rename_picture_to_label(rename_picture, picture)
+
+            stock_skeletton(label, position)
 
 
-
-
+            #RATIO
         else:
             print("nan")
 
