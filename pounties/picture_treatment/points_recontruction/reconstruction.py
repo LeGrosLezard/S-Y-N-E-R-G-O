2d9ec67b4d7437e6d96257f8c7f6f1f87c34b052
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-27 15:27:09 +0100

    Add files via upload

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -18,26 +18,49 @@ from utils_reconstruction import *
 
 def reconstruction(points, ratio, image):
 
+    #=============================
     """Recuperate data"""
-    data = recuperate_data_in_csv(1)
+    #=============================
+    data_csv = recuperate_data_in_csv(1)
 
-    """Treatement of our passation point to reconstruct"""
-    #Recuperate distance, scale, angulus from points to reconstruct.
-    distance, scale = collect_distances(points, ratio)
+
+    #=================================================================
+    """Treatement of our passation point to reconstruct
+    Recuperate distance, scale, angulus from points to reconstruct."""
+    #=================================================================
+
+    passation_distance, passation_scale = collect_distances(points, ratio)
 
     x_y_absice = collect_points(points)
-    angles = points_to_angle(x_y_absice)
+    passation_angles = points_to_angle(x_y_absice)
 
-    searching_points = what_we_need_to_search(distance)
+    searching_points = what_we_need_to_search(passation_distance)
     print(searching_points, "\n")
 
-
+    #================================================
     """Begenning to compare passation and data csv"""
+    #================================================
+
+
     liste_informations_angle = []
     liste_informations_distance = []
 
-    for i in data:
+    for data in data_csv:
 
+        #Treatment data
+        data_distance, data_scale = collect_distances(data[0], data[1])
+        points_data = collect_points(data[0])
+        angle_data = points_to_angle(points_data)
+
+        norm = determine_ratio(data_scale, passation_scale)
+
+        #Recup data
+        distance = proximum_distance(passation_distance, data_distance, norm)
+        angle = proximum_distance(passation_angles, angle_data, 1)
+
+        #Stock data
+        liste_informations_distance.append(distance)
+        liste_informations_angle.append(angle)
 
 
 
