8da8cbafc32076ee6f43bdf4ccf11c9112994d41
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-29 17:14:26 +0100

    Update reconstruction.py

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -1,83 +1,115 @@
+from ok1 import *
 
 
-from utils_reconstruction import *
+def current_points(points, ratio):
+
+    #ratio
+    ratio = make_ratio(ratio)
+
+    #distance
+    distance = collect_distances(points, "", "", "current")
+
+    #angle
+    abscisse = collect_abscisse(points)
+    angle = points_to_angle(abscisse)
+
+    informations = (distance, angle, ratio)
+
+    return informations
 
 
-points_current = [((0, 0), (0, 0)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)), ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)), ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)), ((40, 94), (41, 90))]
-ratio_current = (31, 31, 113, 109)
+def points_to_search(distance, points):
+
+    none_points = what_we_need_to_search(distance)
+
+    fingers_points = points_to_fingers_dict(points)
+
+    search_points = so_we_search(none_points, fingers_points)
+
+    for k, v in fingers_points.items():
+        print("finger ", k.upper(), v)
+
+    print("")
+    print(none_points)
+    print(search_points, "\n\n\n\n")
 
 
-
-
-        
-def rebuilt(points_current, ratio_current):
-
-    passation = passation_treatment(points_current, ratio_current)
-    ratio_current, distance_current, angle_current, search_points, points_current = passation
-
-
-
-    liste_angle, liste_distance = data_treatment(5, ratio_current)
-    print(len(liste_angle))
-
-
-    for info in search_points:
-        
-        print(info)
-
-        finger_name, phax_searching, phax_interest = info
-
-        liste_metablockant = compare_distance(liste_distance, distance_current, finger_name, phax_searching)
-
-        liste_1 = compare_angle(liste_angle, angle_current, finger_name, phax_searching)
-
-        minimums = recuperate_minimums_values(liste_metablockant, liste_1)
-        index_minimum_distance, index_minimum_angle = minimums
-
-
-        #REPLACE DATA CURRENT
-
-        if phax_interest == 0: #[x 0 0]
-            points_current[finger_name][0][1] = points_current[finger_name][1][0]
-            distance, angle = recuperate_angle_distance(liste_distance, liste_angle, index_minimum_angle,
-                                                        index_minimum_distance, finger_name, phax_interest)
-            replace_point(0, 1, 0, "minus", points_current, finger_name, distance, angle)
-
-
-        elif phax_interest == len(points_current[finger_name]) - 1: #[0 0 x]
-            points_current[finger_name][phax_interest][0] = points_current[finger_name][phax_searching][1]
-            distance, angle = recuperate_angle_distance(liste_distance, liste_angle, index_minimum_angle,
-                                                        index_minimum_distance, finger_name, phax_interest)
-            replace_point(phax_interest, 0, 1, "minus", points_current, finger_name, distance, angle)
-
-
-        else:#[0 x 0]
-            points_current[finger_name][phax_interest][0] = points_current[finger_name][phax_searching][1]
-            distance, angle = recuperate_angle_distance(liste_distance, liste_angle, index_minimum_angle,
-                                                        index_minimum_distance, finger_name, phax_interest)
-            replace_point(phax_interest, 0, 1, "add", points_current, finger_name, distance, angle)
+    return search_points, fingers_points
 
 
 
-    olé = []
-    for k, v in points_current.items():
-        for i in v:
-            olé.append(tuple(i))
 
 
-    blank_image = np.zeros((500, 500, 3), np.uint8)
-    for i in olé:
-        for j in i:
-            cv2.circle(blank_image, (int(j[0]), int(j[1])) , 2, (0, 0, 255), 2)
-            cv2.line(blank_image, (i[0]), (i[1]), (0, 255, 0), 2)
-    cv2.imshow("blank_imageaaa", blank_image)
-    cv2.waitKey(0)
 
 
-    return points_current
-    
 
-points_current = rebuilt(points_current, ratio_current)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    points = [((0, 0), (0, 0)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)), ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)), ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)), ((40, 94), (41, 90))]
+    ratio = (31, 31, 113, 109)
+
+
+    informations_current_point = current_points(points, ratio)
+    distance1, angle1, ratio1 = informations_current_point
+    print(distance1, angle1, ratio1)
+
+    points_to_search(distance1, points)
+
+
+
+
+
+
+
+
+
 
 
 
