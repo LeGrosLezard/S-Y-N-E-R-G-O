e6f35bd724cf1cf55eae72eedb8e17f1bb2d09fe
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-30 23:08:45 +0100

    Update reconstruction.py

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -1,252 +1,111 @@
+import math
+
+(75, 79), (68, 55)
+
+from scipy.spatial import distance as dist
 from ok1 import *
 
 
-def current_points(points, ratio):
+def rat(ratio1, ratio2):
+    a = False
+    if ratio1 > ratio2:
+        #print("error")
+        a = True
+    ratio = ratio2 / ratio1
 
-    #Ratio
-    ratio = make_ratio(ratio)
+    return ratio, a
 
-    #Distance
-    distance = collect_distances(points, "", "", "current")
+a = math.degrees(math.atan(79-55/68-75))
+#y1-y2/x2-x1
+print(a)
 
-    #Angle
-    abscisse = collect_coordinate(points)
-    angle = points_to_angle(abscisse)
+b = dist.euclidean((75, 79), (68, 55))
+print(b)
 
-    informations = (distance, angle, ratio)
 
-    return informations
+x = int(b * math.cos(a))
+y = int(b * math.sin(a))
 
 
-def points_to_search(distance, points):
+print(x, y)
 
-    #Phax missed
-    none_points = what_we_need_to_search(distance)
 
-    #Point sort by finger
-    fingers_points = points_to_fingers_dict(points)
 
-    #Phax search
-    search_points = so_we_search(none_points, fingers_points)
 
-    for k, v in fingers_points.items():
-        print("finger ", k.upper(), v)
+distance = []
+angle = []
+dat = []
+for csv_name in range(1, 8):
+    data_csv = recuperate_data_in_csv(csv_name)
 
-    print("")
-    print(none_points)
-    print(search_points, "\n\n\n\n")
+    for nb, data in enumerate(data_csv):
+        dataa, ratio = data[0], data[1]
 
-    informations = (search_points, fingers_points)
+        data = points_to_fingers_dict(dataa)
 
-    return informations
+        x1, y1 = data["m"][0][0][0], data["m"][0][0][1]
+        x2, y2 = data["m"][1][0][0], data["m"][1][0][1]
 
+        
+        e = dist.euclidean((x1, y1), (x2, y2))
 
 
-def data_treatment(ratio_current):
+        d = math.degrees(math.atan(y2-y1/x1-x2))
 
-    
-    number_csv = number_csv_file()
+        distance.append(e)
+        angle.append(d)
+        dat.append((dataa, ratio[2] * ratio[3]))
 
 
-    angulus_list = []
-    distance_list = []
+kk = []
+for nb, (i, j) in enumerate(zip(distance, angle)):
+    #print(nb, i, j)
 
-    for csv_name in range(1, number_csv):
-        data_csv = recuperate_data_in_csv(csv_name)
 
-        for data in data_csv:
+    ratio, er = rat(113 * 109, dat[nb][1])
 
-            points, ratio = data[0], data[1]
+    if er is True:
+        b = b / ratio
+    else:
+        i = i / ratio
 
-            #Normalisation
-            ratio = make_ratio(ratio)
-            norm, which = normalisation(ratio, ratio_current)
 
-            #Distances
-            distance = collect_distances(points, which, norm, "data")
 
-            #Angles
-            abscisse = collect_coordinate(points)
-            angle = points_to_angle(abscisse)
+    yoyo = math.sqrt((a - j)**2 + (b - i)**2)
 
-            #Sauvegard
-            angulus_list.append(angle)
-            distance_list.append(distance)
+    if nb == 41:
+        print(ratio)
+    kk.append((yoyo, nb))
+#x currnet - x, y current - y)
 
 
-    
-    informations = (angulus_list, distance_list)
-    return informations
 
 
 
 
-def reconstruction(points, ratio):
+aaaaa = sorted(kk, key=lambda x: x[0])
+print(aaaaa)
 
-    #Passation treatment.
-    informations_current_point = current_points(points, ratio)
-    distance, angle, ratio = informations_current_point
-    print(distance, angle, ratio)
 
-    #Define points to search in data.
-    actual_searching = points_to_search(distance, points)
-    search_points, points = actual_searching
+print("MAINTENANT PRENDRE LE DOIGT ENTIER PTETRE CHAI PAS ON VERRA")
 
-    #Data treatment in function of passation info.
-    data_lists = data_treatment(ratio)
-    angulus_list, distance_list = data_lists
 
 
-    #In none phax we search:
-    for search in search_points:
+blank_image = np.zeros((500, 500, 3), np.uint8)
+for i in dat[41][0]:
+    for j in i:
 
-        #finger name, if none phax 0 (interest), search phax 1
-        finger_name, phax_search, phax_interest = search
+        cv2.circle(blank_image, (int(j[0]), int(j[1])) , 2, (0, 0, 255), 2)
+        cv2.line(blank_image, (i[0]), (i[1]), (0, 255, 0), 2)
 
-        #Compare distances (data and passation).
-        distance_compareason = compare_distance(distance_list, distance,
-                                                finger_name, phax_search)
+        cv2.circle(blank_image, (75, 79) , 2, (0, 0, 255), 2)
+        cv2.circle(blank_image,(68, 55) , 2, (0, 0, 255), 2)
+        cv2.line(blank_image, (75, 79), (68, 55), (0, 255, 0), 2)
 
-        #Compare angulus (data and passation).
-        angulus_compareason = compare_angle(angulus_list, angle,
-                                            finger_name, phax_search)
 
-        #Recuperate minimums distances and minimums angulus.
-        minimums = minimum_values(distance_compareason, angulus_compareason)
-        index_distance, index_angulus = minimums
+cv2.imshow("blank_imageaaa", blank_image)
+cv2.waitKey(0)
 
-        #Informations for changed angulus/distances to coordinates.
-        a, b = (phax_interest, phax_search)
-
-        info1 = (distance_list, index_distance, finger_name, phax_interest)
-        info2 = (angulus_list, index_angulus, finger_name, phax_interest)
-
-
-        #[x 0 0]
-        if phax_interest == 0:
-
-            #Replace passation points pairs.
-            points[finger_name][a][1] = points[finger_name][b][0]
-
-            #Recuperate mini's data coordinates from csv.
-            minimums_data = recuperate_angle_distance(info1, info2)
-            index_dist, index_angle = minimums_data
-
-            #Replace current points by data given
-            informations = (points, finger_name, phax_interest, index_dist, index_angle, 1, phax_search)
-
-            x = points[finger_name][0][1][0] + int(index_dist * math.cos(index_angle))
-            y = points[finger_name][0][1][1] + int(index_dist * math.sin(index_angle))
-            
-            points[finger_name][0][0] = (x, y)
-
-            
-
-
-        #[0 0 x]
-        elif phax_interest == len(points[finger_name]) - 1:
-
-            #Replace passation points
-            points[finger_name][a][0] = points[finger_name][b][1]
-
-            #Recuperate mini's data coordinates from csv.
-            minimums_data = recuperate_angle_distance(info1, info2)
-            index_dist, index_angle = minimums_data
-
-            #Replace current points by data given
-            informations = (points, finger_name, phax_interest, index_dist, index_angle, 0, phax_search)
-
-            #replace_point(informations, "minus")
-
-            x = points[finger_name][2][0][0] + int(index_dist * math.cos(index_angle))
-            y = points[finger_name][2][0][1] + int(index_dist * math.sin(index_angle))
-
-            points[finger_name][2][1] = (x, y)
-
-
-        #[0 x 0]
-        else:
-
-            #Replace passation points
-            points[finger_name][a][0] = points[finger_name][b][1]
-
-            #Recuperate mini's data coordinates from csv.
-            minimums_data = recuperate_angle_distance(info1, info2)
-            index_dist, index_angle = minimums_data
-
-
-            #Replace current points by data given
-            informations = (points, finger_name, phax_interest, index_dist, index_angle, 0, phax_search)
-
-            #replace_point(informations, "add")
-
-            x = points[finger_name][1][0][0] + int(index_dist * math.cos(index_angle))
-            y = points[finger_name][1][0][1] + int(index_dist * math.sin(index_angle))
-
-
-            points[finger_name][1][1] = (x, y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    for k,v in points.items():
-        print(k, v)
-        if k != "t" and v != [[(0, 0), (0, 0)], [(0, 0), (0, 0)], [(0, 0), (0, 0)]]:
-            points[k].insert(0, [points["t"][0][0], points[k][0][0]])
-
-
-
-    #print(data_lists[0][0])
-    points_draw = []
-    for k, v in points.items():
-        print(v)
-        for i in v:
-            points_draw.append(tuple(i))
-
-
-
-    blank_image = np.zeros((500, 500, 3), np.uint8)
-    for i in points_draw:
-        for j in i:
-            cv2.circle(blank_image, (int(j[0]), int(j[1])) , 2, (0, 0, 255), 2)
-            cv2.line(blank_image, (i[0]), (i[1]), (0, 255, 0), 2)
-
-    cv2.imshow("blank_imageaaa", blank_image)
-    cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    points = [((0, 0), (0, 0)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)), ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)), ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)), ((40, 94), (41, 90))]
-    ratio = (31, 31, 113, 109)
-
-
-    reconstruction(points, ratio)
 
 
 
