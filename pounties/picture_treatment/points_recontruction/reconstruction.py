20f53799868f10939f46070bf4dad5a53047e335
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-31 17:13:39 +0100

    Update reconstruction.py

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -1,90 +1,39 @@
 from reconstruction_utils import *
 
 
-def recuperate_distance(distance_list, scale_list, distances, scale_passation):
-
-    distance_knn = []
-
-    for distance_data in distance_list:
-
-        liste_of_liste = []
-
-        for dist_data, scale_data, dist_passation in zip(distance_data, scale_list, distances):
-
-            if dist_passation == None or dist_data == None:
-                liste_of_liste.append(None)
-
-            else:
-                info = normalise(scale_data, scale_passation, dist_data, dist_passation)
-
-                dist_passation, dist_data = info
-                liste_of_liste.append(dist_passation - dist_data)
-
-        distance_knn.append(liste_of_liste)
-
-
-    return distance_knn
 
 
 
-def recuperate_angulus(angulus_list, angulus):
-
-    angulus_knn = []
-    
-    for angulus_data in angulus_list:
-
-        liste_of_liste = []
-
-        for data, angle in zip(angulus_data, angulus):
-
-            if data == None or angle == None:
-                liste_of_liste.append(None)
-
-            else:
-                liste_of_liste.append(angle - data)
-
-        angulus_knn.append(liste_of_liste)
-
-    return angulus_knn
-
-
-def recuperate_knn(distance_knn, angulus_knn):
-
-
-    knn_dist_list = []
-
-    for distance, angulus in zip(distance_knn, angulus_knn):
-
-
-        liste_of_liste = []
-
-        for d, a in zip(distance, angulus):
-
-            if d == None or a == None:
-                liste_of_liste.append(None)
-
-            else:
-                liste_of_liste.append(math.sqrt((d)**2 + (a)**2))
-
-
-        knn_dist_list.append(liste_of_liste)
-
-    return knn_dist_list
+##def recuperate_knn(distance_knn, angulus_knn):
+##
+##
+##    knn_dist_list = []
+##
+##    for distance, angulus in zip(distance_knn, angulus_knn):
+##
+##        liste_of_liste = []
+##
+##        for d, a in zip(distance, angulus):
+##
+##            if d == None or a == None:
+##                liste_of_liste.append(None)
+##
+##            else:
+##                liste_of_liste.append(math.sqrt((d)**2 + (a)**2))
+##
+##
+##        knn_dist_list.append(liste_of_liste)
+##
+##    return knn_dist_list
 
 
 
 def reconstruction(points, scale):
 
     angulus = collect_angulus(points)
-    print(angulus)
-
     distances = collect_distances(points)
-    print(distances)
-
-    to_search = search_none_detections(angulus, distances)
-    print(to_search)
-
     scale = make_scale(scale)
+
 
     scale_list = []
     angulus_list = []
@@ -107,15 +56,31 @@ def reconstruction(points, scale):
 
 
 
-    
     distance_knn = recuperate_distance(distance_list, scale_list, distances, scale)
     angulus_knn = recuperate_angulus(angulus_list, angulus)
 
+    #knn_list = recuperate_knn(distance_knn, angulus_knn)
 
-    knn_list = recuperate_knn(distance_knn, angulus_knn)
 
-    for i in knn_list:
-        print(i)
+
+    dict_points = list_to_dict(points)
+    dict_points = search_none_detections(dict_points)
+
+    recuperate_data_for_knn(dict_points, distance_knn, angulus_knn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
