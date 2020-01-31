609a0b6627202e0a333cea5a17813c7cdca55cdf
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-01-31 01:26:27 +0100

    Update utils_reconstruction.py

--- pounties/picture_treatment/points_recontruction/utils_reconstruction.py
+++ pounties/picture_treatment/points_recontruction/utils_reconstruction.py
@@ -9,89 +9,18 @@ import auto_write_thread
 from scipy.spatial import distance as dist
 
 
-#==============================
-"""CSV TREATMENT"""
-#==============================
 
-PATH_FOLDER_CSV = r"C:\Users\jeanbaptiste\Desktop\pounties\data\csv"
+#============================================
+"""SEARCH POINTS"""
+#============================================
 
+def search_none_detections(angulus, distances):
+    search = []
+    for index, (angle, distance) in enumerate(zip(angulus, distances)):
+        if angle == None and distance == None:
+            search.append(index)
 
-def number_csv_file():
-
-    global PATH_FOLDER_CSV
-    csv_list = os.listdir(PATH_FOLDER_CSV)
-    number_csv = len(csv_list)
-
-    return number_csv
-
-
-def recuperate_data_in_csv(csv_name):
-    """From csv we recuperate points data"""
-
-    global PATH_FOLDER_CSV
-
-    path = PATH_FOLDER_CSV + "/" + str(csv_name) + ".csv"
-
-    data_list = []
-    with open(path, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-
-        for information in reader:
-            points = ast.literal_eval(information["points"])
-            ratio =  ast.literal_eval(information["ratio"])
-            label =  ast.literal_eval(information["label"])
-
-            data_list.append((points, ratio, label))
-
-    return data_list
-
-
-
-#==============================
-"""NORMALIZE DISTANCES"""
-#==============================
-
-def make_ratio(ratio):
-    """width picture* hight picture"""
-    return ratio[2] * ratio[3]
-
-def normalisation(ratio1, ratio2):
-    """Make the ratio.
-    Define the highter picture -> choice '/' or '*'."""
-
-    if ratio1 > ratio2:
-        norm = ratio1 / ratio2
-        which = 1
-
-    elif ratio2 > ratio1:
-        norm = ratio2 / ratio1
-        which = 2
-
-    return (norm, which)
-
-
-def collect_distances(points, which, norm, mode):
-    """Collect Euclidean distance from each point.
-    Repartite to finger's"""
-
-    distances = []
-    for nb in range(len(points)):
-
-        distance = dist.euclidean(points[nb][0], points[nb][1])
-
-        if mode is "data":
-            #The data picture highter
-            if which == 1:      distance = distance / norm
-            #The data picture smaller
-            elif which == 2:    distance = distance * norm
-        distances.append(distance)
-
-    dico = {"t" : distances[0:4],  "i" : distances[5:8],
-            "m" : distances[9:12], "an" : distances[13:16],
-            "a" : distances[17:20]}
-
-    return dico
-
+    return search
 
 
 
@@ -100,217 +29,108 @@ def collect_distances(points, which, norm, mode):
 """RECUPERATE ANGLES"""
 #============================================
 
-def collect_coordinate(points):
-    """Collect points and make a différence for recuperate angles
-        where:   x = Xi+1 - Xi
-                 y = - ( Yi+1 - Yi)
-    """
+def collect_angulus(points):
+    
+    angulus = []
+    for (pts1, pts2) in points:
 
-    abscisse = []
-    for nb in range(len(points)):
-        ptsX = points[nb][1][0] - points[nb][0][0]
-        ptsY = - (points[nb][1][1] - points[nb][0][1])
-        abscisse.append((ptsX, ptsY))
+        x1, y1 = pts1
+        x2, y2 = pts2
 
-    return abscisse
+        if x1 > 0 and x2 > 0:
+            side = y1 - y2 / x2 - x1
+            angulus.append(math.degrees(math.atan(side)))
+        else:
+            angulus.append(None)
 
-
-def points_to_angle(coordinates):
-    """Here we determinate arctangeante angle of from last abscisse
-        difference in a rectangle triangle ABC:
-        angle acb = atan   (cb / ab)"""
-
-    angulus_list = []
-
-    for points in coordinates:
-        x, y = points[0], points[1]
-
-        if y == 0 and x != 0:
-            if x > 0:    angulus_list.append(0)
-            elif x < 0:  angulus_list.append(180)
-
-        elif x == 0 and y != 0:
-            if y > 0:    angulus_list.append(90)
-            elif y < 0:  angulus_list.append(-90)
-
-        elif points != (0, 0):
-            angle = math.degrees(math.atan(y / x))
-            if angle < 0: angle += 180
-            angulus_list.append(int(angle))
-
-        elif points == (0, 0):
-            angulus_list.append(0)
-
-    dico_angle = {"t" : angulus_list[0:4],  "i"  : angulus_list[5:8],
-                  "m" : angulus_list[9:12], "an" : angulus_list[13:16],
-                  "a" : angulus_list[17:20]}
-
-    return dico_angle
+    return angulus
 
 
 
 
 #============================================
-"""TO RECONSTRUCT"""
+"""RECUPERATE DISTANCE"""
 #============================================
 
-def what_we_need_to_search(dico_passation_distance):
-    """ phax miss = [1, 2], finger = search finger miss,
-        None = ok."""
 
-    dico = {"t" :[], "i" : [], "m" : [], "an" : [], "a" : []}
+def collect_distances(points):
+    """Collect Euclidean distance from each point.
+    Repartite to finger's"""
 
-    for k, v in dico.items():
-        phax = []
-        for nb, i in enumerate(dico_passation_distance[k]):
-            if i == 0.0: phax.append(nb)
+    distances = []
 
-        if len(phax) == 3:       dico[k].append("finger")
-        elif 3 > len(phax) > 0:  dico[k] += [i for i in phax]
-        elif len(phax) == 0:     dico[k].append("None")
+    for (pts1, pts2) in points:
 
-    return dico
+        x1, y1 = pts1
+        x2, y2 = pts2
 
+        eucli = dist.euclidean((x1, y1), (x2, y2))
 
-def so_we_search(none_points, points):
-
-    search_points = []
-    to_not_search = (["finger"], ["None"])
-
-    for finger, searching in none_points.items():
-        if searching not in to_not_search:
-
-            for phax in range(len(searching)):
-
-                #First phax
-                if searching[phax] == 0 and len(searching) == 1:
-                    search_points.append((finger, 1, 0))
-
-                #[0, 1]
-                elif searching[phax] == 0 and\
-                     searching[phax + 1] == searching[phax] + 1:
-                    print("probleme")
-
-                #Other
-                else:
-                    point = searching[phax]
-                    search_points.append((finger, point - 1, point))
-
-    return search_points
+        if eucli == 0:
+            distances.append(None)
+        else:
+            distances.append(eucli)
 
 
-def replace_point(informations, sign):
-
-    #informations :
-    #current points, name finger, phax, distance, angle, position (x, y)
+    return distances
 
 
-    current = informations[0][informations[1]][informations[2]][0]
 
-    if sign == "minus":
-        x = current[0] - int(informations[3] * math.cos(informations[4]))
-        y = current[1] - int(informations[3] * math.sin(informations[4]))
-    elif sign == "add":
-        x = current[0] + int(informations[3] * math.cos(informations[4]))
-        y = current[1] + int(informations[3] * math.sin(informations[4]))
-
-    informations[0][informations[1]][informations[2]][informations[5]] = (x, y)
+def make_scale(ratio):
+    return ratio[2] * ratio[3]
 
 
 
 
-#========================================
-"""COMPAREASON OF DISTANCES"""
-#========================================
-def compare_distance(distance_list, distance, finger_name, phax_searching):
 
-    compareason_list = []
-    for index_data, data_dist in enumerate(distance_list):
+#============================================
+"""FORMAT LIST - TUPLE"""
+#============================================
 
-        from_data = data_dist[finger_name][phax_searching]
-
-        from_passation = distance[finger_name][phax_searching]
-
-        difference = abs(from_data - from_passation)
-
-        compareason_list.append((difference, index_data))
-
-    return compareason_list
+def tuple_to_list(points):
+    return [list(i) for i in points]
 
 
-
-#========================================
-"""COMPAREASON OF ANGULUS"""
-#========================================
-
-def compare_angle(angulus_list, angulus, finger_name, phax_searching):
-
-
-    compareason_list = []
-    for index_data, data_angulus in enumerate(angulus_list):
-
-        from_data = data_angulus[finger_name][phax_searching]
-
-        from_passation = angulus[finger_name][phax_searching]
-
-        distance_difference = abs(from_data - from_passation)
-
-        compareason_list.append((distance_difference, index_data))
-
-    return compareason_list
+def list_to_tuple(points):
+    return [tuple(i) for i in points]
 
 
 
 
-#========================================
-"""RECUPERATE MINIMUM VALUES"""
-#========================================
 
-def minimum_values(distance_list, anglulus_list):
+#==============================
+"""CSV TREATMENT"""
+#==============================
 
-    #Sorted list composed by data and index data list
-    distance_list = sorted(distance_list, key=lambda x: x[0])
-    anglulus_list = sorted(anglulus_list, key=lambda x: x[0])
+PATH_FOLDER_CSV = r"C:\Users\jeanbaptiste\Desktop\pounties\data\csv"
+PATH = PATH_FOLDER_CSV + "/{}.csv"
 
-    minimum_distance, index_distance = distance_list[0]
-    minimum_angle, index_angle = anglulus_list[0]
+def recuperate_data_in_csv():
+    """From csv we recuperate points data"""
 
-    #Recuperate index.
-    informations = (index_distance, index_distance)
-
-    return informations
+    global PATH_FOLDER_CSV
 
 
-def recuperate_angle_distance(info1, info2):
+    #number csv in folder
+    csv_list = os.listdir(PATH_FOLDER_CSV)
+    number_csv = len(csv_list)
 
-    #info1 = data liste distance
-    #info2 = data liste angulus
+    data_list = []
 
-    distance = info1[0][info1[1]][info1[2]][info1[3]]
-    angle = info2[0][info2[1]][info2[2]][info2[3]]
+    for file in range(1, number_csv):
 
-    informations = (distance, angle)
+        with open(PATH.format(file), newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
 
-    return informations
+            for information in reader:
+                points = ast.literal_eval(information["points"])
+                ratio =  ast.literal_eval(information["ratio"])
+                label =  ast.literal_eval(information["label"])
+
+                data_list.append((points, ratio, label))
 
 
-#========================================
-"""TRANSFORMATION DATA TO DICTIONNARY"""
-#========================================
-
-def points_to_fingers_dict(points):
-    """
-        1) - ( (0, 0), (0, 0) ) - > [ (0, 0), (0, 0) ]
-        2) - { t: points, i: points ... }
-    """
-
-    points_treat = [list(i) for i in points]
-
-    dico = {"t" : points_treat[0:4],  "i" : points_treat[5:8],
-            "m" : points_treat[9:12], "an" : points_treat[13:16],
-            "a" : points_treat[17:20]}
-
-    return dico
+    return data_list
 
 
 
