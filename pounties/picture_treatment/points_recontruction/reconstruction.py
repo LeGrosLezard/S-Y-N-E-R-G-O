4f5f83a79ea2345652e998dae2a262d9d0fda00d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-01 20:59:19 +0100

    Update reconstruction.py

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -3,18 +3,21 @@ from reconstruction_utils import *
 points = [((0, 0), (0, 0)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)), ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)), ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)), ((40, 94), (41, 90))]
 scale = (31, 31, 113, 109)
 
-def recuperate_points_passation(points, scale):
+def passation_informations(points, scale):
 
+    #Angulus
     angulus = collect_angulus(points)
+
+    #Distance
     distances = collect_distances(points)
+
+    #Scale
     scale = make_scale(scale)
 
     return angulus, distances, scale
 
 
-
-
-def recuperate_points_data():
+def data_informations():
 
     scale_list = []
     angulus_list = []
@@ -26,12 +29,15 @@ def recuperate_points_data():
 
         pts_data, scale_data = data[0], data[1]
 
+        #Scale
         scale_data = make_scale(scale_data)
         scale_list.append(scale_data)
 
+        #Distance
         distances_data = collect_distances(pts_data)
         distance_list.append(distances_data)
 
+        #Angulus
         anglulus_data = collect_angulus(pts_data)
         angulus_list.append(anglulus_data)
 
@@ -43,39 +49,51 @@ def recuperate_points_data():
 
 
 
+#Passation
+angulus, distances, scale = passation_informations(points, scale)
 
-angulus, distances, scale = recuperate_points_passation(points, scale)
-distance_list, angulus_list, scale_list, data = recuperate_points_data()
-
-
-
-#distances = element_to_dict(distances)
-
-kk = []
-for nb, (dist_data, ang_data) in enumerate(zip(distance_list, angulus_list)):
-
-    scale_data = scale_list[nb]
-
-    if angulus[1] != None and distances[1] != None and\
-       dist_data[1] != None and ang_data[1] != None
-    if scale_data > scale:
-        dist_data[1] = dist_data[1] / (scale_data/scale)
-    elif scale > scale_data:
-        distances[1] = distances[1] / (scale/scale_data)
-
-    print(dist_data[1], ang_data[1], scale_data)
-    print(angulus[1], distances[1], scale)
+#Data
+distance_list, angulus_list, scale_list, data = data_informations()
 
 
-    kk.append((math.sqrt((distances[1] - dist_data[1])**2 + (angulus[1] - ang_data[1])**2), nb))
+angulus = element_to_dict(angulus)
+distances = element_to_dict(distances)
+points = element_to_dict(points)
 
-    print("")
+
+none = ((0, 0), (0, 0))
+points = delete_points(points)
+for k, v in points.items():
+
+
+    for nb, i in enumerate(v):
+        if i == none and nb < len(v) - 1 and v[nb + 1] != none:
+            print(k, v, nb)
+            if nb == 0: nb = 1
+            a(distance_list, angulus_list, scale_list, k, nb, angulus, distances, scale)
+            b(distance_list, angulus_list, scale_list, k, nb, angulus, distances, scale)
+            c(distance_list, angulus_list, scale_list, v, angulus, distances, scale, k)
+            d(distance_list, angulus_list, scale_list, v, angulus, distances, scale, k)
 
 
 
 
-aaaaa = sorted(kk, key=lambda x: x[0])
-print(aaaaa)
+
+
+
+
+
+
+        elif i == none and v[nb - 1] != none:
+            #print(k, v, nb)
+            
+            points["m"][1] = ((5, 5), (5, 5))
+
+            
+
+
+
+
 
 
 
