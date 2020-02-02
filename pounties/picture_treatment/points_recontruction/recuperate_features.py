eee5b0431a951007b539b2c1c8d082eb8728154c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-02 16:40:30 +0100

    Update recuperate_features.py

--- pounties/picture_treatment/points_recontruction/recuperate_features.py
+++ pounties/picture_treatment/points_recontruction/recuperate_features.py
@@ -50,3 +50,58 @@ def collect_distances(points):
 
 def make_scale(ratio):
     return ratio[2] * ratio[3]
+
+
+
+#============================================
+"""RECUPERATE FEATURE FROM PASSATION"""
+#============================================
+
+
+def passation_informations(points, scale):
+
+    #Angulus
+    angulus = collect_angulus(points)
+
+    #Distance
+    distances = collect_distances(points)
+
+    #Scale
+    scale = make_scale(scale)
+
+    return angulus, distances, scale
+
+
+#============================================
+"""RECUPERATE FEATURE FROM DATA CSV"""
+#============================================
+from csv_treatment import recuperate_data_in_csv
+def data_informations():
+
+    scale_list = []
+    angulus_list = []
+    distance_list = []
+    
+    data_csv = recuperate_data_in_csv()
+
+    for nb, data in enumerate(data_csv):
+
+        pts_data, scale_data = data[0], data[1]
+
+        #Scale
+        scale_data = make_scale(scale_data)
+        scale_list.append(scale_data)
+
+        #Distance
+        distances_data = collect_distances(pts_data)
+        distance_list.append(distances_data)
+
+        #Angulus
+        anglulus_data = collect_angulus(pts_data)
+        angulus_list.append(anglulus_data)
+
+
+    return distance_list, angulus_list, scale_list, data_csv
+
+
+
