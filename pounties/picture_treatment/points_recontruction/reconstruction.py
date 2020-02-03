4403cd016f31a1352d99fc33fb4bb459f232afb0
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-03 22:51:10 +0100

    Update reconstruction.py

--- pounties/picture_treatment/points_recontruction/reconstruction.py
+++ pounties/picture_treatment/points_recontruction/reconstruction.py
@@ -1,17 +1,20 @@
+
 #
 from recuperate_features import data_informations
 #
 from recuperate_features import passation_informations
 #
 from knn import recuperate_minimal
+from knn import recuperate_distance_angulus_data
+
+
 #
 from convert_variable import element_to_dict
 #
-from recuperate_points_to_search import searching_points
+from recuperate_points_to_search import identify_phaxs_points
+from recuperate_points_to_search import indentify_finger_points
 #
 from built_points import modify_points
-
-
 
 
 def less_one_points_detected(informations1):
@@ -55,6 +58,58 @@ def less_one_points_detected(informations1):
 
 
 
+def no_points_detected(finger_to_search, points, distance_list, angulus_list, scale_list, scale):
+
+    searching = indentify_finger_points(finger_to_search)
+
+    for finger_name in searching:
+        print(finger_name)
+
+        if len(finger_name) == 1:   #Thumb or annular = 1 finger
+            pass
+
+
+        elif len(finger_name) == 2:
+
+            index_pair = [0, 1, 2]
+
+            finger_name_before = finger_name[0]
+            finger_name_after = finger_name[1]
+
+            before_pts = points[finger_name_before]
+            after_pts = points[finger_name_after]
+
+            if len(before_pts) == 3 and len(after_pts) == 3:
+
+                index_pair = [0, 1, 2]
+
+
+                informations_before = (index_pair, distance_list, angulus_list, finger_name_before)
+                before_distance, before_angulus = recuperate_distance_angulus_data(informations_before)
+
+                informations_after = (index_pair, distance_list, angulus_list, finger_name_after)
+                after_distance, after_angulus = recuperate_distance_angulus_data(informations_after)
+
+                for i, j in zip(listDistance, listAngulus):
+                    pass
+
+                for i, j in zip(listDistance, listAngulus):
+                    pass
+
+
+
+
+            elif len(before_pts) == 3 and len(after_pts) < 3:
+                print("nan")
+
+
+            elif len(before_pts) < 3 and len(after_pts) == 3:
+                print("ici")
+
+
+
+
+
 def reconstruction_points(points, scale):
     """1) - Here we run our skeletton points.
        2) - We order/annotated finger's into dictionnary
@@ -78,7 +133,7 @@ def reconstruction_points(points, scale):
     points = element_to_dict(points)
 
     #Search point none detected.
-    to_search = searching_points(points)
+    to_search, finger_to_search = identify_phaxs_points(points)
     print(to_search, "\n")
 
 
@@ -87,7 +142,7 @@ def reconstruction_points(points, scale):
     for finger_name, pts in to_search.items():
 
 
-        #TWO A) - Less one point detected. Can rebuilt finger
+        #TWO A) - Less one point detected. Can rebuilt finger.
         if pts != []:
             print(finger_name)
 
@@ -99,12 +154,8 @@ def reconstruction_points(points, scale):
 
 
 
-
-
-        #TWO B) - Finger no detected.
-        elif pts == []:
-            pass
-
+    #TWO B) - All points of finger are none.
+    no_points_detected(finger_to_search, points, distance_list, angulus_list, scale_list, scale)
 
 
 
@@ -118,8 +169,25 @@ if __name__ == "__main__":
 
 
 
-    points = [((0, 0), (0, 0)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)), ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)), ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)), ((40, 94), (41, 90))]
+    points = [((81, 115), (97, 105)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)),
+              ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)),
+              ((0, 0), (0, 0)), ((75, 79), (68, 55)), ((68, 55), (57, 44)), ((57, 44), (50, 37)),
+              ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)),
+              ((0, 0), (0, 0)), ((51, 98), (44, 91)), ((44, 91), (40, 94)),  ((40, 94), (41, 90))]
+
+
+#plusieurs doigts
+
+
+
     scale = (31, 31, 113, 109)
+
+##    points = [((81, 115), (97, 105)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69)),
+##              ((0, 0), (0, 0)), ((86, 76), (83, 55)), ((83, 55), (83, 47)), ((83, 47), (83, 40)),
+##              ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)),
+##              ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)), ((0, 0), (0, 0)),
+##              ((81, 115), (97, 105)), ((97, 105), (115, 94)), ((115, 94), (122, 79)), ((122, 79), (126, 69))]
+
 
     reconstruction_points(points, scale)
 
