7766433520a55aa9da0a1c123a337813fd368e0f
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-11 02:14:44 +0100

    Add files via upload

--- partie_site/download/views.py
+++ partie_site/download/views.py
@@ -4,6 +4,25 @@ from django.http import HttpResponseRedirect
 from .models import video_upload
 
 
+def redirection(redirection, location):
+    """Redirection on a template after have download file"""
+
+    #if redirection is True:
+        #return HttpResponseRedirect(location)
+    return HttpResponseRedirect(redirection)
+
+
+def forms_video():
+    """Verify form, recuperate file, clean it and register it
+    thank to the class into media folder."""
+
+    if form.is_valid():
+        name_video = request.FILES['docfile']
+        form.cleaned_data['docfile'].name
+        newdoc = video_upload(docfile = request.FILES['docfile'])
+        newdoc.save()
+        #print("file saved into :'media'")
+
 
 def upload(request):
     """Here we uploading videos.
@@ -15,14 +34,15 @@ def upload(request):
     form = video_upload_form(request.POST, request.FILES)
 
     if request.method == 'POST':
+        #CARE MAYBE YES MAYBE NOT
+        #section = request.POST.get('data')
+        #if section == eyes_section:
 
-        if form.is_valid():
-            name_video = request.FILES['docfile']
-            form.cleaned_data['docfile'].name
-            newdoc = video_upload(docfile = request.FILES['docfile'])
-            newdoc.save()
-            return HttpResponseRedirect('')
+            #forms_video()
+            #redirection(False, "")
+            #return render(request, 'home.html', {'form': form})
 
-    
-      
+        forms_video("", "")
+
     return render(request, 'Upload.html', {'form': form})
+
