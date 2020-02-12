2a0357a1ada8dc349ed9f56cf1e569d6dd57c54b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-12 03:53:54 +0100

    Update forms.py

--- partie_site/download/forms.py
+++ partie_site/download/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class video_upload_form(forms.Form):
-    title = forms.CharField(max_length=50)
-    docfile = forms.FileField(label='Selectionner un fichier')
+
+    title = forms.CharField(max_length=50, required=False)
+
+    docfile = forms.FileField(label='Selectionner un fichier vidéo',
+                              required=False,)
