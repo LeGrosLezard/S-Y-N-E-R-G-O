6ad787446cd0e0a23d2f5dbd3e2e00aad0583389
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-15 21:05:40 +0100

    Delete forms.py

--- partie_site/download/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class video_upload_form(forms.Form):
-
-    title = forms.CharField(max_length=50, required=False)
-
-    docfile = forms.FileField(label='Selectionner un fichier vidéo',
-                              required=False,)
