23f24773496f6289af4cfd4a9f372fcbaea15ba7
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-16 16:43:14 +0100

    Update video_capture.py
    pcque je me souviens que si le mec fait yeux haut droite gauche ca marche pas et faut dire le nombre de frame mais ca marche pas tres bien la

--- partie_site/Synergo/eyes_detector/video_capture.py
+++ partie_site/Synergo/eyes_detector/video_capture.py
@@ -5,27 +5,27 @@ start_time_import = time.time()
 import cv2
 import numpy as np
 
-from face_points import recuperate_landmarks
-from face_points import head_points
-from face_points import get_face_in_box
-from face_points import eyes_points_for_head_analysis
+from recuperate_points.face_points import recuperate_landmarks
+from recuperate_points.face_points import head_points
+from recuperate_points.face_points import get_face_in_box
+from recuperate_points.face_points import eyes_points_for_head_analysis
 
 
 #Blink part
-from blinking_eyes import blinking_eyes
-from analysis_eyes import blink_analysis
+from blinking.blinking_eyes import blinking_eyes
+from blinking.blinking_eyes import blink_analysis
 
 #Pupil part
-from pupille_tracker import pupille_tracker
+from pupille_tracker.pupille_tracker import pupille_tracker
 
 #Eyes movement part
-from eyes_movement import eyes_movements
+from eyes_movement.eyes_movement import eyes_movements, eyes_contours
 
 #Path to model, to media folder.
 from paths import media_path, dlib_model
 
 #Load dlib model
-from face_points import load_model_dlib
+from recuperate_points.face_points import load_model_dlib
 
 #Resize video.
 from pre_test import search_video_size
@@ -46,6 +46,8 @@ cap = cv2.VideoCapture(video)
 
 while True:
 
+    start_time_frame = time.time()
+
     _, frame = cap.read()
 
     height, width = frame.shape[:2]
@@ -56,20 +58,45 @@ while True:
 
     frame = cv2.resize(frame, (width, height))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-##
-##    #Recuperate landmarks and head box.
-##    landmarks, head_box = head_points(gray, predictor, detector)
+
+    #Recuperate landmarks and head box.
+    landmarks, head_box = head_points(gray, predictor, detector) #face_points
 
 
-#    if landmarks is not None:
+    if landmarks is not None:
 
         #Recuperate blink algorythme
-#        blinking_frame, result = blinking_eyes(landmarks, head_box)
-##        blink_analysis(result, nb_frame, blinking_frame)
-##
-##        #Recuperate pupil center
-##        right_eye, left_eye = pupille_tracker(landmarks, frame, gray)
-##        eyes = eyes_movements(landmarks, frame, right_eye, left_eye)
+        blinking_frame, result = blinking_eyes(landmarks, head_box) #blinking_eyes
+        blink_analysis(result, nb_frame, blinking_frame) #blinking_eyes
+
+        #Recuperate pupil center, eyes constitution = (x, y), crop
+        right_eye, left_eye = pupille_tracker(landmarks, frame, gray)
+
+
+        movements = eyes_movements(landmarks, frame, right_eye[0], left_eye[0])
+        if movements != "":
+            print(movements)
+
+  
+        eyes_contours(landmarks, frame, right_eye[0], left_eye[0])
+
+
+
+
+        #Draw
+        x, y, w, h = head_box
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, 'Head', ((x+w) - 30, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12), 2)
+
+
+        if right_eye[0][0] is not None:
+            cv2.circle(right_eye[1], (right_eye[0]), 4, (0, 0, 255), 1)
+        if left_eye[0][0] is not None:
+            cv2.circle(left_eye[1], (left_eye[0]), 4, (0, 0, 255), 1)
+
+
+        print("No ", nb_frame, "run : ", time.time() - start_time_frame)
+        nb_frame += 1
 
 
 
