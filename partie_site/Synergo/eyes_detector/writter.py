3c1d28808321ab9202b99f37c8b7888c49f3ea8c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-16 19:56:02 +0100

    Update writter.py

--- partie_site/Synergo/eyes_detector/writter.py
+++ partie_site/Synergo/eyes_detector/writter.py
@@ -1,11 +1,12 @@
 """We aren't in live, so we can recuperate every 1000 frames ~20 sec
-with that we can take the mean eyes"""
+with that we can take the mean eyes positions"""
 
 import time
 import cv2
 
 from paths import media_path, dlib_model
 
+NUMBER_OF_FRAME = 500
 
 def parametrages(video, number_divise):
 
@@ -35,9 +36,11 @@ def parametrages(video, number_divise):
 
 def video_support(frame_width, frame_height, number_picture, frame_sec):
 
+    global NUMBER_OF_FRAME
+
     #Create (number_picture / 1000) supports avi files.
     #Where 1000 is number of frame who's divide number of picture into the video
-    file_name = int(number_picture / 1000)
+    file_name = int(number_picture / NUMBER_OF_FRAME)
     print("Number of files: ", file_name)
 
     #Put it into dico
@@ -57,16 +60,19 @@ def video_support(frame_width, frame_height, number_picture, frame_sec):
 
 
 
-def video_writter(video):
+def video_writter(video, number_divise):
+
+    global NUMBER_OF_FRAME
 
     #Info video.
-    cap, frame_width, frame_height, number_picture, frame_sec = parametrages(video)
+    cap, frame_width, frame_height, number_picture, frame_sec = parametrages(video, number_divise)
     #Create empties video.
     dico_file = video_support(frame_width, frame_height, number_picture, frame_sec)
 
 
     file_used = 0
     nb_frame = 0
+    start_time_appending = time.time()
 
     while True:
 
@@ -74,8 +80,8 @@ def video_writter(video):
 
         height, width = frame.shape[:2]
 
-        width = int(width/2)
-        height = int(height/2)
+        width  = int(width / number_divise)
+        height = int(height / number_divise)
 
         frame = cv2.resize(frame, (width, height))
 
@@ -86,14 +92,14 @@ def video_writter(video):
 
         #File used += 1.
         #frame re initialise.
-        if nb_frame == 1000:
+        if nb_frame == NUMBER_OF_FRAME:
             file_used += 1
             nb_frame = -1
+            print("File in couse :", file_used)
+            print("Took : ", time.time() - start_time_appending)
+            start_time_appending = time.time()
 
         nb_frame += 1
-
-
-        print(nb_frame, "file in couse :", file_used)
 
         cv2.imshow("Frame", frame)
 
@@ -102,7 +108,6 @@ def video_writter(video):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-     
     cap.release()
     cv2.destroyAllWindows()
 
