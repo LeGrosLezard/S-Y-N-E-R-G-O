9466c44c6e5c9ed8b44ee3e8fcfafc50be0a658c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-16 04:15:37 +0100

    Update views.py

--- partie_site/Synergo/views.py
+++ partie_site/Synergo/views.py
@@ -4,14 +4,40 @@ It's the inteface beetween template and model (data base)"""
 
 from django.shortcuts import render
 
+#Json response to template
 from django.http import JsonResponse
 
+#Protection
 from django.views.decorators.csrf import csrf_protect
 
-from django.http import JsonResponse
-
+#Uploading model/form
 from .models import video_upload
 from .forms import video_upload_form
+
+
+def uploading_file(request):
+    """Here is a function for uploading files.
+    We call the form, verif his validity, cleanning it, save it
+    and return name of video for the next AJAX"""
+
+    #Call form.
+    form = video_upload_form(request.POST, request.FILES)
+
+    #Post from template.
+    if request.method == 'POST':
+
+        #Verify validity of the form.
+        if form.is_valid():
+
+            name_video = request.FILES['docfile']                       #Recuperate the file.
+            form.cleaned_data['docfile'].name                           #Cleanning.
+            newdoc = video_upload(docfile = request.FILES['docfile'])   #Call model.
+            newdoc.save()                                               #Saving.
+
+            return JsonResponse({"response" : str(name_video)})         #Response.
+
+
+
 
 def treat_video(request):
 
@@ -25,38 +51,19 @@ def treat_video(request):
 
 
 
-
-
 @csrf_protect
 def home(request):
-    """Home template, principal template.
-    Is made up of an access to the site and
-    sections to present the project
-    (eyes, face, head, hand, langage sections)."""
+    """Home template, principal template. Is made up of an access to the site and
+    sections to present the project (eyes, face, head, hand, langage sections)."""
 
     form = video_upload_form(request.POST, request.FILES)
 
-    if request.method == 'POST':
-
-        if form.is_valid():
-            name_video = request.FILES['docfile']
-            form.cleaned_data['docfile'].name
-            newdoc = video_upload(docfile = request.FILES['docfile'])
-            newdoc.save()
- 
-            return JsonResponse({"response" : str(name_video)})
 
 
-    return render(request, "Home.html",
-                  {'form':form, "response" : "coucou"})
+    return render(request, "Home.html", {'form':form})
 
 
 
 
 def essaie(request):
-    """Home template, principal template.
-    Is made up of an access to the site and
-    sections to present the project
-    (eyes, face, head, hand, langage sections)."""
-
     return render(request, "essaie.html")
