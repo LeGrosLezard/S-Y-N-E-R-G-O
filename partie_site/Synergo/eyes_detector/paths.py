360f1cb47da81dc728142734871a5ceeac662230
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-20 22:37:39 +0100

    Add files via upload

--- partie_site/Synergo/eyes_detector/paths.py
+++ partie_site/Synergo/eyes_detector/paths.py
@@ -1,7 +1,7 @@
 media_path = r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\media\video_upload\{}"
 dlib_model = r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\Synergo\eyes_detector\models\shape_predictor_68_face_landmarks.dat"
 
-path_data  = r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\Synergo\eyes_detector\data"
+path_data  =      r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\Synergo\eyes_detector\data"
 path_data_video = r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\Synergo\eyes_detector\data\{}"
 
 
