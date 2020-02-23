5dec058585351afc0513d27cb89590c7de4a93b2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2020-02-23 01:57:34 +0100

    Update video_capture_sleep_APP_1.py

--- partie_site/Synergo/eyes_detector/video_capture_sleep_APP_1.py
+++ partie_site/Synergo/eyes_detector/video_capture_sleep_APP_1.py
@@ -10,13 +10,13 @@ from .recuperate_points.face_points import head_points
 from .recuperate_points.face_points import get_face_in_box
 from .recuperate_points.face_points import eyes_points_for_head_analysis
 
-
 #Pupil part
 from .pupille_tracker.pupille_tracker import pupille_tracker
 
 #Path to model, to media folder.
 from .paths import media_path, dlib_model
 from .paths import path_eyes_detector_stuff
+from .paths import video_save_media
 
 #Blink part
 from .blinking.blinking_eyes import blinking_eyes
@@ -26,50 +26,47 @@ from .blinking.blinking_eyes import final
 #Load dlib model
 from .recuperate_points.face_points import load_model_dlib
 
-
 print("Import time : ", time.time() - start_time_import)
 
 
 
-def video_parameters(video_name, dlib_model):
 
-    #Recuperate name of the video.
-    video  = media_path.format(video_name)
+def displaying(alarm, frame):
 
-    #Load DLIB.
-    predictor, detector = load_model_dlib(dlib_model)
+    out = False
+    
+    if alarm[0] is not "":
+        cv2.putText(frame, alarm[0], (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+        out = True
+    if alarm[1] is not "":
+        cv2.putText(frame, alarm[1], (0, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,100,255), 2)
+        out = True
+    if alarm[2] is not "":
+        cv2.putText(frame, alarm[2], (0, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12), 2)
+        out = True
+
+    return out
+
+global_alarm = []
+def video_capture_to_face_sleep(path_video, video_name):
 
 
-    cap = cv2.VideoCapture(video)
+    cap = cv2.VideoCapture(path_video)
 
     #Recuperate video informations.
     frame_width  = int(cap.get(3))
     frame_height = int(cap.get(4))
     frame_sec = cap.get(cv2.CAP_PROP_FPS)
 
-    out = cv2.VideoWriter("coucou.avi", cv2.VideoWriter_fourcc('M','J','P','G'), int(frame_sec),
+    video_path = video_save_media.format(video_name + ".mp4")
+
+    out = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'X264'), 20,
                           (frame_width, frame_height))
 
+    #Load DLIB.
+    predictor, detector = load_model_dlib(dlib_model)
+
     start_time = time.time()
-
-
-    return predictor, detector, cap, out, start_time
-
-def displaying(alarm, frame):
-    if alarm[0] is not "":
-        cv2.putText(frame, alarm[0], (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-    if alarm[1] is not "":
-        cv2.putText(frame, alarm[1], (0, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,100,255), 2)
-    if alarm[2] is not "":
-        cv2.putText(frame, alarm[2], (0, 300), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36,255,12), 2)
-        start_time = time.time()
-
-
-global_alarm = []
-def video_capture_to_face_sleep(video_name):
-
-    predictor, detector, cap, out, start_time = video_parameters(video_name, dlib_model)
-
     nb_frame = 0
     continuer = True
     while continuer:
@@ -92,19 +89,26 @@ def video_capture_to_face_sleep(video_name):
                 timmer = time.time() - start_time
                 alarm = blink_analysis(result, nb_frame, blinking_frame, timmer) #blinking_eyes
 
-                displaying(alarm, frame)
+                to_slow = displaying(alarm, frame)
 
-                out.write(frame)
+                if to_slow is True:
+                    for i in range(10):
+                        out.write(frame)
+                else:
+                    out.write(frame)
                 nb_frame += 1
 
         else:
+            video_length = time.time() - start_time
             continuer = False
 
 
-    report = final()
-    print(report)
+    report = final(video_length)
+
     
-    return report
+    return report, "/media/video_save/" + video_name + ".mp4"
 
-
+if __name__ == "__main__":
+    path_video = r"C:\Users\jeanbaptiste\Desktop\SYNERGO_SITE\Synergo\media\video_upload\g.mp4"
+    video_capture_to_face_sleep(path_video, "g.mp4")
 
